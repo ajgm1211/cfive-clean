a3f0e9948a0a901a4350f14eac3030c48711776e
Alejandro <ale1211@gmail.com>
2018-04-03 14:57:23 -0400

    Implementacion  Template Metronic
    Former-commit-id: 1e67849c83f9ba27e58a306caa37f83f7e149f55

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,85 @@
+<div class="page-header">
+
+
+    <div class="page-header-menu">
+
+
+        <div class="container">
+
+
+
+            <!-- BEGIN MEGA MENU -->
+            <!-- DOC: Apply "hor-menu-light" class after the "hor-menu" class below to have a horizontal menu with white background -->
+            <!-- DOC: Remove data-hover="dropdown" and data-close-others="true" attributes below to disable the dropdown opening on mouse hover -->
+
+            <div class="hor-menu">
+
+                <ul class="nav navbar-nav">
+                    <li>      <div class="">
+                        <a href="index.html">
+                            <img src="images/logo.png" alt="logo" class="">
+                        </a>
+                        </div>
+                    </li>
+
+                    <li aria-haspopup="true" class="menu-dropdown mega-menu-dropdown  mega-menu-full">
+                        <a href="javascript:;"> <b>COTIZACIONES</b>
+                            <span class="arrow"></span>
+                        </a>
+
+                    </li>
+                    <li aria-haspopup="true" class="menu-dropdown mega-menu-dropdown  mega-menu-full">
+                        <a href="javascript:;"> <b>CLIENTES</b>
+                            <span class="arrow"></span>
+                        </a>
+
+                    </li>
+                    <li aria-haspopup="true" class="menu-dropdown mega-menu-dropdown  mega-menu-full">
+                        <a href="javascript:;"> <b>PRECIO</b>
+                            <span class="arrow"></span>
+                        </a>
+
+                    </li>
+                    <li aria-haspopup="true" class="menu-dropdown mega-menu-dropdown  mega-menu-full">
+                        <a href="javascript:;"> <b>CONFIGURACIÓN</b>
+                            <span class="arrow"></span>
+                        </a>
+
+                    </li>
+                    <li>
+
+                        <ul>
+
+                            <li class="dropdown dropdown-user dropdown-dark">
+                                <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
+                                    <img alt="" class="img-circle" src="Metronic/assets/layouts/layout3/img/avatar9.jpg">
+                                    <span class="username username-hide-mobile">Nick</span>
+                                </a>
+                                <ul class="dropdown-menu dropdown-menu-default">
+                                    <li>
+                                        <a href="page_user_profile_1.html">
+                                            <i class="icon-user"></i> Mi perfil </a>
+                                    </li>
+
+                                    <li class="divider"> </li>
+
+                                    <li>
+                                        <a href="page_user_login_1.html">
+                                            <i class="icon-key"></i> Log Out </a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+
+                    </li>
+
+
+
+                </ul>
+
+            </div>
+            <!-- END MEGA MENU -->
+        </div>
+    </div>
+    <!-- END HEADER MENU -->
+</div>
