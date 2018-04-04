3351cf8cc41858b8ddc275c896de3df8667cbc0a
juanfrag <francisco@clichead.com>
2018-04-04 12:52:12 +0100

    Menu Modificado

--- resources/views/includes/header_menu.blade.php
+++ resources/views/includes/header_menu.blade.php
@@ -15,6 +15,43 @@
 											</span>
             </a>
         </li>
+
+        <li class="m-menu__item  m-menu__item--submenu m-menu__item--rel"  data-menu-submenu-toggle="click" data-redirect="true" aria-haspopup="true">
+            <a  href="#" class="m-menu__link m-menu__toggle">
+                <span class="m-menu__link-title">
+												<span class="m-menu__link-wrap">
+													<span class="m-menu__link-text">
+														Clients
+													</span>
+												</span>
+											</span>
+                <i class="m-menu__hor-arrow la la-angle-down"></i>
+                <i class="m-menu__ver-arrow la la-angle-right"></i>
+            </a>
+            <div class="m-menu__submenu m-menu__submenu--classic m-menu__submenu--left">
+                <span class="m-menu__arrow m-menu__arrow--adjust"></span>
+                <ul class="m-menu__subnav">
+                    <li class="m-menu__item "  data-redirect="true" aria-haspopup="true">
+                        <a  href="header/actions.html" class="m-menu__link ">
+                            <i class="m-menu__link-icon flaticon-business"></i>
+                            <span class="m-menu__link-text">
+															Companies
+														</span>
+                        </a>
+                    </li>
+                    <li class="m-menu__item "  data-redirect="true" aria-haspopup="true">
+                        <a  href="header/actions.html" class="m-menu__link ">
+                            <i class="m-menu__link-icon flaticon-business"></i>
+                            <span class="m-menu__link-text">
+															Contacs
+														</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </li>
+
+
         <li class="m-menu__item  m-menu__item--submenu m-menu__item--rel"  data-menu-submenu-toggle="click" data-redirect="true" aria-haspopup="true">
             <a  href="#" class="m-menu__link m-menu__toggle">
                 <span class="m-menu__link-title">
