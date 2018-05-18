762f39d51042bd853536ea3d64bec36ae3525de5
bettoarcaya <betoarcaya@gmail.com>
2018-05-18 09:50:04 -0400

    feat: creando archivo add.blade y form_terms.blade
    Former-commit-id: 33c7ca008b096f6edff3cffe60ddfb71e7cda1cc

---
+++ resources/views/terms/add.blade.php
@@ -0,0 +1,22 @@
+<div class="m-portlet">
+    <!--begin::Form-->
+    {!! Form::open(['route' => 'terms.store']) !!}
+    <div class="m-portlet__body">
+        <div class="m-form__section m-form__section--first">
+            <div class="form-group m-form__group">
+                @include('terms.partials.form_terms')
+            </div>
+        </div>
+        <div class="m-portlet__foot m-portlet__foot--fit">
+            <br>
+            <div class="m-form__actions m-form__actions">
+                {!! Form::submit('Save', ['class'=> 'btn btn-primary']) !!}
+                <a class="btn btn-success" href="{{url()->previous()}}">
+                    Cancel
+                </a>
+            </div>
+        </div>
+    </div>
+    {!! Form::close() !!}
+    <!--end::Form-->
+</div>
--- resources/views/terms/list.blade.php
+++ resources/views/terms/list.blade.php
@@ -40,30 +40,6 @@
                     <div class="row align-items-center">
                         <div class="col-xl-8 order-2 order-xl-1">
                             <div class="form-group m-form__group row align-items-center">
-
-                                <!--div class="col-md-4">
-                                    <div class="m-form__group m-form__group--inline">
-                                        <div class="m-form__label">
-                                            <label class="m-label m-label--single">
-                                                Rol:
-                                            </label>
-                                        </div>
-                                        <div class="m-form__control">
-                                            <select class="form-control m-bootstrap-select" id="m_form_type">
-                                                <option value="">
-                                                    All
-                                                </option>
-                                                <option value="1">
-                                                    Admin
-                                                </option>
-                                                <option value="2">
-                                                    Company
-                                                </option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                    <div class="d-md-none m--margin-bottom-10"></div>
-                                </div-->
                                 <div class="col-md-4">
                                     <div class="m-input-icon m-input-icon--left">
                                         <input type="text" class="form-control m-input" placeholder="Search..." id="generalSearch">
