6818272b7fb865a2ca30c9f983c25848a5a281d6
bettoarcaya <betoarcaya@gmail.com>
2018-05-22 18:53:12 -0400

    feat: agregando link directo con la vista de agregar nuevo termino y condiciones
    Former-commit-id: c5a91ec1c7fd2d67e1fc060ff0ca2e9a095c01f0 [formerly 137be409e184d5ce39ed359daf3b406ff4eb2bb8]
    Former-commit-id: da00e9f8d5321f7365692b9b8374fbe0c26eeca3

--- resources/views/terms/partials/form_terms.blade.php
+++ resources/views/terms/partials/form_terms.blade.php
@@ -3,11 +3,6 @@
     
 </div>
 
-<div class="form-group m-form__group">
-    {!! Form::text('port', null, ['placeholder' => 'Please enter the port name','class' => 'form-control m-input','required' => 'required']) !!}
-
-</div>
-
 <div class="form-group m-form__group"  >
     {!! Form::label('Port', 'Port') !!}<br>
     {{ Form::select('id', $array, ['class'=>'form-control']) }}
