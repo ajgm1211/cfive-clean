052e7da36e92033427ebf63aba5de4f3dbc4ab92
bettoarcaya <betoarcaya@gmail.com>
2018-06-04 19:14:45 -0400

    feat: creando modelo, migracion y controlador para los merge tags
    Former-commit-id: 18cbfc2e797309346f009d0cb2bbd114b7d03a13

---
+++ app/Http/Controllers/MergeTagController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\MergeTag;
+use Illuminate\Http\Request;
+
+class MergeTagController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\MergeTag  $mergeTag
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MergeTag $mergeTag)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\MergeTag  $mergeTag
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MergeTag $mergeTag)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\MergeTag  $mergeTag
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MergeTag $mergeTag)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\MergeTag  $mergeTag
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MergeTag $mergeTag)
+    {
+        //
+    }
+}
