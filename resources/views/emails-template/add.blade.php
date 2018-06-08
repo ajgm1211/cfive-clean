4c26a2bf177bd057bc9f7b87f0b0ebc6c0c605d1
bettoarcaya <betoarcaya@gmail.com>
2018-06-08 18:46:00 -0400

    feat: creando estructura html para el contenido de cada taga e implementandolo en los templates detinyMCE
    Former-commit-id: 312cfeb91ce8a4a6627723fbcbcfb73f848eb388

--- resources/views/emails-template/add.blade.php
+++ resources/views/emails-template/add.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="m-portlet">
     <!--begin::Form-->
-    {!! Form::model(['route' => 'mail-templates.store']) !!}
+    {!! Form::open(['route' => 'mail-templates.store']) !!}
     <div class="m-portlet__body">
         <div class="m-form__section m-form__section--first">
             <div class="form-group m-form__group">
