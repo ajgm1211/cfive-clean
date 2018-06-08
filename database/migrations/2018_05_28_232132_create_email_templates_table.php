97b58c9f05c598eb3fbe6cdfbfb658b6a3c0ff5d
bettoarcaya <betoarcaya@gmail.com>
2018-06-08 18:53:48 -0400

    feat: cambiando el campo menssage de string a longtext
    Former-commit-id: 5269a99d457a870ab5f9f8eaf41697dce03e42d1

--- database/migrations/2018_05_28_232132_create_email_templates_table.php
+++ database/migrations/2018_05_28_232132_create_email_templates_table.php
@@ -17,7 +17,7 @@ class CreateEmailTemplatesTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('subject');
-            $table->string('menssage');
+            $table->longText('menssage');
             $table->string('company');
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
