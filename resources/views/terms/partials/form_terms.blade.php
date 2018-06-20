a51b22d3d8babf4c33bbfd7bf39a0c19be3b3ed7
bettoarcaya <betoarcaya@gmail.com>
2018-06-19 21:10:22 -0400

    fix: eliminando campo mensaje en lista de email templates
    Former-commit-id: 723ec49a33394fb41755c91040bb74ec74559e3b

--- resources/views/terms/partials/form_terms.blade.php
+++ resources/views/terms/partials/form_terms.blade.php
@@ -10,7 +10,7 @@
 
 <div class="form-group m-form__group"  >
     {!! Form::label('Port', 'Port') !!}<br>
-    {{ Form::select('ports[]', $array, null, ['class'=>'m-select2-general form-control','multiple' => 'multiple']) }}
+    {!! Form::select('ports[]', $array, null, ['class'=>'m-select2-general form-control', 'multiple' => 'multiple']) !!}
 </div>
 
 <div class="form-group m-form__group">
