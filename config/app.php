0dcd50a1bc557c339db52a96b75fa67c148d489f
Alejandro G <ale1211@gmail.com>
2018-07-02 15:23:59 -0400

    app.php
    Former-commit-id: e7dca28aa39c0ff193ab1effaa6e433c514f8647

--- config/app.php
+++ config/app.php
@@ -162,11 +162,10 @@ return [
         Yajra\DataTables\DataTablesServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
         Laracasts\Flash\FlashServiceProvider::class,
-        //Barryvdh\DomPDF\ServiceProvider::class,
-        //GoogleMaps\ServiceProvider\GoogleMapsServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class,
+        GoogleMaps\ServiceProvider\GoogleMapsServiceProvider::class,
         Barryvdh\Elfinder\ElfinderServiceProvider::class,
-
-        //Maatwebsite\Excel\ExcelServiceProvider::class,
+        Maatwebsite\Excel\ExcelServiceProvider::class,
     ],
 
     /*
@@ -218,9 +217,9 @@ return [
         'DataTables' => Yajra\DataTables\Facades\DataTables::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
-        //'PDF' => Barryvdh\DomPDF\Facade::class,
-        //'GoogleMaps' => GoogleMaps\Facade\GoogleMapsFacade::class,
-        //'Excel' => Maatwebsite\Excel\Facades\Excel::class,
+        'PDF' => Barryvdh\DomPDF\Facade::class,
+        'GoogleMaps' => GoogleMaps\Facade\GoogleMapsFacade::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
     ],
 
 ];
