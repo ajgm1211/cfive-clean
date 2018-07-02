3d0af2ed2328154d4d32ef6a531aa43284344451
Julio Avila <javila3090@gmail.com>
2018-07-02 13:10:25 -0400

    Fixed issues in merge
    Former-commit-id: 018e88cf48286dbfedcd760b098086e0c2de7533

--- app/Http/Controllers/ContractsController.php
+++ app/Http/Controllers/ContractsController.php
@@ -397,7 +397,7 @@ class ContractsController extends Controller
             $nombre = $file->getClientOriginalName();
 
             $dd = \Storage::disk('UpLoadFile')->put($nombre,\File::get($file));
-            //dd(\Storage::disk('UpLoadFile')->url($nombre));
+            //dd(\Storage::disk('UpLoadFile')->url($nombre),\File::get($file));
 
             $contract = $request->contract_id;
             $errors=0;
--- database/migrations/2018_05_16_214528_create_quotes_table.php
+++ database/migrations/2018_05_16_214528_create_quotes_table.php
@@ -40,7 +40,7 @@ class CreateQuotesTable extends Migration
             $table->float('sub_total_origin')->nullable();
             $table->float('sub_total_freight')->nullable();
             $table->float('sub_total_destination')->nullable();
-            $table->integer('status_quote_id')->unsigned();
+            $table->integer('status_quote_id')->unsigned()->default(1);
             $table->foreign('status_quote_id')->references('id')->on('status_quotes');            
             $table->timestamps();
         });
