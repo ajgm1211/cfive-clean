e86d22443bea0ff58939c8e0a77eb4f76384d000
Julio Avila <javila3090@gmail.com>
2018-07-05 15:44:43 -0400

    05072018, Modified Quote's PDF and Added Companies restrictions
    Former-commit-id: ce52b594d54ab41c67b3648ede1944b176be0f22

--- resources/views/quotes/pdf/index.blade.php
+++ resources/views/quotes/pdf/index.blade.php
@@ -3,15 +3,16 @@
 <head>
     <meta charset="utf-8">
     <title>Quote #{{$quote->id}}</title>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}" media="all" />
     <link rel="stylesheet" href="{{asset('css/style-pdf.css')}}" media="all" />
 </head>
 <body style="background-color: white; font-size: 11px;">
     <header class="clearfix">
         <div id="logo">
-            <img src="logo.png">
+            <img src="{{$user->companyUser->logo}}" class="img img-responsive" width="200">
         </div>
         <div id="company">
-            <div><b>Quotation Id</b> <span style="color: gold"><b>#{{$quote->id}}</b></span></div>
+            <div><b>Quotation Id</b> <span style="color: #D0AD67"><b>#{{$quote->id}}</b></span></div>
             <div><b>Date of issue:</b> {{$quote->created_at}}</div>
             <div><b>Validity: </b>{{$quote->created_at}}</div>
         </div>
@@ -19,69 +20,51 @@
     <main>
         <div id="details" class="clearfix details">
             <div class="client">
-                <div class="panel panel-default" style="width: 300px;">
-                    <div class="panel-heading title" style="color:#000">From:</div>
-                    <div class="panel-body">
-                        <span id="destination_input" style="line-height: 0.4">
-                            <p>{{$user->name}}</p>
-                            <p><b>{{$user->companyUser->name}}</b></p>
-                            <p>{{$user->companyUser->address}}</p>
-                            <p>{{$user->companyUser->phone}}</p>
-                        </span>
-                    </div>
-                </div>
+                <p>From:</p>
+                <span id="destination_input" style="line-height: 0.4">
+                    <p>{{$user->name}}</p>
+                    <p><b>{{$user->companyUser->name}}</b></p>
+                    <p>{{$user->companyUser->address}}</p>
+                    <p>{{$user->companyUser->phone}}</p>
+                </span>
+
             </div>
-            <div class="company text-right" style="float: right; width: 150px;">
-                <div class="panel panel-default" >
-                    <div class="panel-heading title" style="color:#000">To:</div>
-                    <div class="panel-body">
-                        <span id="destination_input" style="line-height: 0.4">
-                            <p>Alejandro Gonzalez</p>
-                            <p><b>Maersk</b></p>
-                            <p>Caracas, Venezuela</p>
-                            <p>+580424156820</p>
-                        </span>
-                    </div>
-                </div>
+            <div class="company text-right" style="float: right; width: 350px;">
+                <p>To:</p>
+                <span id="destination_input" style="line-height: 0.4">
+                    <p>{{$quote->contact->first_name.' '.$quote->contact->last_name}}</p>
+                    <p><b>{{$quote->company->business_name}}</b></p>
+                    <p>{{$quote->company->address}}</p>
+                    <p>{{$quote->contact->phone}}</p>
+                </span>
+
             </div>
         </div>
-        <div id="details" class="clearfix details">
-            <div class="client" style="width: 350px;">
-                <div >
-                    <table border="0" cellspacing="0" cellpadding="0">
-                        <thead>
-                            <tr>
-                                <th class="unit" ><b>Origin</b></th>
-                            </tr>
-                        </thead>
-                        <tbody >
-                            <tr class="text-center">
-                                <td>{{$origin_harbor->name}}</td>
-                            </tr>
-                        </tbody>
-
-                    </table>
+        <div id="" class="clearfix">
+            <div class="client" style="width: 150px;">
+                <div class="panel panel-default" style="width: 350px;">
+                    <div class="panel-heading title">Origin</div>
+                    <div class="panel-body">
+                        <span id="origin_input" style="color: #1D3A6E;">
+                            {{$origin_harbor->name}}
+                        </span>
+                    </div>
                 </div>
             </div>
             <div class="company" style="float: right; width: 350px;">
-                <table border="0" cellspacing="0" cellpadding="0">
-                    <thead>
-                        <tr>
-                            <th class="unit"><b>Destination</b></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr class="text-center">
-                            <td>{{$destination_harbor->name}}</td>
-                        </tr>
-                    </tbody>
-                    
-                </table>
+                <div class="panel panel-default" style="width: 350px; height: 83px;">
+                    <div class="panel-heading title">Destination</div>
+                    <div class="panel-body">
+                        <span id="destination_input" style="color: #1D3A6E;">
+                            {{$destination_harbor->name}}
+                        </span>
+                    </div>
+                </div>
             </div>
         </div>
         <div id="details" class="clearfix details">
             <hr>
-            <div class="company">
+            <div class="company" style="color: #1D3A6E;">
                 <h3 class="title"><b>Cargo details:</b></h3>
                 <p>{!! $quote->qty_20 != '' ? $quote->qty_20.' x 20\' container':'' !!}</p>
                 <p>{!! $quote->qty_40 != '' ? $quote->qty_40.' x 40\' container':'' !!}</p>
@@ -89,7 +72,7 @@
             </div>
         </div>
         <hr>
-        <h3 class="title">Origin ammounts</h3>
+        <h3 class="title">Origin charges:</h3>
         <br>
         <table border="0" cellspacing="0" cellpadding="0">
             <thead >
@@ -98,7 +81,8 @@
                     <th class="unit"><b>Detail</b></th>
                     <th class="unit"><b>Units</b></th>
                     <th class="unit"><b>Price per units</b></th>
-                    <th class="unit"><b>Total</b></th>
+                    <th class="unit"><b>Total </b></th>
+                    <th class="unit"><b>Total &nbsp;@if(isset($currency_cfg->alphacode)){{$currency_cfg->alphacode}}@endif</b></th>
                 </tr>
             </thead>
             <tbody>
@@ -108,19 +92,20 @@
                     <td class="white">{{$origin_ammount->detail}}</td>
                     <td class="white">{{$origin_ammount->units}}</td>
                     <td class="white">{{$origin_ammount->price_per_unit}}</td>
-                    <td class="white">{{$origin_ammount->total_ammount}}</td>
+                    <td class="white">{{$origin_ammount->total_ammount}} {{$origin_ammount->currency->alphacode}}</td>
+                    <td class="white">{{$origin_ammount->total_ammount_2}} &nbsp;@if(isset($currency_cfg->alphacode)){{$currency_cfg->alphacode}}@endif</td>
                 </tr>
                 @endforeach
             </tbody>
             <tfoot>
                 <tr class="text-center subtotal">
                     <td colspan="4"></td>
-                    <td style="font-size: 12px;"><b>SUBTOTAL</b></td>
-                    <td style="font-size: 12px;">{{$quote->sub_total_origin}} USD</td>
+                    <td style="font-size: 12px; color: #01194F"><b>Subtotal</b></td>
+                    <td style="font-size: 12px; color: #01194F"><b>{{$quote->sub_total_origin}} &nbsp;@if(isset($currency_cfg->alphacode)){{$currency_cfg->alphacode}}@endif</b></td>
                 </tr>
             </tfoot>
         </table>
-        <h3 class="title">Freight ammounts</h3>
+        <h3 class="title">Freight charges:</h3>
         <br>
         <table border="0" cellspacing="0" cellpadding="0">
             <thead class="text-center">
@@ -130,6 +115,7 @@
                     <th class="unit"><b>Units</b></th>
                     <th class="unit"><b>Price per units</b></th>
                     <th class="unit"><b>Total</b></th>
+                    <th class="unit"><b>Total &nbsp;@if(isset($currency_cfg->alphacode)){{$currency_cfg->alphacode}}@endif</b></th>
                 </tr>
             </thead>
             <tbody>
@@ -139,19 +125,20 @@
                     <td>{{$freight_ammount->detail}}</td>
                     <td>{{$freight_ammount->units}}</td>
                     <td>{{$freight_ammount->price_per_unit}}</td>
-                    <td>{{$freight_ammount->total_ammount}}</td>
+                    <td>{{$freight_ammount->total_ammount}} {{$freight_ammount->currency->alphacode}}</td>
+                    <td>{{$freight_ammount->total_ammount_2}}</td>
                 </tr>
                 @endforeach
             </tbody>
             <tfoot>
                 <tr class="text-center" style="font-size: 12px;">
                     <td colspan="4"></td>
-                    <td style="font-size: 12px;"><b>SUBTOTAL</b></td>
-                    <td style="font-size: 12px;">{{$quote->sub_total_freight}} USD</td>
+                    <td style="font-size: 12px; color: #01194F"><b>Subtotal</b></td>
+                    <td style="font-size: 12px; color: #01194F"><b>{{$quote->sub_total_freight}} &nbsp;@if(isset($currency_cfg->alphacode)){{$currency_cfg->alphacode}}@endif</b></td>
                 </tr>
             </tfoot>
         </table>
-        <h3 class="title">Destination ammounts</h3>
+        <h3 class="title">Destination charges:</h3>
         <br>
         <table border="0" cellspacing="0" cellpadding="0">
             <thead>
@@ -161,6 +148,7 @@
                     <th class="unit"><b>Units</b></th>
                     <th class="unit"><b>Price per units</b></th>
                     <th class="unit"><b>Total</b></th>
+                    <th class="unit"><b>Total &nbsp;@if(isset($currency_cfg->alphacode)){{$currency_cfg->alphacode}}@endif</b></th>
                 </tr>
             </thead>
             <tbody>
@@ -170,15 +158,17 @@
                     <td>{{$destination_ammount->detail}}</td>
                     <td>{{$destination_ammount->units}}</td>
                     <td>{{$destination_ammount->price_per_unit}}</td>
-                    <td>{{$destination_ammount->total_ammount}}</td>
+                    <td>{{$destination_ammount->total_ammount}} {{$destination_ammount->currency->alphacode}}</td>
+                    <td>{{$destination_ammount->total_ammount_2}} &nbsp;@if(isset($currency_cfg->alphacode)){{$currency_cfg->alphacode}}@endif</td>
                 </tr>
                 @endforeach
             </tbody>
             <tfoot>
                 <tr class="text-center">
                     <td colspan="4"></td>
-                    <td style="font-size: 12px;"><b>SUBTOTAL</b></td>
-                    <td style="font-size: 12px;">{{$quote->sub_total_destination}} USD</td>
+                    <td style="font-size: 12px; color: #01194F"><b>Subtotal</b></td>
+                    <td style="font-size: 12px; color: #01194F"><b>{{$quote->sub_total_destination}} 
+                    &nbsp;@if(isset($currency_cfg->alphacode)){{$currency_cfg->alphacode}}@endif</b></td>
                 </tr>
             </tfoot>
         </table>
@@ -186,7 +176,7 @@
     <div class="clearfix details">
         <hr>
         <div class="company">
-            <h3 class="title"><b>TOTAL: {{$quote->sub_total_origin+$quote->sub_total_freight+$quote->sub_total_destination}} USD</b></h3>
+            <h3 class="title text-right" style="color: #01194F;"><b>TOTAL: {{$quote->sub_total_origin+$quote->sub_total_freight+$quote->sub_total_destination}} &nbsp;@if(isset($currency_cfg->alphacode)){{$currency_cfg->alphacode}}@endif</b></h3>
         </div>
     </div>
     <footer>
