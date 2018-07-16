a03f5e29eeac3408e7eb9771a3e527b481e6c1c1
Luis Blanco <luedblra@gmail.com>
2018-07-16 16:27:39 -0400

    se resuelve mapeo por columna
    Former-commit-id: c627c47861f9dd37f6b741799819d43085e6c0d2 [formerly bc1c56912b989573e9bf1e2806e9ddd4fdc76760]
    Former-commit-id: fe82252f204f7763b809dcd232e651873a3c3343

--- routes/web.php
+++ routes/web.php
@@ -96,6 +96,13 @@ Route::middleware(['auth'])->prefix('contracts')->group(function () {
     Route::get('CorrectedSurchargeForContracts','ContractsController@SaveCorrectedSurcharge')->name('Corrected.Surcharge.For.Contracts');
     Route::get('DestroySurchargeFailCorrectForContracts','ContractsController@DestroySurchargeFailCorrect')->name('Destroy.Surcharge.FailCorrect.For.Contracts');
     Route::get('UpdateSurchargeForContracts','ContractsController@UpdateSurchargeCorrect')->name('Update.Surcharge.For.Contractss');
+    
+    //Contract FCL Importation
+    
+    route::get('imporfcl',function(){
+       return view('contracts.ImporContractFcl');
+    })->name('importaion.fcl');
+    Route::PUT('UploadFileNewContracts','ContractsController@UploadFileNewContract')->name('Upload.File.New.Contracts');
 });
 
 Route::resource('UploadFile','FileHarborsPortsController');
