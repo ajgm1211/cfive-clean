38bf30288da7d7b9c53168344c1634444ba9c365
Julio Avila <javila3090@gmail.com>
2018-07-23 20:49:23 -0400

    23072018, Added address input in quotes, Changed redirect after quote's store, associated surcharges with sale terms
    Former-commit-id: 26054ab63e05bd843fe23de3c6f1b961c11439b6

---
+++ app/SaleTermSurcharge.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SaleTermSurcharge extends Model
+{
+    protected $fillable =['surcharge_id','sale_term_id'];
+}
--- resources/views/surcharges/edit.blade.php
+++ resources/views/surcharges/edit.blade.php
@@ -5,6 +5,11 @@
         <div class="m-form__section m-form__section--first">
             <div class="form-group m-form__group">
                 @include('surcharges.partials.form_surcharges')
+
+                <div class="form-group m-form__group">
+                    {!! Form::label('sale_term_id', 'Sale Terms') !!}<br>
+                    {{ Form::select('sale_term_id[]',$sale_terms,@$selected_sale_terms_array,['class'=>'custom-select form-control','id' => 'sale_term_id','multiple'=>'true']) }}
+                </div>                
             </div>
         </div>
         <div class="m-portlet__foot m-portlet__foot--fit">
@@ -19,3 +24,8 @@
     {!! Form::close() !!}
     <!--end::Form-->
 </div>
+<script type="text/javascript">
+    $('#sale_term_id').select2({
+      placeholder: "Select an option"
+    });
+</script>
