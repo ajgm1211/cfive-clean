7366be5b525bdffd7d4252b28bc299bc0008fa88
Luis Blanco <luedblra@gmail.com>
2018-07-25 12:24:34 -0400

    se resuelven relaciones con tablas fails, se agrega cambio de status por proyecto completado, se agrega method con direccionamiento incluido en la vista para rates + surcharge
    Former-commit-id: 1cb2bf8654b4bdc114d1ca0da891670131f5cf5b [formerly 33eaab168db7e501c6e10f73b1fcbc52a1dda0c8]
    Former-commit-id: 5a349c5cb0cca040f12deefe8690bbd5fc158f4b

--- database/migrations/2018_06_26_185743_add_fail_sur_charge_table.php
+++ database/migrations/2018_06_26_185743_add_fail_sur_charge_table.php
@@ -19,11 +19,12 @@ class AddFailSurChargeTable extends Migration
             $table->string('port_orig')->nullable();
             $table->string('port_dest')->nullable();
             $table->string('typedestiny_id')->nullable();
-            $table->string('contract_id')->nullable();
+            $table->integer('contract_id')->unsigned();
             $table->string('calculationtype_id')->nullable();
             $table->string('ammount')->nullable();
             $table->string('currency_id')->nullable();
             $table->string('carrier_id')->nullable();
+            $table->foreign('contract_id')->references('id')->on('contracts')->onDelete('cascade');
             $table->softDeletes();
             $table->timestamps();
         });
