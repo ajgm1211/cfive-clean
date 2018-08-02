cb9700fe627d1c949a7cc8690d2d14292025dca6
Alejandro G <ale1211@gmail.com>
2018-08-02 10:14:24 -0400

    Se crea la notificacion general
    Former-commit-id: 78b81151cba52c9b08d060a594b551446dc83239 [formerly 96635f9c0a21215436c5ddd53aead90f559dfea7]
    Former-commit-id: 71d9c90c3b25bd812e3d152b571e3fc5207c0949

---
+++ app/Notifications/N_general.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use App\User;
+class N_general extends Notification
+{
+  use Queueable;
+
+  protected $user;
+  protected $message;
+
+
+  public function __construct(User $user, 'message')
+  {
+    $this->user = $user;
+    $this->message = $message;    
+  }
+  /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+  public function via($notifiable)
+  {
+    return ['database','broadcast'];
+  }
+
+
+  public function toArray($notifiable)
+  {
+    return [
+      'id' => $this->id,
+      'read_at' => null,
+      'id_company' => $this->user->company_user_id,
+      'name_user' => $this->user->name,
+      'message' => $this->message,
+      'data' => [
+        'id_company' => $this->user->company_user_id,
+        'name_user' => $this->user->name,
+        'message' => $this->message,
+      ],
+    ];
+  }
+}
