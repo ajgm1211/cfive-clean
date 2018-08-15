a36a07d67a5ea951899794207ed225b4bbcf3bf3
Luis Blanco <luedblra@gmail.com>
2018-08-15 16:26:51 -0400

    se solventa problema de observador, se hacen pruebas exitosas con 7mil registros. se inicia la creacion de la vista informativa
    Former-commit-id: 5ee2d9c259b681e1c210acf8134c84f095665177

--- app/Observers/ContractObserver.php
+++ app/Observers/ContractObserver.php
@@ -8,61 +8,64 @@ use App\User;
 
 class ContractObserver
 {
-  /**
+    /**
      * Handle to the contract "created" event.
      *
      * @param  \App\Contract  $contract
      * @return void
      */
-  public function created(Contract $contract)
-  {
-    $userLogin  = auth()->user();
-    $idCompany = $contract->company_user_id;
+    public function created(Contract $contract)
+    {
+        $userLogin  = auth()->user();
+        $idCompany = $contract->company_user_id;
 
-    $users = User::all()->where('company_user_id','=',$idCompany);
-    $message = 'agrego el contrato :' . $contract->number ;
-    foreach ($users as $user) {
-      $user->notify(new N_general($userLogin,$message));
+        $users = User::all()->where('company_user_id','=',$idCompany);
+        $message = 'agrego el contrato :' . $contract->number ;
+        foreach ($users as $user) {
+            $user->notify(new N_general($userLogin,$message));
+        }
+
     }
 
-  }
-
-  /**
+    /**
      * Handle the contract "updated" event.
      *
      * @param  \App\Contract  $contract
      * @return void
      */
-  public function updated(Contract $contract)
-  {
-    $userLogin  = auth()->user();
-    $idCompany = $contract->company_user_id;
-  
+    public function updated(Contract $contract)
+    {
 
-    $users = User::all()->where('company_user_id','=',$idCompany);
-    $message = 'actualizo el contrato :' . $contract->number ;
+     if(auth()->user() != null){
+            $userLogin  = auth()->user();
+            $idCompany = $contract->company_user_id;
+            $users = User::all()->where('company_user_id','=',$idCompany);
+            $message = 'actualizo el contrato :' . $contract->number ;
 
-    foreach ($users as $user) {
-      $user->notify(new N_general($userLogin,$message));
+            foreach ($users as $user) {
+                $user->notify(new N_general($userLogin,$message));
+            }
+
+        }
+
+        
     }
 
-  }
-
-  /**
+    /**
      * Handle the contract "deleted" event.
      *
      * @param  \App\Contract  $contract
      * @return void
      */
-  public function deleted(Contract $contract)
-  {
-    $userLogin  = auth()->user();
-    $idCompany = $contract->company_user_id;
+    public function deleted(Contract $contract)
+    {
+        $userLogin  = auth()->user();
+        $idCompany = $contract->company_user_id;
 
-    $users = User::all()->where('company_user_id','=',$idCompany);
-    $message = 'elimino el contrato :' . $contract->number ;
-    foreach ($users as $user) {
-      $user->notify(new N_general($userLogin,$message));
+        $users = User::all()->where('company_user_id','=',$idCompany);
+        $message = 'elimino el contrato :' . $contract->number ;
+        foreach ($users as $user) {
+            $user->notify(new N_general($userLogin,$message));
+        }
     }
-  }
 }
