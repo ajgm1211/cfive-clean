6a9c506487d995a91c4bb31f8d829e1c4acd9457
Julio Avila <javila3090@gmail.com>
2018-08-20 21:50:43 -0300

    Fixed modals and general changes
    Former-commit-id: fed887b66b7e68119cff4f9fc378790fb5daa71c [formerly a41278b901beab90ba05c72ab5e70dc79bd018a9]
    Former-commit-id: dd991e3f527dbf265e667b1e22a4282875de1567

--- resources/views/companies/edit.blade.php
+++ resources/views/companies/edit.blade.php
@@ -7,36 +7,31 @@
  */
 ?>
 
-<div class="m-portlet">
 
-  <!--begin::Form-->
-  {{ Form::model($company, array('route' => array('companies.update', $company->id), 'method' => 'PUT')) }}
-  <div class="m-portlet__body">
-    <div class="m-form__section m-form__section--first">
-      <div class="form-group m-form__group">
+
+<!--begin::Form-->
+{{ Form::model($company, array('route' => array('companies.update', $company->id), 'method' => 'PUT')) }}
+<div class="m-form__section m-form__section--first">
+    <div class="form-group m-form__group">
         @include('companies.partials.form_add_companies')
-
-        {!! Form::label('price_id', 'Price Level') !!}<br>
-        {{ Form::select('price_id[]',$prices,@$company->company_price->price_id,['placeholder' => 'Please choose a option','class'=>'custom-select form-control','id' => 'm_select2-edit-company','multiple'=>true]) }}
-        {!! Form::label('users_id', 'Associate User') !!}<br>
-        {{ Form::select('users[]',$users,@$company->groupUserCompanies->pluck('user_id'),['class'=>'custom-select form-control','id' => 'users_company','multiple'=>'true']) }}
-        <!--begin::Form-->
-
-      </div>
+        <div class="form-group m-form__group">
+            {!! Form::label('price_id', 'Price Level') !!}<br>
+            {{ Form::select('price_id[]',$prices,@$company->company_price->price_id,['placeholder' => 'Please choose a option','class'=>'custom-select form-control','id' => 'm_select2-edit-company','multiple'=>true]) }}
+        </div>
+        <div class="form-group m-form__group">
+            {!! Form::label('users_id', 'Associate User') !!}<br>
+            {{ Form::select('users[]',$users,@$company->groupUserCompanies->pluck('user_id'),['class'=>'custom-select form-control','id' => 'users_company','multiple'=>'true']) }}
+        </div>
     </div>
-  </div>
-  <div class="m-portlet__foot m-portlet__foot--fit">
+</div>
+<div class="m-portlet__foot m-portlet__foot--fit">
     <div class="m-form__actions m-form__actions">
-      {!! Form::submit('Update', ['class'=> 'btn btn-primary']) !!}
-      <button class="btn btn-success" type="button" class="close" data-dismiss="modal" aria-label="Close">
-        <span aria-hidden="true">Cancel</span>
-      </button>
+        {!! Form::submit('Update', ['class'=> 'btn btn-success']) !!}
     </div>
-  </div>
+    <br>
 </div>
 {!! Form::close() !!}
 <!--end::Form-->
-</div>
 <script src="{{asset('js/base.js')}}" type="text/javascript"></script>
 <script src="/assets/demo/default/custom/components/forms/widgets/bootstrap-daterangepicker.js" type="text/javascript"></script>
 <script src="/assets/demo/default/custom/components/forms/widgets/select2.js" type="text/javascript"></script>
