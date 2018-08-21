7398f3fa2d83eca2caa4620d959367bb77ac9ddf
Alejandro G <ale1211@gmail.com>
2018-08-21 15:11:34 -0400

    Pruebas ofuscamiento de id
    Former-commit-id: d22df62125a2a95736b26302d6f8717f78c01ebc [formerly 3a2f88de4b351ed72494ef464064fa917a9109c1]
    Former-commit-id: 8b2314810062701772b8c5e07d25383e2d553d0b

---
+++ app/Function.php
@@ -0,0 +1,33 @@
+<?php
+use App\User;
+use Illuminate\Support\Facades\Auth;
+
+function setearRouteKey($key)
+{
+  $user =  User::where('company_user_id', "=",Auth::user()->company_user_id)->with('companyUser')->first();
+  
+  if(!empty($user)){
+    $hash = $user->companyUser->hash;
+  }else{
+    $hash = 'MySecretSalt';
+  }
+  $hashids = new \Hashids\Hashids($hash);
+
+  return $hashids->encode($key);
+}
+
+function obtenerRouteKey($key)
+{
+  $user =  User::where('company_user_id', "=",Auth::user()->company_user_id)->with('companyUser')->first();
+  if(!empty($user)){
+    $hash =$user->companyUser->hash;
+  }else{
+    $hash = 'MySecretSalt';
+  }
+  $hashids = new \Hashids\Hashids($hash);
+  $key = $hashids->decode($key);
+  return $key[0];
+}
+
+
+
