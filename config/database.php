006440f361b3e8b5e695f290fb32dedab5b0710a
Luis Blanco <luedblra@gmail.com>
2018-09-04 17:28:51 -0400

    Se modifican vistas de request, se agrega comando en kernel y se configura BD
    Former-commit-id: d6ca85562bbec34a928fb0593ad2d1460e6648ba [formerly 4594133818dc47fad311f17f642ba219fb6a604f]
    Former-commit-id: d83a3a6a71ff51c83eeb1adb0f539f5a194c006c

--- config/database.php
+++ config/database.php
@@ -68,9 +68,9 @@ return [
             'strict' => false,
             'engine' => null,
 
-        ],*/
-       
-      
+        ],
+        
+      */
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
