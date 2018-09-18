08de67dad43b777eb32d5128f62edcc846f01001
Alejandro G <ale1211@gmail.com>
2018-09-18 13:31:35 -0400

    se coloca segun rol el last login , se agrega dos nuevas pruebas de dusk unity
    Former-commit-id: 2c34cc87e7dea2eebf6a1270a13fd35d9d480463

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -68,7 +68,7 @@ All
                     </div>
                     <div class="col-xl-4 order-1 order-xl-2 m--align-right">
 
-                        <button type="button" class="addCompany btn btn-primary m-btn m-btn--custom m-btn--icon m-btn--air m-btn--pill" onclick="AbrirModal('add',0)">
+                        <button type="button" dusk="addCompany" class="btn btn-primary m-btn m-btn--custom m-btn--icon m-btn--air m-btn--pill" onclick="AbrirModal('add',0)">
                             <span>
                                 <i class="la la-user"></i>
                                 <span>
--- tests/Browser/CompanyTest.php
+++ tests/Browser/CompanyTest.php
@@ -8,25 +8,38 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class CompanyTest extends DuskTestCase
 {
-  /**
-     * A Dusk test example.
-     *
-     * @return void
-     */
-  public function testExample()
-  {
 
-    $this->browse(function ($browser) {
-      $this->assertTrue(true);
-      $browser->visit('/companies') //Go to the homepage
-        ->click('addCompany') //Click the Register link
+  public function testAddCompanies()
+    {
+        $this->browse(function (Browser $browser) {
+   
+            $browser->visit('/login')
+                ->type('email', 'admin@example.com')
+                ->type('password', 'secret')
+                ->press('Login')
+                ->visit("/companies/add")
+                ->type('business_name', 'company name')
+                ->type('phone', '45123456789')
+                ->type('email', 'mail@mail.com')
+                ->type('address', 'company address')
+                ->press('Save')
+                ->assertPathIs('/companies');
+        });
+    }
+  
+  
 
-        ->type('address', 'valparaiso chile')
-        ->value('business_name','test')
-        ->value('phone', '555-55-55')
-        ->value('email', 'secret@example.com')
-        ->click('button[type="submit"]')
-        ->assertPathIs('/companies');
-    });
-  }
+      public function testEditCompany(){
+        $this->browse(function (Browser $browser) {
+          $this->assertTrue(true);
+            $browser->visit("/companies/1/edit")
+                ->type('business_name', 'Alex name')
+                ->type('phone', '45123456789')
+                ->type('email', 'mail@mail.com')
+                ->type('address', 'company address')
+                ->press('Update');
+            
+        });
+    }
+    
 }
