9ec024a8e4f5da943f5c7ef8410e08d8931ceaa4
Luis Blanco <luedblra@gmail.com>
2018-09-20 11:56:46 -0400

    Se agrega columna en el modelos de Harbors, Se carga modulo de Reprocesar Rates fallidos por puertos inexistentes, se agregan notificaciones, se crea job para reprocesar, en funcionamiento...
    Former-commit-id: 70c132fb76e0586ccfcb0e44af9f659f51e46f80

--- app/Http/Controllers/ImportationController.php
+++ app/Http/Controllers/ImportationController.php
@@ -6,10 +6,16 @@ use Illuminate\Http\Request;
 use App\FailRate;
 use App\Rate;
 use App\Harbor;
+use App\Carrier;
+use App\Currency;
+use App\Contract;
+use App\User;
+use App\Jobs\ReprocessRatesJob;
+use App\Notifications\N_general;
 
 class ImportationController extends Controller
 {
-    public function ReprocesarRates($id){
+    public function ReprocesarRates(Request $request, $id){
 
         $countfailrates = FailRate::where('contract_id','=',$id)->count();
         if($countfailrates <= 150){
@@ -29,17 +35,41 @@ class ImportationController extends Controller
                 $destinationV       = '';
                 $originEX           = '';
                 $destinyEX          = '';
+                $twentyVal          = '';
+                $fortyVal           = '';
+                $fortyhcVal         = '';
+                $carrierVal         = '';
+                $carrierArr         = '';
+                $twentyArr          = '';
+                $fortyArr           = '';
+                $fortyhcArr         = '';
+                $currencyArr        = '';
+                $currencyVal        = '';
+                $currenct           = '';
 
-                $originB  = false;
-                $destinyB = false;
+                $curreExitBol       = false;
+                $originB            = false;
+                $destinyB           = false;
+                $twentyExiBol       = false;
+                $fortyExiBol        = false;
+                $fortyhcExiBol      = false;
+                $values             = true;
+                $carriExitBol       = false;
 
-                $originEX   = explode('_',$failrate->origin_port);
-                $destinyEX  = explode('_',$failrate->destiny_port);
-                $carrierEX  = count(explode('_',$failrate->carrier_id));
-                $twuentyEX  = count(explode('_',$failrate->twuenty));
-                $fortyEX    = count(explode('_',$failrate->forty));
-                $fortyhcEX  = count(explode('_',$failrate->fortyhc));
-                $currencyEX = count(explode('_',$failrate->currency_id));
+                $originEX    = explode('_',$failrate->origin_port);
+                $destinyEX   = explode('_',$failrate->destiny_port);
+                $carrierArr  = explode('_',$failrate->carrier_id);
+                $twentyArr   = explode('_',$failrate->twuenty);
+                $fortyArr    = explode('_',$failrate->forty);
+                $fortyhcArr  = explode('_',$failrate->fortyhc);
+                $currencyArr = explode('_',$failrate->currency_id);
+
+
+                $carrierEX     = count($carrierArr);
+                $twuentyEX     = count($twentyArr);
+                $fortyEX       = count($fortyArr);
+                $fortyhcEX     = count($fortyhcArr);
+                $currencyEX    = count($currencyArr);
 
                 if($carrierEX   <= 1 &&  $twuentyEX   <= 1 &&
                    $fortyEX     <= 1 &&  $fortyhcEX   <= 1 &&
@@ -68,24 +98,93 @@ class ImportationController extends Controller
                         }
                     }
 
-                    // Validacion de los datos en buen estado ------------------------------------------------------------------------
-                    if($originB == true && $destinyB == true){
-                        $collecciont = collect([]);
-                        
-                        $collecciont = [
-                            'origin_port'  => $originV,
-                            'destiny_port' => $destinationV,
-                        ];
-                        dd($collecciont);
+                    //---------------- Carrier ------------------------------------------------------------------
 
-                    } else {
-                        dd('No existe alguno de los puertos');
+                    $carrierResul = str_replace($caracteres,'',$carrierArr[0]);
+                    $carrier = Carrier::where('name','=',$carrierResul)->first();
+                    if(empty($carrier->id) != true){
+                        $carriExitBol = true;
+                        $carrierVal = $carrier->id;
                     }
+
+                    //---------------- 20' ------------------------------------------------------------------
+
+                    if(empty($twentyArr[0]) != true || (int)$twentyArr[0] == 0){
+                        $twentyExiBol = true;
+                        $twentyVal   = (int)$twentyArr[0];
+                    }
+
+                    //----------------- 40' -----------------------------------------------------------------
+
+                    if(empty($fortyArr[0]) != true || (int)$fortyArr[0] == 0){
+                        $fortyExiBol = true;
+                        $fortyVal   = (int)$fortyArr[0];
+                    }
+
+                    //----------------- 40'HC --------------------------------------------------------------
+
+                    if(empty($fortyhcArr[0]) != true || (int)$fortyhcArr[0] == 0){
+                        $fortyhcExiBol = true;
+                        $fortyhcVal   = (int)$fortyhcArr[0];
+                    }
+
+                    if($twentyVal == 0
+                       && $fortyVal == 0
+                       && $fortyhcVal == 0){
+                        $values = false;
+                    }
+                    //----------------- Currency -----------------------------------------------------------
+
+                    $currenct = Currency::where('alphacode','=',$currencyArr[0])->first();
+
+                    if(empty($currenct->id) != true){
+                        $curreExitBol = true;
+                        $currencyVal =  $currenct->id;
+                    }
+
+                    // Validacion de los datos en buen estado ------------------------------------------------------------------------
+                    if($originB == true && $destinyB == true &&
+                       $twentyExiBol   == true && $fortyExiBol  == true &&
+                       $fortyhcExiBol  == true && $values       == true &&
+                       $carriExitBol   == true && $curreExitBol == true){
+                        $collecciont = '';
+
+                        $collecciont = Rate::create([
+                            'origin_port'   => $originV,
+                            'destiny_port'  => $destinationV,
+                            'carrier_id'    => $carrierVal,                            
+                            'contract_id'   => $id,
+                            'twuenty'       => $twentyVal,
+                            'forty'         => $fortyVal,
+                            'fortyhc'       => $fortyhcVal,
+                            'currency_id'   => $currencyVal
+                        ]);
+                        $failrate->forceDelete();
+
+                    } 
                 }
+            }
+            $contractData = Contract::find($id);
+            $usersNotifiques = User::where('type','=','admin')->get();
+            foreach($usersNotifiques as $userNotifique){
+                $message = 'The Rates was Reprocessed. Contract: ' . $contractData->number ;
+                $userNotifique->notify(new N_general($userNotifique,$message));
             }
 
         } else {
-            //se ejecuta un en un job
+            ReprocessRatesJob::dispatch($id);
+            $request->session()->flash('message.nivel', 'success');
+            $request->session()->flash('message.content', 'The rates are reprocessing in the background');
+            return redirect()->route('Failed.Rates.Developer.For.Contracts',[$id,'1']);
+        }
+
+        $request->session()->flash('message.nivel', 'success');
+        $request->session()->flash('message.content', 'The rates are being reprocessed');
+        $countfailratesNew = FailRate::where('contract_id','=',$id)->count();
+        if($countfailratesNew > 0){
+            return redirect()->route('Failed.Rates.Developer.For.Contracts',[$id,'1']);
+        }else{
+            return redirect()->route('Failed.Rates.Developer.For.Contracts',[$id,'0']);
         }
     }
 }
