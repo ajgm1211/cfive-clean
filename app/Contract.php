2fe9f47beda0695564bd6cc7f1e1bb08876443cb
Alejandro G <ale1211@gmail.com>
2018-09-25 15:51:11 -0400

    se modifican los modelos company,companyuser,contact,contract,contractCompR,contractUserR,globalcharge,inland,quote,saleterm,saletermsur,statusquote
    Former-commit-id: e0e27e61d6158570dd7e0be4cff74eda6377aa3b
    Former-commit-id: c520bf54e90d31ae86ddc7e5bbdaf8ed514e97b2

--- app/Contract.php
+++ app/Contract.php
@@ -3,10 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class Contract extends Model
+class Contract extends Model implements Auditable
 {
-  protected $table    = "contracts";            
+  use \OwenIt\Auditing\Auditable;
+  protected $table    = "contracts";     
 
   protected $fillable = ['id', 'name','number','company_user_id','validity','expire','status'];
 
@@ -17,32 +19,32 @@ class Contract extends Model
   {
     return $this->belongsTo('App\CompanyUser');
   }
-  
+
   public function localcharges(){
-        //return $this->hasManyThrough('App\LocalCharCarrier', 'App\LocalCharge');
+    //return $this->hasManyThrough('App\LocalCharCarrier', 'App\LocalCharge');
     return $this->hasMany('App\LocalCharge');
   }
 
   public function contract_company_restriction(){
 
     return $this->HasMany('App\ContractCompanyRestriction');
-    
+
   }
 
   public function contract_user_restriction(){
 
     return $this->HasMany('App\ContractUserRestriction');
-    
+
   }
 
   public function user(){
 
     return $this->belongsTo('App\User');
-    
+
   }
-    
+
   public function FilesTmps(){
     return $thid->hasMany('App\FileTmp');  
   }
-  
+
 }
--- app/SaleTermSurcharge.php
+++ app/SaleTermSurcharge.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class SaleTermSurcharge extends Model
+class SaleTermSurcharge extends Model implements Auditable
 {
+  use \OwenIt\Auditing\Auditable;
   protected $fillable =['surcharge_id','sale_term_id'];
 
   public function saleterm()
