9691be2895f012a36d5b93eea4b3206b2f97e8a9
Alejandro G <ale1211@gmail.com>
2018-10-08 16:51:42 -0400

    global charges cambios por pais
    Former-commit-id: 7c4140100c9c5f61ef7e99db2c4c9af21a7fe336
    Former-commit-id: 82f45c29aff2a3f42bf6b88947f6b99543ffe689

--- public/js/globalcharges.js
+++ public/js/globalcharges.js
@@ -1,104 +1,104 @@
 function display_l(id){
 
-  $("#tr_l"+id+" .val").attr('hidden','true');
-  $("#tr_l"+id+" .in").removeAttr('hidden');
-  $("#tr_l"+id+" .in input , #tr_l"+id+" .in select ").prop('disabled', false);
+	$("#tr_l"+id+" .val").attr('hidden','true');
+	$("#tr_l"+id+" .in").removeAttr('hidden');
+	$("#tr_l"+id+" .in input , #tr_l"+id+" .in select ").prop('disabled', false);
 
 
-  $("#save_l"+id).removeAttr('hidden');
-  $("#cancel_l"+id).removeAttr('hidden');
-  $("#remove_l"+id).removeAttr('hidden');
-  $("#edit_l"+id).attr('hidden','true');
+	$("#save_l"+id).removeAttr('hidden');
+	$("#cancel_l"+id).removeAttr('hidden');
+	$("#remove_l"+id).removeAttr('hidden');
+	$("#edit_l"+id).attr('hidden','true');
 
 }
 
 function cancel_l(id){
 
-  $("#tr_l"+id+" .val").removeAttr('hidden');
-  $("#tr_l"+id+" .in").attr('hidden','true');
-  $("#tr_l"+id+" .in input , #tr_l"+id+" .in select ").prop('disabled', true);
+	$("#tr_l"+id+" .val").removeAttr('hidden');
+	$("#tr_l"+id+" .in").attr('hidden','true');
+	$("#tr_l"+id+" .in input , #tr_l"+id+" .in select ").prop('disabled', true);
 
-  $("#save_l"+id).attr('hidden','true');
-  $("#cancel_l"+id).attr('hidden','true');
-  $("#remove_l"+id).attr('hidden','true');
-  $("#edit_l"+id).removeAttr('hidden');
+	$("#save_l"+id).attr('hidden','true');
+	$("#cancel_l"+id).attr('hidden','true');
+	$("#remove_l"+id).attr('hidden','true');
+	$("#edit_l"+id).removeAttr('hidden');
 
 }
 
 function save_l(id,idval){
 
-  $.ajax({
-    type: 'GET',
-    url: 'globalcharges/updateGlobalCharge/' + idval,
-    data: {
-      'surcharge_id' : $("#type"+id).val(),
-      'port_orig' : $("#port_orig"+id).val(),
-      'port_dest' : $("#port_dest"+id).val(),
-      'changetype' : $("#changetype"+id).val(),
-      'carrier_id' : $("#localcarrier"+id).val(),
-      'calculationtype_id' : $("#calculationtype"+id).val(),
-      'ammount' : $("#ammount"+id).val(),
-      'currency_id' : $("#localcurrency"+id).val()
+	$.ajax({
+		type: 'GET',
+		url: 'globalcharges/updateGlobalCharge/' + idval,
+		data: {
+			'surcharge_id' : $("#type"+id).val(),
+			'port_orig' : $("#port_orig"+id).val(),
+			'port_dest' : $("#port_dest"+id).val(),
+			'changetype' : $("#changetype"+id).val(),
+			'carrier_id' : $("#localcarrier"+id).val(),
+			'calculationtype_id' : $("#calculationtype"+id).val(),
+			'ammount' : $("#ammount"+id).val(),
+			'currency_id' : $("#localcurrency"+id).val()
 
-    },
-    success: function(data) {
-
-
-      swal(
-        'Updated!',
-        'Your local charge has been updated.',
-        'success'
-      )
-      $("#save_l"+id).attr('hidden','true');
-      $("#cancel_l"+id).attr('hidden','true');
-      $("#remove_l"+id).attr('hidden','true');
-      $("#edit_l"+id).removeAttr('hidden');
-
-      $("#tr_l"+id+" .val").removeAttr('hidden');
-      $("#tr_l"+id+" .in").attr('hidden','true');
-      $("#tr_l"+id+" .in input , #tr_l"+id+" .in select ").prop('disabled', true);
-      var selText ="";
-      var porText = "";
-      var porTextDest = "";
-      $("#localcarrier"+id+" option:selected").each(function () {
-        var $this = $(this);
-        if ($this.length) {
-          selText += $this.text()+ ", ";
-
-        }
-      });
-      $("#port_orig"+id+" option:selected").each(function () {
-        var $this = $(this);
-        if ($this.length) {
-          porText += $this.text()+ ", ";
-
-        }
-      });
-      $("#port_dest"+id+" option:selected").each(function () {
-        var $this = $(this);
-        if ($this.length) {
-          porTextDest += $this.text()+ ", ";
-
-        }
-      });
+		},
+		success: function(data) {
 
 
-      $("#divtype"+id).html($("#type"+id+" option:selected").text());
-      $("#divport"+id).html(porText);
-      $("#divportDest"+id).html(porTextDest);
+			swal(
+				'Updated!',
+				'Your local charge has been updated.',
+				'success'
+			)
+			$("#save_l"+id).attr('hidden','true');
+			$("#cancel_l"+id).attr('hidden','true');
+			$("#remove_l"+id).attr('hidden','true');
+			$("#edit_l"+id).removeAttr('hidden');
 
-      $("#divchangetype"+id).html($("#changetype"+id+" option:selected").text());
-      $("#divcarrier"+id).html(selText);
-      $("#divcalculation"+id).html($("#calculationtype"+id+" option:selected").text());
-      $("#divammount"+id).html($("#ammount"+id).val());
-      $("#divcurrency"+id).html($("#localcurrency"+id+" option:selected").text());
+			$("#tr_l"+id+" .val").removeAttr('hidden');
+			$("#tr_l"+id+" .in").attr('hidden','true');
+			$("#tr_l"+id+" .in input , #tr_l"+id+" .in select ").prop('disabled', true);
+			var selText ="";
+			var porText = "";
+			var porTextDest = "";
+			$("#localcarrier"+id+" option:selected").each(function () {
+				var $this = $(this);
+				if ($this.length) {
+					selText += $this.text()+ ", ";
 
-    },
-    error: function (request, status, error) {
-      alert(request.responseText);
-    }
+				}
+			});
+			$("#port_orig"+id+" option:selected").each(function () {
+				var $this = $(this);
+				if ($this.length) {
+					porText += $this.text()+ ", ";
 
-  });
+				}
+			});
+			$("#port_dest"+id+" option:selected").each(function () {
+				var $this = $(this);
+				if ($this.length) {
+					porTextDest += $this.text()+ ", ";
+
+				}
+			});
+
+
+			$("#divtype"+id).html($("#type"+id+" option:selected").text());
+			$("#divport"+id).html(porText);
+			$("#divportDest"+id).html(porTextDest);
+
+			$("#divchangetype"+id).html($("#changetype"+id+" option:selected").text());
+			$("#divcarrier"+id).html(selText);
+			$("#divcalculation"+id).html($("#calculationtype"+id+" option:selected").text());
+			$("#divammount"+id).html($("#ammount"+id).val());
+			$("#divcurrency"+id).html($("#localcurrency"+id+" option:selected").text());
+
+		},
+		error: function (request, status, error) {
+			alert(request.responseText);
+		}
+
+	});
 
 }
 
@@ -107,104 +107,119 @@ function save_l(id,idval){
 $("#new").on("click", function() {
 
 
-  $('#buttons').removeAttr('hidden');
-  var $template = $('#globalclone');
-  $myClone = $template.clone().removeAttr('hidden').removeAttr('id');
-  $myClone.addClass('closetr');
-  $myClone.find("select").select2();
+	$('#buttons').removeAttr('hidden');
+	var $template = $('#globalclone');
+	$myClone = $template.clone().removeAttr('hidden').removeAttr('id');
+	$myClone.addClass('closetr');
+	$myClone.find("select").select2();
 
-  $ids = $( ".port_orig" ).length;
-  $myClone.find(".port_orig").attr('name', 'port_orig'+$ids+'[]');
-  $myClone.find(".port_dest").attr('name', 'port_dest'+$ids+'[]');
-  $myClone.find(".carrier").attr('name', 'localcarrier'+$ids+'[]');
-  $("#sample_editable_2").append($myClone);
-  // $("#tclone").clone().removeAttr('hidden').removeAttr('class').appendTo("#sample_editable_1");
-  // $clone = $template.clone().removeAttr('hidden').removeAttr('id').insertBefore($template);
+	$ids = $( ".port_orig" ).length;
+	$myClone.find(".port_orig").attr('name', 'port_orig'+$ids+'[]');
+	$myClone.find(".port_dest").attr('name', 'port_dest'+$ids+'[]');
+	$myClone.find(".carrier").attr('name', 'localcarrier'+$ids+'[]');
+	$("#sample_editable_2").append($myClone);
+	// $("#tclone").clone().removeAttr('hidden').removeAttr('class').appendTo("#sample_editable_1");
+	// $clone = $template.clone().removeAttr('hidden').removeAttr('id').insertBefore($template);
 
 });
 
 $(document).on('click', '.m_sweetalert_demo_8', function (e) {
-  var res = $("i",this).attr('id'); 
+	var res = $("i",this).attr('id'); 
 
-  var theElement = $(this);
-  var idval = res.substr(4);
+	var theElement = $(this);
+	var idval = res.substr(4);
 
-  swal({
-    title: 'Are you sure?',
-    text: "You won't be able to revert this!",
-    type: 'warning',
-    showCancelButton: true,
-    confirmButtonText: 'Yes, delete it!'
-  }).then(function(result) {
-    if (result.value) {
+	swal({
+		title: 'Are you sure?',
+		text: "You won't be able to revert this!",
+		type: 'warning',
+		showCancelButton: true,
+		confirmButtonText: 'Yes, delete it!'
+	}).then(function(result) {
+		if (result.value) {
 
-      $.ajax({
-        type: 'get',
-        url: 'globalcharges/deleteGlobalCharge/' + idval,
-        success: function(data) {
-          swal(
-            'Deleted!',
-            'Your file has been deleted.',
-            'success'
-          )
-          $(theElement).closest('tr').remove();
+			$.ajax({
+				type: 'get',
+				url: 'globalcharges/deleteGlobalCharge/' + idval,
+				success: function(data) {
+					swal(
+						'Deleted!',
+						'Your file has been deleted.',
+						'success'
+					)
+					$(theElement).closest('tr').remove();
 
-        }
-      });
+				}
+			});
 
-    }
+		}
 
-  });
+	});
 
 });
 
 
 $(document).on('click', '.remove', function () {
-  $(this).closest('tr').remove();
-  $i = 1;
-  $('.closetr').each(function () {
-    var res = $(".port_orig",this).removeAttr('name').attr('name', 'port_orig'+$i+'[]');
-    var resDest = $(".port_dest",this).removeAttr('name').attr('name', 'port_dest'+$i+'[]');
-    var car = $(".carrier",this).removeAttr('name').attr('name', 'localcarrier'+$i+'[]');
-    $i++;
-  });
+	$(this).closest('tr').remove();
+	$i = 1;
+	$('.closetr').each(function () {
+		var res = $(".port_orig",this).removeAttr('name').attr('name', 'port_orig'+$i+'[]');
+		var resDest = $(".port_dest",this).removeAttr('name').attr('name', 'port_dest'+$i+'[]');
+		var car = $(".carrier",this).removeAttr('name').attr('name', 'localcarrier'+$i+'[]');
+		$i++;
+	});
 });
 
 $(document).on('click', '.cancel', function () {
-  $('.closetr').closest('tr').remove();
-  $('#buttons').attr('hidden','true');
+	$('.closetr').closest('tr').remove();
+	$('#buttons').attr('hidden','true');
 });
 
+function activarCountry(act){
 
+
+	var divCountry = $( ".divcountry");
+	var divport = $( ".divport");
+	if(act == 'divcountry'){
+		divport.attr('hidden','true');
+		divCountry.removeAttr('hidden');
+	}else if(act == 'divport'){
+		divCountry.attr('hidden','true');
+		divport.removeAttr('hidden');
+	}
+
+
+
+}
 
 
 $('.m-select2-general').select2({
-  placeholder: "Select an option"
+	placeholder: "Select an option"
 });
 
 $(document).on('click', '.addS', function () {
-  
-  toastr.options = {
-  "closeButton": true,
-  "debug": false,
-  "newestOnTop": false,
-  "progressBar": false,
-  "positionClass": "toast-bottom-right",
-  "preventDuplicates": true,
-  "onclick": null,
-  "showDuration": "0",
-  "hideDuration": "0",
-  "timeOut": "0",
-  "extendedTimeOut": "0",
-  "showEasing": "swing",
-  "hideEasing": "linear",
-  "showMethod": "fadeIn",
-  "hideMethod": "fadeOut"
-};
-  var surcharge = $('.type');
-  if(surcharge.val() == null){
-    toastr.error('You have to first add surcharges terms in order to add surcharges to this global. <a href="surcharges" > <b> Add Surcharge</b> </a>!','IMPORTANT MESSAGE!');
-  }
+
+	toastr.options = {
+		"closeButton": true,
+		"debug": false,
+		"newestOnTop": false,
+		"progressBar": false,
+		"positionClass": "toast-bottom-right",
+		"preventDuplicates": true,
+		"onclick": null,
+		"showDuration": "0",
+		"hideDuration": "0",
+		"timeOut": "0",
+		"extendedTimeOut": "0",
+		"showEasing": "swing",
+		"hideEasing": "linear",
+		"showMethod": "fadeIn",
+		"hideMethod": "fadeOut"
+	};
+	var surcharge = $('.type');
+	if(surcharge.val() == null){
+		toastr.error('You have to first add surcharges terms in order to add surcharges to this global. <a href="surcharges" > <b> Add Surcharge</b> </a>!','IMPORTANT MESSAGE!');
+	}
 } );
 
 
