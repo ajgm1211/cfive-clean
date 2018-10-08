82f45c29aff2a3f42bf6b88947f6b99543ffe689
Alejandro G <ale1211@gmail.com>
2018-10-08 16:51:42 -0400

    global charges cambios por pais
    Former-commit-id: 7c4140100c9c5f61ef7e99db2c4c9af21a7fe336

---
+++ app/GlobalCharCountry.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class GlobalCharCountry extends Model
+{
+	protected $table    = "globalcharcountry";
+	protected $fillable =   ['country_orig','country_dest','globalcharge_id'];
+	public $timestamps = false;
+	public function globalcharge()
+	{
+		return $this->belongsTo('App\GlobalCharge','globalcharge_id');
+	}
+	public function countryOrig(){
+		return $this->belongsTo('App\Countries','country_orig');
+	}
+	public function countryDest(){
+		return $this->belongsTo('App\Countries','country_dest');
+
+	}
+}
