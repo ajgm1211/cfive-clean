68331942390f5ae6403e9e387258cbfe382100b4
Julio Avila <javila3090@gmail.com>
2018-10-17 17:52:36 -0300

    Fixed issue with modal add contacts in companies details
    Former-commit-id: 3b9c87f4814608285771f215cce13bc5513bbe7f

--- resources/views/contracts/index.blade.php
+++ resources/views/contracts/index.blade.php
@@ -224,8 +224,6 @@
                                 </div>
                                 <div class="col-xl-12 order-1 order-xl-2 m--align-right">
                                     <a href="{{ route('contracts.add') }}">
-
-
                                         <button type="button" class="btn btn-primary m-btn m-btn--custom m-btn--icon m-btn--air m-btn--pill" >
                                             <span>
                                                 <span>
