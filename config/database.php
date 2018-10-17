0ab06fb9c072cd67ccdfa3510ca26bd40da231dc
Julio Avila <javila3090@gmail.com>
2018-10-17 16:12:49 -0300

    Changed config database
    Former-commit-id: 894805a4dc47087bddacfc84e448e1cecd809bd5
    Former-commit-id: 3a9591d9ce3065429493dc19825fe98a7a037457

--- config/database.php
+++ config/database.php
@@ -1,12 +1,12 @@
 <?php
 
 
-if (!defined('RDS_HOSTNAME')) {
+/*if (!defined('RDS_HOSTNAME')) {
     define('RDS_HOSTNAME', $_SERVER['RDS_HOSTNAME']);
     define('RDS_USERNAME', $_SERVER['RDS_USERNAME']);
     define('RDS_PASSWORD', $_SERVER['RDS_PASSWORD']);
     define('RDS_DB_NAME', $_SERVER['RDS_DB_NAME']);
-}
+}*/
 
 
 
@@ -51,7 +51,7 @@ return [
             'prefix' => '',
         ],
 
-        'mysql' => [
+        /*'mysql' => [
             'driver' => 'mysql',
             'host' => RDS_HOSTNAME,
             'port' => env('DB_PORT', '3306'),
@@ -64,8 +64,8 @@ return [
             'prefix' => '',
             'strict' => false,
             'engine' => null,
-        ],
-/*
+        ],*/
+
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
@@ -79,7 +79,7 @@ return [
             'prefix' => '',
             'strict' => true,
             'engine' => null,
-        ],*/
+        ],
 
         'pgsql' => [
             'driver' => 'pgsql',
