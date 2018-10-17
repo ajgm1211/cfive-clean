70983ca13bd0353643b00e2d396a402d8a14d9a8
Julio Avila <javila3090@gmail.com>
2018-10-17 17:25:17 -0300

    Changed config database to prod variables
    Former-commit-id: 20aceed9f14257e752687aa262b867a6813dcbb5

--- config/database.php
+++ config/database.php
@@ -1,12 +1,12 @@
 <?php
 
 
-/*if (!defined('RDS_HOSTNAME')) {
+if (!defined('RDS_HOSTNAME')) {
     define('RDS_HOSTNAME', $_SERVER['RDS_HOSTNAME']);
     define('RDS_USERNAME', $_SERVER['RDS_USERNAME']);
     define('RDS_PASSWORD', $_SERVER['RDS_PASSWORD']);
     define('RDS_DB_NAME', $_SERVER['RDS_DB_NAME']);
-}*/
+}
 
 
 
@@ -51,7 +51,7 @@ return [
             'prefix' => '',
         ],
 
-        /*'mysql' => [
+        'mysql' => [
             'driver' => 'mysql',
             'host' => RDS_HOSTNAME,
             'port' => env('DB_PORT', '3306'),
@@ -64,9 +64,9 @@ return [
             'prefix' => '',
             'strict' => false,
             'engine' => null,
-        ],*/
+        ],
 
-        'mysql' => [
+        /*'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
@@ -79,7 +79,7 @@ return [
             'prefix' => '',
             'strict' => true,
             'engine' => null,
-        ],
+        ],*/
 
         'pgsql' => [
             'driver' => 'pgsql',
