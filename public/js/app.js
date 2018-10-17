05e0040dbf6a8676dc6efcff6f7465ff7100fcab
Alejandro G <ale1211@gmail.com>
2018-10-17 11:38:10 -0400

    pusher production
    Former-commit-id: 22a451925e7703395cc7aa1ad7bc42d295c097c3
    Former-commit-id: 593b9307dff6266e3925340469a0d6ca1a475abd

--- public/js/app.js
+++ public/js/app.js
@@ -13967,7 +13967,7 @@ if (token) {
 window.Pusher = __webpack_require__(37);
 window.Echo = new __WEBPACK_IMPORTED_MODULE_0_laravel_echo___default.a({
   broadcaster: 'pusher',
-  key: "7b30149b695b6cf5cbb0",
+  key: "1052abebf4043210e7f1",
   cluster: "us2",
   encrypted: true
 });
