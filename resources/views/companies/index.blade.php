9b3de2c987d5332aa29abfffe99d40696de72c04
Julio Avila <javila3090@gmail.com>
2018-10-17 16:59:32 -0300

    Fixed issue with add companies modal
    Former-commit-id: b5e3d84ae4ab4c7bfbaf3ab4dd1c692469863b19

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -183,47 +183,11 @@ All
       </table>
     </div>
   </div>
-  <div class="modal fade bd-example-modal-lg" id="modalupload"   role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLongTitle">
-            Upload Companies
-          </h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">
-              &times;
-            </span>
-          </button>
-        </div>
-        <div id="edit-modal-body" class="modal-body">
-          {!! Form::open(['route' => 'Upload.Company', 'method' => 'POST', 'files' => 'true'])!!}
 
-          <div class="form-group row pull-right">
-            <div class="col-md-3 ">
-              
-            </div>
-          </div>
-          <div class="form-group row ">
-            <div class="col-md-1 "></div>
-            <div class="col-md-6 ">
-              <input type="file" name="file" value="Load File" required />
-            </div>
-          </div>
-        </div>
-        <div id="edit-modal-body" class="modal-footer">
-          {!! Form::submit('Load', ['class'=> 'btn btn-primary']) !!}
-          <button class="btn btn-success" type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">Cancel</span>
-          </button>
-        </div>
-        {!! Form::close() !!}
-      </div>
-    </div>
-  </div>
 </div>
 @include('companies.partials.companiesModal')
 @include('companies.partials.deleteCompaniesModal')
+@include('companies.partials.uploadCompanies')
 @endsection
 
 @section('js')
