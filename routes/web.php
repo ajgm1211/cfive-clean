66973efa60428fcb9c98b2586d07bf61a29ec5fc
Alejandro G <ale1211@gmail.com>
2018-10-17 12:44:06 -0400

    se resuelve nombre de rutas
    Former-commit-id: ccc8b45c21e2bbced514c768e6bef34e487fb80f

--- routes/web.php
+++ routes/web.php
@@ -206,7 +206,7 @@ Route::resource('prices', 'PriceController')->middleware('auth');
 Route::middleware(['auth'])->prefix('contacts')->group(function () {
     Route::get('add', 'ContactController@add')->name('contacts.add');
     Route::get('addCM', 'ContactController@addWithModal')->name('contacts.addCM'); // with modal
-    Route::get('addCM', 'ContactController@addWithModalManualQuote')->name('contacts.addCMMQ'); // with modal in manual quote
+    Route::get('addCMMQ', 'ContactController@addWithModalManualQuote')->name('contacts.addCMMQ'); // with modal in manual quote
     Route::get('delete/{contact_id}', 'ContactController@destroy')->name('contacts.delete');
 });
 Route::resource('contacts', 'ContactController')->middleware('auth');
