f965667ec14dae3b036575722b190886ff8d5068
Luis Blanco <luedblra@gmail.com>
2018-11-07 15:47:25 -0400

    Se modifica tablas con thead dinamico, se agrega lista para desloguear, se cambia nombre en import contract, se quita scrollx en quote.listado
    Former-commit-id: d27213e890c9051e9dc6aa1e45ca65c7a3385aee
    Former-commit-id: 144f67aa50f75a28a23095c8ba892b92addbedd3

--- resources/views/quotes/index.blade.php
+++ resources/views/quotes/index.blade.php
@@ -10,133 +10,133 @@
 @endsection
 @section('content')
 <div class="m-content">
-    <div class="m-portlet m-portlet--mobile">
-        @if(Session::has('message.nivel'))
-        <div class="col-md-12">
-            <br>
-            <div class="m-alert m-alert--icon m-alert--outline alert alert-{{ session('message.nivel') }} alert-dismissible fade show" role="alert">
-                <div class="m-alert__icon">
-                    <i class="la la-warning"></i>
-                </div>
-                <div class="m-alert__text">
-                    <strong>
-                        {{ session('message.title') }}
-                    </strong>
-                    {{ session('message.content') }}
-                </div>
-                <div class="m-alert__close">
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"></button>
-                </div>
+   <div class="m-portlet m-portlet--mobile">
+      @if(Session::has('message.nivel'))
+      <div class="col-md-12">
+         <br>
+         <div class="m-alert m-alert--icon m-alert--outline alert alert-{{ session('message.nivel') }} alert-dismissible fade show" role="alert">
+            <div class="m-alert__icon">
+               <i class="la la-warning"></i>
             </div>
-        </div>
-        @endif
-        <div class="m-portlet__body">
-            <!--begin: Search Form -->
-            <div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
-                <div class="row align-items-center">
-                    <div class="col-xl-6 order-2 order-xl-1">
-                        <div class="form-group m-form__group row align-items-center">
-                            <div class="col-md-4">
-                                <div class="m-input-icon m-input-icon--left">
-                                    <input type="text" class="form-control m-input" placeholder="Search..." id="generalSearch">
-                                    <span class="m-input-icon__icon m-input-icon__icon--left">
-                                        <span>
-                                            <i class="la la-search"></i>
-                                        </span>
-                                    </span>
-                                </div>
-                            </div>
+            <div class="m-alert__text">
+               <strong>
+                  {{ session('message.title') }}
+               </strong>
+               {{ session('message.content') }}
+            </div>
+            <div class="m-alert__close">
+               <button type="button" class="close" data-dismiss="alert" aria-label="Close"></button>
+            </div>
+         </div>
+      </div>
+      @endif
+      <div class="m-portlet__body">
+         <!--begin: Search Form -->
+         <div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
+            <div class="row align-items-center">
+               <div class="col-xl-6 order-2 order-xl-1">
+                  <div class="form-group m-form__group row align-items-center">
+                     <div class="col-md-4">
+                        <div class="m-input-icon m-input-icon--left">
+                           <input type="text" class="form-control m-input" placeholder="Search..." id="generalSearch">
+                           <span class="m-input-icon__icon m-input-icon__icon--left">
+                              <span>
+                                 <i class="la la-search"></i>
+                              </span>
+                           </span>
                         </div>
-                    </div>
-                    <div class="col-xl-6 order-1 order-xl-2 m--align-right">
-                        <a href="{{route('quotes.automatic')}}" class="btn btn-primary m-btn m-btn--custom m-btn--icon m-btn--air m-btn--pill">
-                            <span>
-                                <span>
-                                    Search Rates
-                                </span>
-                                <i class="la la-plus"></i>
-                            </span>
-                        </a>
-                        <a href="{{route('quotes.create')}}" class="btn btn-primary m-btn m-btn--custom m-btn--icon m-btn--air m-btn--pill">
-                            <span>
-                                <span>
-                                    Manual Quote
-                                </span>
-                                <i class="la la-plus"></i>
-                            </span>
-                        </a>
-                        <div class="m-separator m-separator--dashed d-xl-none"></div>
-                    </div>
-                </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-xl-6 order-1 order-xl-2 m--align-right">
+                  <a href="{{route('quotes.automatic')}}" class="btn btn-primary m-btn m-btn--custom m-btn--icon m-btn--air m-btn--pill">
+                     <span>
+                        <span>
+                           Search Rates
+                        </span>
+                        <i class="la la-plus"></i>
+                     </span>
+                  </a>
+                  <a href="{{route('quotes.create')}}" class="btn btn-primary m-btn m-btn--custom m-btn--icon m-btn--air m-btn--pill">
+                     <span>
+                        <span>
+                           Manual Quote
+                        </span>
+                        <i class="la la-plus"></i>
+                     </span>
+                  </a>
+                  <div class="m-separator m-separator--dashed d-xl-none"></div>
+               </div>
             </div>
-            <table class="table tableData" id="tablequote"  width="100%">
-                <thead>
-                    <tr class="title-quote">
-                        <th title="Status">
-                            Status
-                        </th>
-                        <th title="id">
-                            Id
-                        </th>
-                        <th title="Client">
-                            Client
-                        </th>
-                        <th title="Created">
-                            Created
-                        </th>
-                        <th title="Owner">
-                            Owner
-                        </th>
-                        <th title="Origin">
-                            Origin
-                        </th>
-                        <th title="Destination">
-                            Destination
-                        </th>
-                        <th title="Ammount">
-                            Ammount
-                        </th>
-                        <th title="Markup">
-                            Markup
-                        </th>
-                        <th title="Type">
-                            Type
-                        </th>
-                        <th title="Options">
-                            Options
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
+         </div>
+         <table class="table tableData" id="tablequote" width="100%">
+            <thead >
+               <tr class="title-quote">
+                  <th title="Status">
+                     Status
+                  </th>
+                  <th title="id">
+                     Id
+                  </th>
+                  <th title="Client">
+                     Client
+                  </th>
+                  <th title="Created">
+                     Created
+                  </th>
+                  <th title="Owner">
+                     Owner
+                  </th>
+                  <th title="Origin">
+                     Origin
+                  </th>
+                  <th title="Destination">
+                     Destination
+                  </th>
+                  <th title="Ammount">
+                     Ammount
+                  </th>
+                  <th title="Markup">
+                     Markup
+                  </th>
+                  <th title="Type">
+                     Type
+                  </th>
+                  <th title="Options">
+                     Options
+                  </th>
+               </tr>
+            </thead>
+            <tbody>
 
-                </tbody>
-            </table>
-            <div class="modal fade" id="change_status_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLongTitle">
-                                Change status
-                            </h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">
-                                    &times;
-                                </span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
+            </tbody>
+         </table>
+         <div class="modal fade" id="change_status_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+               <div class="modal-content">
+                  <div class="modal-header">
+                     <h5 class="modal-title" id="exampleModalLongTitle">
+                        Change status
+                     </h5>
+                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">
+                           &times;
+                        </span>
+                     </button>
+                  </div>
+                  <div class="modal-body">
 
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-danger" data-dismiss="modal">
-                                Close
-                            </button>
-                        </div>
-                    </div>
-                </div>
+                  </div>
+                  <div class="modal-footer">
+                     <button type="button" class="btn btn-danger" data-dismiss="modal">
+                        Close
+                     </button>
+                  </div>
+               </div>
             </div>
-        </div>
-    </div>
+         </div>
+      </div>
+   </div>
 </div>
 @include('quotes.partials.quotesModal');
 @endsection
@@ -149,83 +149,89 @@
 <script src="/assets/demo/default/custom/components/forms/widgets/select2.js" type="text/javascript"></script>
 <script src="{{asset('js/base.js')}}" type="text/javascript"></script>
 <script>
-    function AbrirModal(action,id){
-        if(action == "edit"){
-            var url = '{{ route("quotes.edit", ":id") }}';
-            url = url.replace(':id', id);
-            $('.modal-body').load(url,function(){
-                $('#priceModal').modal({show:true});
-            });
-        }
-        if(action == "delete"){
-            var url = '{{ route("quotes.destroy", ":id") }}';
-            url = url.replace(':id', id);
-            $('.modal-body').load(url,function(){
-                $('#deletePriceModal').modal({show:true});
-            });
-        }
-        if(action == "change_status"){
-            var url = '{{ route("quotes.change_status", ":id") }}';
-            url = url.replace(':id', id);
-            $('.modal-body').load(url,function(){
-                $('#change_status_modal').modal({show:true});
-            });
-        }
-    }
+   function AbrirModal(action,id){
+      if(action == "edit"){
+         var url = '{{ route("quotes.edit", ":id") }}';
+         url = url.replace(':id', id);
+         $('.modal-body').load(url,function(){
+            $('#priceModal').modal({show:true});
+         });
+      }
+      if(action == "delete"){
+         var url = '{{ route("quotes.destroy", ":id") }}';
+         url = url.replace(':id', id);
+         $('.modal-body').load(url,function(){
+            $('#deletePriceModal').modal({show:true});
+         });
+      }
+      if(action == "change_status"){
+         var url = '{{ route("quotes.change_status", ":id") }}';
+         url = url.replace(':id', id);
+         $('.modal-body').load(url,function(){
+            $('#change_status_modal').modal({show:true});
+         });
+      }
+   }
 
-    $(document).ready(function() {
-        $('#select-origin--2').select2();
-        $('#select-destination--2').select2();
-    });
-
-    $(function() {
-
-        $('#tablequote').DataTable({
-            ordering: true,
-            searching: true,
-            processing: true,
-            serverSide: true,
-            order: [[ 3, "asc" ],[ 4, "asc" ]],
-            ajax:  "{{ route('quotes.index.datatable') }}",
-            columns: [
-
-                {data: 'statusC', name: 'statusC'},
-                {data: 'comquotes', name: 'comquotes'},
-                {data: 'client', name: 'client'},
-                {data: 'created', name: 'created'},
-                {data: 'owner', name: 'owner'},
-                {data: 'origin', name: 'origin'},
-                {data: 'destination', name: 'destination'},
-                {data: 'ammount', name: 'ammount'},
-                {data: 'markup', name: 'markup'},
-                {data: 'type', name: 'type'},
-                {data: 'action', name: 'action', orderable: false, searchable: false },
-            ] ,
-
-            buttons: [
-                {
-                    extend: 'copyHtml5',
-                    exportOptions: {
-                        columns: [0, 1, 2, 3]
-                    }
-                },
-                {
-                    extend: 'excelHtml5',
-                    exportOptions: {
-                        columns: [0, 1, 2, 3]
-                    }
-                },
-                {
-                    extend: 'pdfHtml5',
-                    exportOptions: {
-                        columns: [0, 1, 2, 3]
-                    }
-                }
-            ]
+   $(document).ready(function() {
+      $('#select-origin--2').select2();
+      $('#select-destination--2').select2();
+   });
 
 
-        });
-    });
+   $(function() {
+
+      
+      
+      $('#tablequote').DataTable({
+         ordering: true,
+         searching: true,
+         processing: true,
+         serverSide: true,
+         order: [[ 3, "asc" ],[ 4, "asc" ]],
+         ajax:  "{{ route('quotes.index.datatable') }}",
+         columns: [
+
+            {data: 'statusC', name: 'statusC'},
+            {data: 'comquotes', name: 'comquotes'},
+            {data: 'client', name: 'client'},
+            {data: 'created', name: 'created'},
+            {data: 'owner', name: 'owner'},
+            {data: 'origin', name: 'origin'},
+            {data: 'destination', name: 'destination'},
+            {data: 'ammount', name: 'ammount'},
+            {data: 'markup', name: 'markup'},
+            {data: 'type', name: 'type'},
+            {data: 'action', name: 'action', orderable: false, searchable: false },
+         ] ,
+         "autoWidth": true,
+        
+         "ordering": true,
+         "paging":         true,
+         buttons: [
+            {
+               extend: 'copyHtml5',
+               exportOptions: {
+                  columns: [0, 1, 2, 3]
+               }
+            },
+            {
+               extend: 'excelHtml5',
+               exportOptions: {
+                  columns: [0, 1, 2, 3]
+               }
+            },
+            {
+               extend: 'pdfHtml5',
+               exportOptions: {
+                  columns: [0, 1, 2, 3]
+               }
+            }
+         ]
+
+
+      });
+   });
 
 </script>
 @stop
