eef62f81e8edb66a220837d7dd34d090650e6d9a
Alejandro G <ale1211@gmail.com>
2018-11-19 15:07:11 -0400

    modelo y controlador rate LCL
    Former-commit-id: 0dffd56632fb1201276ff651300d445a7384d39c

--- app/ContractLcl.php
+++ app/ContractLcl.php
@@ -9,9 +9,15 @@ class ContractLcl extends Model implements Auditable
 {
   use \OwenIt\Auditing\Auditable;
   protected $table    = "contracts_lcl";
+  protected $fillable = ['id', 'name','number','company_user_id','validity','expire','status'];
 
   public function companyUser()
   {
     return $this->belongsTo('App\CompanyUser');
+  }
+
+  public function user()
+  {
+    return $this->belongsTo('App\User');
   }
 }
