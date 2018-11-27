a09f2196463b1b43293176022814d1315bb131f2
Alejandro G <ale1211@gmail.com>
2018-11-27 10:47:53 -0400

    global charges LCL
    Former-commit-id: 47462324aa80ec0a74edeb6bca53ba38ae58343c
    Former-commit-id: b5c207e74064d4d2b500990f8141f2238e9b662f

---
+++ app/GlobalCharCarrierLcl.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class GlobalCharCarrierLcl extends Model
+{
+  protected $table    = "globalcharcarriers_lcl";
+  protected $fillable =   ['carrier_id','globalchargelcl_id'];
+  public $timestamps = false;
+  public function globchargelcl()
+  {
+
+    return $this->belongsTo('App\GlobalChargeLcl');
+  }
+  public function carrier(){
+
+    return $this->belongsTo('App\Carrier');
+
+  }
+}
