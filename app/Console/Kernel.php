8ee17374f9165a4fbd806287cd4bc7f8669c1536
Alejandro G <ale1211@gmail.com>
2018-11-29 10:17:57 -0400

    cambios a servidor aws
    Former-commit-id: df5d9f87c0af9f54b30cda580fb2b0027d734f6e

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -32,9 +32,9 @@ class Kernel extends ConsoleKernel
     $schedule->command('command:updateCurrenciesEur')
       ->twiceDaily(6, 14)->appendOutputTo(storage_path('logs/commands.log'));
     $schedule->exec('php /var/www/html/artisan queue:work --timeout=3600 --tries=7 &')->withoutOverlapping();
-   // Comandos para backups 
-    $schedule->command('backup:clean')->daily()->at('01:40');
-    $schedule->command('backup:run')->daily()->at('02:00');
+    // Comandos para backups 
+    // $schedule->command('backup:clean')->daily()->at('01:40');
+    //$schedule->command('backup:run')->daily()->at('02:00');
   }
 
   /**
