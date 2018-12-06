cf809648b23309bc33249b05e5b292af895208ea
Julio Avila <javila3090@gmai.com>
2018-12-06 13:02:15 -0300

    Modified errors pages
    Former-commit-id: 677bcb28707104325213154b632bdf163d8d7a57
    Former-commit-id: a6d4758c2aa8e367c9e17de2f396ad363c4683a8

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Error 500</title>
+    <title>Error 404</title>
 
     <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
@@ -43,9 +43,8 @@
     <div class="content">
         <img class="img" src="{{asset('images/logo-icon.png')}}"/>
         <div class="title">
-            <h1 class="size-60">500</h1>
-            <p><b>An error has occurred while we were processing your request, please try again.</b></p>
-            <p><b>If it persists, contact the site administrator.</b></p>
+            <h1 class="size-60">404</h1>
+            <p><b>The page you are looking for does not exist.</b></p>
         </div>
     </div>
 </div>
--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -13,7 +13,7 @@
             margin: 0;
             padding: 0;
             width: 100%;
-            color: #B0BEC5;
+            /*color: #B0BEC5;*/
             display: table;
             font-weight: 100;
             font-family: 'Lato', sans-serif;
