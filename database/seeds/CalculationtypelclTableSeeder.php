57b82613d40fcf8810208e4188836f77b42070cc
Alejandro G <ale1211@gmail.com>
2018-12-18 14:25:04 -0400

    lcl seeders
    Former-commit-id: fddd8786d315270e7606f9d167caf553904df00b
    Former-commit-id: eacb9107440ddbe1a64e5ee6365202b34613f9ad

--- database/seeds/CalculationtypelclTableSeeder.php
+++ database/seeds/CalculationtypelclTableSeeder.php
@@ -4,40 +4,77 @@ use Illuminate\Database\Seeder;
 
 class CalculationtypelclTableSeeder extends Seeder
 {
-  /**
-     * Run the database seeds.
+
+    /**
+     * Auto generated seed file
      *
      * @return void
      */
-  public function run()
-  {
-    \DB::table('calculationtypelcl')->delete();
+    public function run()
+    {
+        
 
-    \DB::table('calculationtypelcl')->insert(array (
-      0 => 
-      array (
-        'id' => 1,
-        'name' => 'W/M',
-        'code' => 'W/M',
-        'created_at' => NULL,
-        'updated_at' => NULL,
-      ),
-      1 => 
-      array (
-        'id' => 2,
-        'name' => 'Per Shipment',
-        'code' => 'SHIP',
-        'created_at' => NULL,
-        'updated_at' => NULL,
-      ),
-      2 => 
-      array (
-        'id' => 3,
-        'name' => 'Per BL',
-        'code' => 'BL',
-        'created_at' => NULL,
-        'updated_at' => NULL,
-      ),
-    ));
-  }
+        \DB::table('calculationtypelcl')->delete();
+        
+        \DB::table('calculationtypelcl')->insert(array (
+            0 => 
+            array (
+                'id' => 1,
+                'name' => 'Per HBL',
+                'code' => 'HBL',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            1 => 
+            array (
+                'id' => 2,
+                'name' => 'Per Shipment',
+                'code' => 'SHIP',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            2 => 
+            array (
+                'id' => 3,
+                'name' => 'Per BL',
+                'code' => 'BL',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            3 => 
+            array (
+                'id' => 4,
+                'name' => 'TON/M3',
+                'code' => 'TON/M3',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            4 => 
+            array (
+                'id' => 5,
+                'name' => 'PER TON',
+                'code' => 'TON',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            5 => 
+            array (
+                'id' => 6,
+            'name' => 'PER TON (TON o M3)',
+                'code' => 'TON-M3',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            6 => 
+            array (
+                'id' => 7,
+            'name' => 'PER M3 (TON o M3)',
+                'code' => 'M3-TON',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+        ));
+        
+        
+    }
 }
