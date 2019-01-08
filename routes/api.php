44042b211827117926c96e52792e47bbc0f7214e
Julio Avila <javila3090@gmai.com>
2019-01-07 22:03:47 -0300

    Working on Api Rest
    Former-commit-id: 642987d61105633e5998c03092e75a0c94d38a18

--- routes/api.php
+++ routes/api.php
@@ -17,10 +17,24 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::middleware('auth:api')->get('/v1/quotes','QuoteController@index');
+
 Route::get('currency/{value}', function($value) {
     return \App\Currency::find($value);
 });
 
 Route::get('airports/', function() {
     return \App\Airport::All();
+});
+
+Route::group(['prefix' => 'v1'], function () {
+    Route::post('login', 'AuthApiController@login');
+    Route::post('signup', 'AuthController@signup');
+    Route::get('test', 'ApiController@test');
+
+    Route::group(['middleware' => 'auth:api'], function() {
+        Route::get('logout', 'AuthApiController@logout');
+        Route::get('user', 'AuthApiController@user');
+        Route::get('quotes', 'QuoteController@index');
+    });
 });
