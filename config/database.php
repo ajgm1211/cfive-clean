3707a71340ca3d90929d29a18b81073aca77b655
Alejandro G <ale1211@gmail.com>
2019-01-14 10:50:09 -0400

    configuracion https
    Former-commit-id: f0418d0df0d54dff7a783a516e27748db6d8df1d
    Former-commit-id: 0863f5b7401761a841c6e4a69f745433a1a185f0

--- config/database.php
+++ config/database.php
@@ -1,12 +1,12 @@
 <?php
 
 
-/*if (!defined('RDS_HOSTNAME')) {
+if (!defined('RDS_HOSTNAME')) {
     define('RDS_HOSTNAME', $_SERVER['RDS_HOSTNAME']);
     define('RDS_USERNAME', $_SERVER['RDS_USERNAME']);
     define('RDS_PASSWORD', $_SERVER['RDS_PASSWORD']);
     define('RDS_DB_NAME', $_SERVER['RDS_DB_NAME']);
-}*/
+}
 
 return [
 
@@ -47,7 +47,7 @@ return [
             'prefix' => '',
         ],
 
-        /*'mysql' => [
+        'mysql' => [
             'driver' => 'mysql',
             'host' => RDS_HOSTNAME,
             'port' => env('DB_PORT', '3306'),
@@ -60,8 +60,8 @@ return [
             'prefix' => '',
             'strict' => false,
             'engine' => null,
-        ],*/
-
+        ],
+/*
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
@@ -75,7 +75,7 @@ return [
             'prefix' => '',
             'strict' => true,
             'engine' => null,
-        ],
+        ],*/
 
         'pgsql' => [
             'driver' => 'pgsql',
