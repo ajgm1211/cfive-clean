085a190f3f908677282b95ba47756daef87da33c
Alejandro G <ale1211@gmail.com>
2019-01-30 12:00:42 -0400

    Pipeline
    Former-commit-id: ff78c5af476bdfd28e062bea610b8478abad248d

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,7 +10,7 @@
                                 <div class="m-login__signin">
                                     <div class="m-login__head">
                                         <h3 class="m-login__title">
-                                            Welcome to CargofivE
+                                            Welcome to Cargofive
                                         </h3>
                                     </div>
                                     @if (session('status'))
