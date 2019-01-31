8c516991965de3c97bebc117899e0c75166ebb93
Alejandro G <ale1211@gmail.com>
2019-01-31 09:44:13 -0400

    Welcome to cargofive
    Former-commit-id: 8cc5638a22b6fd7f49c0a4217b799d9e78d43339
    Former-commit-id: 7131f88408a9d2ad3e34420f79a95079bffa58c8

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,7 +10,7 @@
                                 <div class="m-login__signin">
                                     <div class="m-login__head">
                                         <h3 class="m-login__title">
-                                            Welcome to  Nightmare
+                                            Welcome to Cargofive
                                         </h3>
                                     </div>
                                     @if (session('status'))
