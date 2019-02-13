d39ae3f513b70acfeed644b98e85f881e3a6e76c
Julio Avila <javila3090@gmai.com>
2019-02-13 18:03:01 -0300

    Finished schedules modifications
    Former-commit-id: 4c90501470d01c674c063be4edcccded8ef6a26e
    Former-commit-id: 8adb32f40b0dfeb67f3578abf9d8372993f6ccac

--- app/Schedule.php
+++ app/Schedule.php
@@ -7,11 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Schedule extends Model
 {
   protected $table    = "schedules";
-  protected $fillable =['id','vessel','etd','transit_time','type','eta','quotes_id'];
+  protected $fillable =['id','vessel','etd','transit_time','type','eta','quote_id'];
   
   public function quotes()
   {
-    return $this->belongsTo('App\Quote');
+    return $this->belongsTo('App\Quote','quote_id','id');
   }
 
 }
