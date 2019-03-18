d17082ad8728985b73983bc90ffaa1821af263c6
Julio Avila <javila3090@gmai.com>
2019-03-18 16:57:27 -0300

    Added Quote V2 Controller and Model
    Former-commit-id: 13029c8d735b31d42ee7dae1b278b626244c6aee

---
+++ database/migrations/2019_03_18_184936_create_quote_v2s_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuoteV2sTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quote_v2s', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('quote_id');
+            $table->integer('custom_quote_id');
+            $table->enum('type',['LCL','FCL']);
+            $table->string('delivery_type');
+            $table->longText('equipment');
+            $table->integer('company_id')->unsigned();
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
+            $table->integer('contact_id')->unsigned();
+            $table->foreign('contact_id')->references('id')->on('contacts')->onDelete('cascade');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('price_id')->unsigned();
+            $table->foreign('price_id')->references('id')->on('prices')->onDelete('cascade');
+            $table->integer('origin_port_id')->unsigned()->nullable();
+            $table->foreign('origin_port_id')->references('id')->on('harbors');
+            $table->integer('destination_port_id')->unsigned()->nullable();
+            $table->foreign('destination_port_id')->references('id')->on('harbors');
+            $table->string('origin_address')->nullable();
+            $table->string('destination_address')->nullable();
+            $table->date('quote_validity');
+            $table->integer('currency_id')->unsigned();
+            $table->foreign('currency_id')->references('id')->on('currency')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quote_v2s');
+    }
+}
