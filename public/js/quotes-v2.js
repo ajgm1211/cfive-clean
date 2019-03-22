02a2345fa17670bca42548098b8aa56618d9d81a
Julio Avila <javila3090@gmai.com>
2019-03-22 13:08:15 -0300

    Working on quotes v2
    Former-commit-id: 370a628ecc27f16868dd8fe1c7f944f5d17d6723

--- public/js/quotes-v2.js
+++ public/js/quotes-v2.js
@@ -38,3 +38,43 @@ $(document).ready(function() {
         }
     });
 });
+
+$(document).on('click', '#edit-quote', function () {
+    $(".quote_id_span").attr('hidden','true');
+    $(".company_span").attr('hidden','true');
+    $(".status_span").attr('hidden','true');
+    $(".delivery_type_span").attr('hidden','true');
+    $(".price_level_span").attr('hidden','true');
+    $(".type_span").attr('hidden','true');
+    $(".incoterm_id_span").attr('hidden','true');
+    $(".contact_id_span").attr('hidden','true');
+    $(".quote_id").removeAttr('hidden');
+    $(".company_id").removeAttr('hidden');
+    $(".type").removeAttr('hidden');
+    $(".status").removeAttr('hidden');
+    $(".delivery_type").removeAttr('hidden');
+    $(".incoterm_id").removeAttr('hidden');
+    $(".contact_id").removeAttr('hidden');
+    $("#update_buttons").removeAttr('hidden');
+    $("#edit_li").attr('hidden','true');
+});
+
+$(document).on('click', '#cancel', function () {
+    $(".quote_id_span").removeAttr('hidden');
+    $(".company_span").removeAttr('hidden');
+    $(".status_span").removeAttr('hidden');
+    $(".delivery_type_span").removeAttr('hidden');
+    $(".price_level_span").removeAttr('hidden');
+    $(".type_span").removeAttr('hidden');
+    $(".incoterm_id_span").removeAttr('hidden');
+    $(".contact_id_span").removeAttr('hidden');
+    $(".quote_id").attr('hidden','true');
+    $(".company_id").attr('hidden','true');
+    $(".type").attr('hidden','true');
+    $(".status").attr('hidden','true');
+    $(".delivery_type").attr('hidden','true');
+    $(".incoterm_id").attr('hidden','true');
+    $(".contact_id").attr('hidden','true');
+    $("#update_buttons").attr('hidden','true');
+    $("#edit_li").removeAttr('hidden');
+});
