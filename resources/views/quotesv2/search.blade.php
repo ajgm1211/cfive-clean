4ab9b89e2690963594d06492bb70d056b722905d
Alejandro G <ale1211@gmail.com>
2019-04-01 16:19:54 -0400

    contenedores unidos por surcharges en freight
    Former-commit-id: 2aea8bed64c4a037526e443980f1c0d7611565ed

--- resources/views/quotesv2/search.blade.php
+++ resources/views/quotesv2/search.blade.php
@@ -211,10 +211,10 @@
                   <span class="darkblue cabezeras">Freight</span><br><br>
                 </div>
                 <div class="row bg-light">
-                  <div class="col-lg-2"><span class="portalphacode">Charge</span></div>
-                  <div class="col-lg-2"><span class="portalphacode">Detail</span></div>
-                  <div class="col-lg-2" {{ $equipmentHides['20'] }}><span class="portalphacode">20'</span></div>
-                  <div class="col-lg-2" {{ $equipmentHides['40'] }}><span class="portalphacode">40'</span></div>
+                  <div class="col-lg-3"><span class="portalphacode">Charge</span></div>
+                  <div class="col-lg-3"><span class="portalphacode">Detail</span></div>
+                  <div class="col-lg-1" {{ $equipmentHides['20'] }}><span class="portalphacode">20'</span></div>
+                  <div class="col-lg-1" {{ $equipmentHides['40'] }}><span class="portalphacode">40'</span></div>
                   <div class="col-lg-1" {{ $equipmentHides['40hc'] }}><span class="portalphacode">40HC'</span></div>
                   <div class="col-lg-1"  {{ $equipmentHides['40nor'] }}><span class="portalphacode">40NOR'</span></div>
                   <div class="col-lg-1" {{ $equipmentHides['45'] }}><span class="portalphacode">45'</span></div>
@@ -222,10 +222,10 @@
                 </div><br>
                 @foreach($arr->rates as $rates)
                 <div class="row">
-                  <div class="col-lg-2">{{ $rates['type'] }}</div>
-                  <div class="col-lg-2">{{ $rates['detail'] }}</div>
-                  <div class="col-lg-2" {{ $equipmentHides['20'] }}>{{ @$rates['price20'] }} + {{ @$rates['markup20'] }} <i class="la la-caret-right"></i> {{  @$rates['monto20'] }}  </div>
-                  <div class="col-lg-2" {{ $equipmentHides['40'] }}>{{ @$rates['price40'] }} + {{ @$rates['markup40'] }} <i class="la la-caret-right"></i> {{  @$rates['monto40'] }}  </div>
+                  <div class="col-lg-3">{{ $rates['type'] }}</div>
+                  <div class="col-lg-3">{{ $rates['detail'] }}</div>
+                  <div class="col-lg-1" {{ $equipmentHides['20'] }}>{{ @$rates['price20'] }} + {{ @$rates['markup20'] }} <i class="la la-caret-right"></i> {{  @$rates['monto20'] }}  </div>
+                  <div class="col-lg-1" {{ $equipmentHides['40'] }}>{{ @$rates['price40'] }} + {{ @$rates['markup40'] }} <i class="la la-caret-right"></i> {{  @$rates['monto40'] }}  </div>
                   <div class="col-lg-1" {{ $equipmentHides['40hc'] }}>{{ @$rates['price40hc'] }} + {{ @$rates['markup40hc'] }} <i class="la la-caret-right"></i> {{  @$rates['monto40hc'] }}  </div>
                   <div class="col-lg-1" {{ $equipmentHides['40nor'] }}>{{ @$rates['price40nor'] }} + {{ @$rates['markup40nor'] }} <i class="la la-caret-right"></i> {{  @$rates['monto40nor'] }}  </div>
                   <div class="col-lg-1" {{ $equipmentHides['45'] }}>{{ @$rates['price45'] }} + {{ @$rates['markup45'] }} <i class="la la-caret-right"></i> {{  @$rates['monto45'] }}  </div>
@@ -235,17 +235,23 @@
                 @foreach($arr->localfreight as $localfreight)
 
                 <div class="row">
-                  <div class="col-lg-2">{{  str_replace(["[","]","\""], ' ', $localfreight['99']->pluck('surcharge_name')  ) }}</div>
-                  <div class="col-lg-2">{{  str_replace(["[","]","\""], ' ', $localfreight['99']->pluck('calculation_name')  ) }}</div>
-                  <div class="col-lg-2">
+                  <div class="col-lg-3">{{  str_replace(["[","]","\""], ' ', $localfreight['99']->pluck('surcharge_name')  ) }}</div>
+                  <div class="col-lg-3">{{  str_replace(["[","]","\""], ' ', $localfreight['99']->pluck('calculation_name')  ) }}</div>
+                  <div class="col-lg-1">
                     {{ isset($localfreight['20']) ?   str_replace(["[","]","\""], ' ', $localfreight['20']->pluck('monto')) : '0.00' }}  + {{ isset($localfreight['20']) ?   str_replace(["[","]","\""], ' ', $localfreight['20']->pluck('markup')) : '0.00' }}  <i class="la la-caret-right"></i>    {{ isset($localfreight['20']) ?   str_replace(["[","]","\""], ' ', $localfreight['20']->pluck('montoMarkup')) : '0.00' }}          
                   </div>      
-                  <div class="col-lg-2">
+                  <div class="col-lg-1">
                     {{ isset($localfreight['40']) ?  str_replace(["[","]","\""], ' ', $localfreight['40']->pluck('monto')) :'0.00' }} + {{ isset($localfreight['40']) ?   str_replace(["[","]","\""], ' ', $localfreight['40']->pluck('markup')) : '0.00' }}     <i class="la la-caret-right"></i>      {{ isset($localfreight['40']) ?   str_replace(["[","]","\""], ' ', $localfreight['40']->pluck('montoMarkup')) : '0.00' }}                  
                   </div>
-                  <div class="col-lg-1" {{ $equipmentHides['40hc'] }}><i class="la la-caret-right"></i>  </div>
-                  <div class="col-lg-1" {{ $equipmentHides['40nor'] }}> <i class="la la-caret-right"></i>   </div>
-                  <div class="col-lg-1" {{ $equipmentHides['45'] }}> <i class="la la-caret-right"></i></div>
+                  <div class="col-lg-1" {{ $equipmentHides['40hc'] }}>  
+                    {{ isset($localfreight['40hc']) ?  str_replace(["[","]","\""], ' ', $localfreight['40hc']->pluck('monto')) :'0.00' }} + {{ isset($localfreight['40hc']) ?   str_replace(["[","]","\""], ' ', $localfreight['40hc']->pluck('markup')) : '0.00' }}     <i class="la la-caret-right"></i>      {{ isset($localfreight['40hc']) ?   str_replace(["[","]","\""], ' ', $localfreight['40hc']->pluck('montoMarkup')) : '0.00' }}     
+                  </div>
+                  <div class="col-lg-1" {{ $equipmentHides['40nor'] }}>  
+                    {{ isset($localfreight['40nor']) ?  str_replace(["[","]","\""], ' ', $localfreight['40nor']->pluck('monto')) :'0.00' }} + {{ isset($localfreight['40nor']) ?   str_replace(["[","]","\""], ' ', $localfreight['40nor']->pluck('markup')) : '0.00' }}     <i class="la la-caret-right"></i>      {{ isset($localfreight['40nor']) ?   str_replace(["[","]","\""], ' ', $localfreight['40nor']->pluck('montoMarkup')) : '0.00' }}     
+                  </div>
+                  <div class="col-lg-1" {{ $equipmentHides['45'] }}>     
+                    {{ isset($localfreight['45']) ?  str_replace(["[","]","\""], ' ', $localfreight['45']->pluck('monto')) :'0.00' }} + {{ isset($localfreight['45']) ?   str_replace(["[","]","\""], ' ', $localfreight['45']->pluck('markup')) : '0.00' }}     <i class="la la-caret-right"></i>      {{ isset($localfreight['45']) ?   str_replace(["[","]","\""], ' ', $localfreight['45']->pluck('montoMarkup')) : '0.00' }}     
+                  </div>
                   <div class="col-lg-1" ><span class="">USD</span></div>
                   <div class="col-lg-1" ></div>
                 </div><br>
