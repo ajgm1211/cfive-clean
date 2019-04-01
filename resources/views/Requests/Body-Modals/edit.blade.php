a62551907d73c1c847e89cc1f7a1819e8d145946
Luis <luedblra@gmail.com>
2019-04-01 18:36:37 -0400

    se termina modulo para fcl
    Former-commit-id: 2d48b788a06c07ec002d25d4ba87821c907e76f4

--- resources/views/Requests/Body-Modals/edit.blade.php
+++ resources/views/Requests/Body-Modals/edit.blade.php
@@ -9,13 +9,33 @@
         <label for="number" class="form-control-label">
             Number:
         </label>
-        <input type="text" name="number" value="{{$requests->numbercontract}}" required="required" class="form-control" disabled id="NameMD">
+        <input type="text" name="number" value="{{$requests->numbercontract}}" required="required" class="form-control" disabled id="Number">
     </div>
     <div class="col-lg-4">
         <label for="number" class="form-control-label">
-            Number:
+            Company User:
         </label>
-        <input type="text" name="number" value="{{$requests->companyuser->name}}" required="required" class="form-control" disabled id="NameMD">
+        <input type="text" name="number" value="{{$requests->companyuser->name}}" required="required" class="form-control" disabled id="Company">
+    </div>
+</div>
+<div class="form-group row">
+    <div class="col-lg-4">
+        <label for="number" class="form-control-label">
+            Id:
+        </label>
+        <input type="text" name="number" value="{{$requests->id}}" required="required" class="form-control" disabled id="NumberMD">
+    </div>
+    <div class="col-lg-4">
+        <label for="number" class="form-control-label">
+            Date:
+        </label>
+        <input type="text" name="number" value="{{$requests->created}}" required="required" class="form-control" disabled id="NumberMD">
+    </div>
+    <div class="col-lg-4">
+        <label for="number" class="form-control-label">
+            Username Load:
+        </label>
+        <input type="text" name="number" value="{{$requests->username_load}}" required="required" class="form-control" disabled id="NumberMD">
     </div>
     <input type="hidden" id="idContract" value="{{$requests->id}}"/>
 </div>
