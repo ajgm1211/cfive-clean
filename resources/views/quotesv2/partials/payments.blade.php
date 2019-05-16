ad53a3a23fc2a1d7ba982f5e9435fd1a72bbd0a7
Julio Avila <javila3090@gmai.com>
2019-05-16 18:07:36 -0400

    Modified show quotes v2
    Former-commit-id: f800a3c2ca6bb259bfeea19533471c0cafb0cc79

---
+++ resources/views/quotesv2/partials/payments.blade.php
@@ -0,0 +1,40 @@
+<div class="row">
+            <div class="col-md-12">
+                <div class="m-portlet custom-portlet">
+                    <div class="m-portlet__head">
+                        <div class="row" style="padding-top: 20px;">
+                            <h3 class="title-quote size-14px">Payment conditions</h3>
+                        </div>
+                        <div class="m-portlet__head-tools">
+                            <ul class="nav nav-tabs m-tabs m-tabs-line m-tabs-line--right m-tabs-line-danger" role="tablist" style="border-bottom: none;">
+                                <li class="nav-item m-tabs__item" id="edit_li">
+                                    <a class="btn btn-primary-v2" id="edit-payments" data-toggle="tab" href="#m_portlet_tab_1_1" role="tab">
+                                        Edit&nbsp;&nbsp;<i class="fa fa-pencil"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="m-portlet__body">
+                        <div class="card card-body bg-light">
+                            <span class="payment_conditions_span">{!! $quote->payment_conditions !!}</span>
+                            <div class="payment_conditions_textarea" hidden>
+                                <textarea name="payment_conditions" class="form-control payment_conditions editor" id="payment_conditions">{!!$quote->payment_conditions!!}</textarea>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12 text-center" id="update_payments" hidden>
+                                    <br>
+                                    <a class="btn btn-danger" id="cancel-payments" data-toggle="tab" href="#m_portlet_tab_1_1" role="tab">
+                                        Cancel&nbsp;&nbsp;<i class="fa fa-close"></i>
+                                    </a>
+                                    <a class="btn btn-primary" id="update-payments" data-toggle="tab" href="#m_portlet_tab_1_1" role="tab">
+                                        Update &nbsp;&nbsp;<i class="fa fa-pencil"></i>
+                                    </a>
+                                    <br>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
