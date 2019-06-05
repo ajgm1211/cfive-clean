aec8fb4e67df28cce9ae5ba8ef554f78edaebba9
Alejandro G <ale1211@gmail.com>
2019-06-05 12:03:51 -0400

    se agrega a la cotizacion los nuevos campos transit , via , schedule y remarks

--- app/AutomaticRate.php
+++ app/AutomaticRate.php
@@ -23,7 +23,7 @@ class AutomaticRate extends Model
     ];
 
 
-    protected $fillable = ['quote_id','contract','validity_start','validity_end','origin_port_id','destination_port_id','carrier_id','rates','markups','currency_id','total','amount','markups','origin_airport_id','destination_airport_id','airline_id'];
+    protected $fillable = ['quote_id','contract','validity_start','validity_end','origin_port_id','destination_port_id','carrier_id','rates','markups','currency_id','total','amount','markups','origin_airport_id','destination_airport_id','airline_id','remarks','schedule_type','transit_time','via'];
 
     public function quote()
     {
