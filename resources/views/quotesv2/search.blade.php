d0f1efa2a0575185c15b9bd5c51d37e96818b69c
Alejandro G <ale1211@gmail.com>
2019-06-06 15:43:59 -0400

    fix inlands distance

--- resources/views/quotesv2/search.blade.php
+++ resources/views/quotesv2/search.blade.php
@@ -846,7 +846,7 @@
                           <span class="portalphacode" style="margin-right:5px;">Schedule Type: </span> {{ $arr->sheduleType  }}
                         </div>
                         <div class="col-lg-1 d-flex align-items-center">
-                          <span class="portalphacode" style="margin-right:15px; white-space:nowrap"> <i class="la la-truck"></i> Time:  </span>  {{ $arr->transit_time   }}
+                          <span class="portalphacode" style="margin-right:15px; white-space:nowrap">  TT:  </span>  {{ $arr->transit_time   }}
                         </div>
                         <div class="col-lg-2 d-flex align-items-center">
                           <span class="portalphacode" style="margin-right:15px;"> Via: </span> {{  $arr->via }}
