a0ccc4c2380cd8bb6621ca411d134ba4593cd34d
Luis Blanco <luedblra@gmail.com>
2019-06-15 13:31:16 -0400

    se activa ssl
    Former-commit-id: 3eb2e722748bc9ff0d0ce803d0f3070e34227398

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,10 +23,9 @@ class AppServiceProvider extends ServiceProvider
     Contract::observe(ContractObserver::class);
     Quote::observe(QuoteObserver::class);
 
-	/*
     if(env('APP_ENV') !== 'local') {
       $url->forceScheme('https');
-    }*/
+    }
   }
 
   public function register(){
