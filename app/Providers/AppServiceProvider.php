131caa34d237c7573812eb6fb60bef0dbb68ffde
Luis Blanco <luedblra@gmail.com>
2019-06-15 13:26:01 -0400

    se cambia htacces por ssl
    Former-commit-id: 60958971b68310f828bb6cf6a2ac723619f3b7c8 [formerly 776e89eefd034041247fb54fce851e55424374f1]
    Former-commit-id: a9b0d476db6b058554683ff4207ed79436f8ec79

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,9 +23,10 @@ class AppServiceProvider extends ServiceProvider
     Contract::observe(ContractObserver::class);
     Quote::observe(QuoteObserver::class);
 
+	/*
     if(env('APP_ENV') !== 'local') {
       $url->forceScheme('https');
-    }
+    }*/
   }
 
   public function register(){
