2bb18d0d3ea502ccaafe55a15f5260b9a8f2b770
Julio Avila <javila3090@gmai.com>
2019-06-18 19:00:25 -0400

    Modified pdfs
    Former-commit-id: de2fa675cac084d0cf0b7ae3c229d72bd76705d9 [formerly a49c5b632b660c427b5dbfe304607aa0a769351c]
    Former-commit-id: 735ded76195bfa9ab3b62e97bdba2d974f56ebe1

--- resources/views/quotesv2/pdf/index_lcl_air.blade.php
+++ resources/views/quotesv2/pdf/index_lcl_air.blade.php
@@ -224,9 +224,9 @@
                         <td >{{number_format((float)(@$total_freight+@$total_origin+@$total_destination)/$quote->chargeable_weight, 2, '.', '')}}</td>
                         <td >{{number_format((float)@$total_freight+@$total_origin+@$total_destination, 2, '.', '')}}</td>
                         @if($quote->pdf_option->show_schedules==1)
-                            <td>{{$rate->schedule_type}}</td>
-                            <td>{{$rate->transit_time}}</td>
-                            <td>{{$rate->via}}</td>
+                            <td>{{$rate->schedule_type!='' ? $rate->schedule_type:'-'}}</td>
+                            <td>{{$rate->transit_time!='' ? $rate->transit_time:'-'}}</td>
+                            <td>{{$rate->via!='' ? $rate->via:'-'}}</td>
                         @endif
                         <td >{{$quote->pdf_option->grouped_total_currency==0 ?$currency_cfg->alphacode:$quote->pdf_option->total_in_currency}}</td>
                     </tr>
