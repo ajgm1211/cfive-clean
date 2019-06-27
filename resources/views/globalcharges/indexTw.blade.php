20bd6ce07858d13aa54c961541e888e6b5ced093
Alejandro G <ale1211@gmail.com>
2019-06-27 09:03:34 -0400

    Changes LCL store
    Former-commit-id: d353c51e5b80b4c04ff1ea3b11a8457afc328c65 [formerly 7d0db58304df0ef3483b2efe99531ab07a3dea4a]
    Former-commit-id: 99326dd864c7ef1792be45e4bbad7811d85badca

--- resources/views/globalcharges/indexTw.blade.php
+++ resources/views/globalcharges/indexTw.blade.php
@@ -201,7 +201,7 @@ New \ Status Import  &nbsp;
 
     $(function() {
         $('#requesttable').DataTable({
-            processing: true,
+
             //serverSide: true,
             ajax: '{!! route("globalcharges.show",$company_userid) !!}',
             columns: [
@@ -222,12 +222,10 @@ New \ Status Import  &nbsp;
             "searching": true,
             "ordering": true,
             "width": true,
-            "info": true,
-            "deferLoading": 57,
             "autoWidth": false,
             "stateSave": true,
             "processing": true,
-            "dom": 'Bfrtip',
+            "serverSide": true,
             "paging": true
         });
 
