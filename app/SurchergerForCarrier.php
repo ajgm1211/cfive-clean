f420a19e128094d719ad3cc0f2eaa8dfec611b6c
luis <luedblra@gmail.com>
2019-07-18 16:03:56 -0400

    se efectua cambios en modelo
    Former-commit-id: 30abff0f80a843004b8c6809bde9bf760b0dee84 [formerly 7187870a5da57b51de94385eadcdb02de71036ea]
    Former-commit-id: 8118d0c409bb5a78675e267008a73493f61bb969

--- app/SurchergerForCarrier.php
+++ app/SurchergerForCarrier.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class SurchergerForCarrier extends Model
 {
     protected $table    = "surchargers_for_carrier";
-    protected $fillable = ['id','name','status'];
+    protected $fillable = ['id','name'];
 }
