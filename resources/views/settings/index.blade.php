3d9a94b0087cdda3995f2a5f566287b6b4512db5
Julio <javila3090@gmail.com>
2019-07-25 17:52:01 -0400

    Fixed bugs en inlands PDF, Added Inlands to LCL/AIR Pdf, Added Footer Pdf options in Settings

--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -76,10 +76,13 @@
                             <a class="nav-link color-black" aria-controls="home" aria-selected="true"><h6><b>Company profile</b></h6></a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true"><i class="fa fa-home"></i>&nbsp;Company info</a>
+                            <a class="nav-link active" id="general-tab" data-toggle="tab" href="#general" role="tab" aria-controls="home" aria-selected="true"><i class="fa fa-home"></i>&nbsp;Company info</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false"><i class="fa fa-edit"></i> &nbsp;PDF Settings</a>
+                            <a class="nav-link" id="pdf-tab" data-toggle="tab" href="#pdf" role="tab" aria-controls="profile" aria-selected="false"><i class="fa fa-edit"></i> &nbsp;PDF Settings</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="emails-tab" data-toggle="tab" href="#emails" role="tab" aria-controls="profile" aria-selected="false"><i class="fa fa-envelope"></i> &nbsp;Emails</a>
                         </li>
                     </ul>
                 </div>
@@ -88,7 +91,7 @@
                     @if(!isset($company->companyUser))
                     <form id="default-currency" enctype="multipart/form-data">
                         <div class="tab-content" id="myTabContent">
-                            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                            <div class="tab-pane fade show active" id="general" role="tabpanel" aria-labelledby="general-tab">
                                 <div class="row">
                                     <div class="col-md-6">
                                         <div class="form-group files">
@@ -134,7 +137,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                            <div class="tab-pane fade" id="pdf" role="tabpanel" aria-labelledby="pdf-tab">
                                 <div class="col-md-12">
                                     <div class="form-group m-form__group">
                                         <label for="pdf_language">PDF language</label>
@@ -152,7 +155,7 @@
                     @else
                     <form id="default-currency" enctype="multipart/form-data">
                         <div class="tab-content" id="myTabContent">
-                            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                            <div class="tab-pane fade show active" id="general" role="tabpanel" aria-labelledby="general-tab">
                                 <div class="row">
                                     <div class="col-md-6">
                                         <div class="form-group files">
@@ -208,20 +211,50 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-                                <div class="col-md-4">
-                                    <div class="form-group m-form__group">
-                                        <label for="pdf_language">PDF language</label>
-                                        {{ Form::select('pdf_language',['0'=>'Choose a language','1'=>'English','2'=>'Spanish','3'=>'Portuguese'],$company->companyUser->pdf_language,['placeholder' => 'Please choose a option','class'=>'custom-select form-control','id' => 'pdf_language','required'=>'true']) }}
+                            <div class="tab-pane fade" id="pdf" role="tabpanel" aria-labelledby="pdf-tab">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <div class="form-group m-form__group">
+                                            <label for="pdf_language">PDF language</label>
+                                            {{ Form::select('pdf_language',['0'=>'Choose a language','1'=>'English','2'=>'Spanish','3'=>'Portuguese'],$company->companyUser->pdf_language,['placeholder' => 'Please choose a option','class'=>'custom-select form-control','id' => 'pdf_language','required'=>'true']) }}
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="form-group m-form__group">
+                                            <label for="footer_type">PDF Footer</label>
+                                            {{ Form::select('footer_type',[''=>'Choose a type','Text'=>'Text','Image'=>'Image'],$company->companyUser->footer_type,['placeholder' => 'Please choose a option','class'=>'custom-select form-control','id' => 'pdf_footer','required'=>'true']) }}
+                                        </div>
                                     </div>
                                 </div>
-                                <div class="col-md-3">
-                                    <br>
-                                    <div class="form-group m-form__group">
-                                        <button type="button" id="default-currency-submit" class="btn btn-primary btn-block" style="background-color: #006BFA !important;">Update</button>
+                                <div class="row">
+                                    <div class="col-md-12 {{$company->companyUser->footer_type=='Image' ? '':'hide'}}" id="footer_image">
+                                        <hr>
+                                        <div class="form-group files">
+                                            <label for="footer_image">PDF Footer image (1000 x 80 pixels)</label>
+                                            <input type="file" class="form-control-file" name="footer_image">
+                                            @if($company->companyUser->footer_image!='')
+                                            <img src="{{Storage::disk('s3_upload')->url($company->companyUser->footer_image )}}" class="img img-thumbnail img-fluid" style="width:100%; height: 80px;">
+                                            @endif
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12 {{$company->companyUser->footer_type=='Text' ? '':'hide'}}" id="footer_text">
+                                        <hr>
+                                        <div class="form-group files">
+                                            <label for="footer_text">PDF Footer text</label>
+                                            <textarea name="footer_text" class="form-control footer_text editor" id="footer_text">{!!$company->companyUser->footer_text!!}</textarea>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-3">
+                                        <br>
+                                        <div class="form-group m-form__group">
+                                            <button type="button" id="default-currency-submit" class="btn btn-primary btn-block" style="background-color: #006BFA !important;">Update</button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
+                            <div class="tab-pane fade" id="emails" role="tabpanel" aria-labelledby="emails-tab"></div>
                         </div>
                     </form>
                     @endif
@@ -237,6 +270,8 @@
 <script src="/assets/demo/default/custom/components/forms/widgets/select2.js" type="text/javascript"></script>
 <script src="{{asset('js/base.js')}}" type="text/javascript"></script>
 <script src="{{asset('js/settings.js')}}" type="text/javascript"></script>
+<script src="{{asset('js/tinymce/jquery.tinymce.min.js')}}"></script>
+<script src="{{asset('js/tinymce/tinymce.min.js')}}"></script>
 <script>
     $('#currency_id').select2({
         placeholder: "Select an option"
@@ -244,5 +279,44 @@
     $('#pdf_language').select2({
         placeholder: "Select an option"
     });
+    $('#pdf_footer').select2({
+        placeholder: "Select an option"
+    });
+    var editor_config = {
+        path_absolute : "/",
+        selector: "textarea.editor",
+        plugins: ["template"],
+        toolbar: "insertfile undo redo | template | bold italic strikethrough | alignleft aligncenter alignright alignjustify | ltr rtl | bullist numlist outdent indent removeformat formatselect| link image media | emoticons charmap | code codesample | forecolor backcolor",
+        external_plugins: { "nanospell": "{{asset('js/tinymce/plugins/nanospell/plugin.js')}}" },
+        nanospell_server:"php",
+        browser_spellcheck: true,
+        relative_urls: false,
+        remove_script_host: false,
+        file_browser_callback : function(field_name, url, type, win) {
+            var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
+            var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
+
+            var cmsURL = editor_config.path_absolute + 'laravel-filemanager?field_name=' + field_name;
+            if (type == 'image') {
+                cmsURL = cmsURL + "&type=Images";
+            } else {
+                cmsURL = cmsURL + "&type=Files";
+            }
+
+            tinymce.activeEditor.windowManager.open({
+                file: '<?= route('elfinder.tinymce4') ?>',// use an absolute path!
+                title: 'File manager',
+                width: 900,
+                height: 450,
+                resizable: 'yes'
+            }, {
+                setUrl: function (url) {
+                    win.document.getElementById(field_name).value = url;
+                }
+            });
+        }
+    };
+
+    tinymce.init(editor_config);
 </script>
 @stop
