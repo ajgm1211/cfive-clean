7aca7c224e7a7289f7217198b43be376c76085bc
Julio <javila3090@gmail.com>
2019-08-06 18:57:24 -0400

    Working on API vforwarding features
    Former-commit-id: 61f2f4fc63ad110e8d9137a83ab66d4405ed982a

--- public/js/api-settings.js
+++ public/js/api-settings.js
@@ -1,9 +1,45 @@
 //Mostrar/Ocultar opciones api
-$(document).on('change', '#enable-api', function () {
+$(document).on('change', '#enable_api', function () {
     var value = 0;
     if($(this). prop("checked") == true){
+        value = 1;
         $('#api-table').removeClass('hide');
     }else{
         $('#api-table').addClass('hide');
     }
+
+    $.ajax({
+        type: 'GET',
+        url: '/api/enable/',
+        data: {
+            'value' : value,
+            'company_user_id' : $('#company_user_id').val(),
+        },
+        success: function(data) {
+            /*swal(
+                'Updated!',
+                'Api enabled successfully',
+                'success'
+            )*/
+        }
+    });
+});
+
+//Guardar Api Key
+$(document).on('click', '#store_api_key', function () {
+    $.ajax({
+        type: 'GET',
+        url: '/api/store/key',
+        data: {
+            'api_key' : $('#api_key').val(),
+            'company_user_id' : $('#company_user_id').val(),
+        },
+        success: function(data) {
+            swal(
+                'Well done!',
+                'Api key saved successfully',
+                'success'
+            )
+        }
+    });
 });
