a198c61b015cd28a33dfd426734d304a7c044597
Alejandro Gonz <ale1211@gmail.com>
2019-08-06 12:24:47 -0400

    Bug inland
    Former-commit-id: 106b996d8590898ea0fe59135c686daca245ac3c

--- resources/views/quotesv2/show.blade.php
+++ resources/views/quotesv2/show.blade.php
@@ -148,140 +148,140 @@
 @endsection
 @section('title', 'Quotes | Details')
 @section('content')
-    <div class="m-content bg-quotes">
-        @if(Session::has('message.nivel'))
-            <div class="col-md-12">
-                <br>
-                <div class="m-alert m-alert--icon m-alert--outline alert alert-{{ session('message.nivel') }} alert-dismissible fade show" role="alert">
-                    <div class="m-alert__icon">
-                        <i class="la la-warning"></i>
-                    </div>
-                    <div class="m-alert__text">
-                        <strong>
-                            {{ session('message.title') }}
-                        </strong>
-                        {{ session('message.content') }}
-                    </div>
-                    <div class="m-alert__close">
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close"></button>
-                    </div>
-                </div>
-            </div>
-        @endif
-
-        @include('quotesv2.partials.show_head')
-
-        <!-- Charges -->
-        @if($quote->type=='FCL')
-            @include('quotesv2.partials.ratesByContainer')
-        @else
-            @include('quotesv2.partials.ratesByPackage')
-        @endif
-
-        <!-- PDF Layout -->
-        @include('quotesv2.partials.pdf_layout')
-
-        <!-- Payments and terms conditions -->
-        @include('quotesv2.partials.payments')
-
-        <!-- Terms and conditions -->
-        @include('quotesv2.partials.terms')
-
+<div class="m-content bg-quotes">
+  @if(Session::has('message.nivel'))
+  <div class="col-md-12">
+    <br>
+    <div class="m-alert m-alert--icon m-alert--outline alert alert-{{ session('message.nivel') }} alert-dismissible fade show" role="alert">
+      <div class="m-alert__icon">
+        <i class="la la-warning"></i>
+      </div>
+      <div class="m-alert__text">
+        <strong>
+          {{ session('message.title') }}
+        </strong>
+        {{ session('message.content') }}
+      </div>
+      <div class="m-alert__close">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close"></button>
+      </div>
     </div>
-    @include('quotesv2.partials.sendQuoteModal')
-    @include('quotesv2.partials.createRateModal')
-    @include('quotesv2.partials.createInlandModal')
-    @include('quotesv2.partials.editRateModal')
+  </div>
+  @endif
+
+  @include('quotesv2.partials.show_head')
+
+  <!-- Charges -->
+  @if($quote->type=='FCL')
+  @include('quotesv2.partials.ratesByContainer')
+  @else
+  @include('quotesv2.partials.ratesByPackage')
+  @endif
+
+  <!-- PDF Layout -->
+  @include('quotesv2.partials.pdf_layout')
+
+  <!-- Payments and terms conditions -->
+  @include('quotesv2.partials.payments')
+
+  <!-- Terms and conditions -->
+  @include('quotesv2.partials.terms')
+
+</div>
+@include('quotesv2.partials.sendQuoteModal')
+@include('quotesv2.partials.createRateModal')
+@include('quotesv2.partials.createInlandModal')
+@include('quotesv2.partials.editRateModal')
 @endsection
 
 @section('js')
-    @parent
-    <script src="{{asset('js/base.js')}}" type="text/javascript"></script>
-    <script src="{{asset('js/quotes-v2.js')}}" type="text/javascript"></script>
-    <script src="/assets/demo/default/custom/components/forms/widgets/bootstrap-datepicker.js" type="text/javascript"></script>
-    <script src="/assets/demo/default/custom/components/forms/widgets/bootstrap-touchspin.js" type="text/javascript"></script>
-    <script src="/assets/demo/default/custom/components/forms/widgets/ion-range-slider.js" type="text/javascript"></script>
-    <script src="/assets/demo/default/custom/components/base/dropdown.js" type="text/javascript"></script>
-    <script src="{{asset('js/tinymce/jquery.tinymce.min.js')}}"></script>
-    <script src="{{asset('js/tinymce/tinymce.min.js')}}"></script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBCVgHV1pi7UVCHZS_wMEckVZkj_qXW7V0&libraries=places&callback=initAutocomplete" async defer></script>
-    <script type="text/javascript">
+@parent
+<script src="{{asset('js/base.js')}}" type="text/javascript"></script>
+<script src="{{asset('js/quotes-v2.js')}}" type="text/javascript"></script>
+<script src="/assets/demo/default/custom/components/forms/widgets/bootstrap-datepicker.js" type="text/javascript"></script>
+<script src="/assets/demo/default/custom/components/forms/widgets/bootstrap-touchspin.js" type="text/javascript"></script>
+<script src="/assets/demo/default/custom/components/forms/widgets/ion-range-slider.js" type="text/javascript"></script>
+<script src="/assets/demo/default/custom/components/base/dropdown.js" type="text/javascript"></script>
+<script src="{{asset('js/tinymce/jquery.tinymce.min.js')}}"></script>
+<script src="{{asset('js/tinymce/tinymce.min.js')}}"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBCVgHV1pi7UVCHZS_wMEckVZkj_qXW7V0&libraries=places&callback=initAutocomplete" async defer></script>
+<script type="text/javascript">
 
-        /*** GOOGLE MAPS API ***/
+  /*** GOOGLE MAPS API ***/
 
-      var autocomplete;
-      function initAutocomplete() {
-        var geocoder = new google.maps.Geocoder();
-        var autocomplete = new google.maps.places.Autocomplete((document.getElementById('origin_address')));
-        var autocomplete_destination = new google.maps.places.Autocomplete((document.getElementById('destination_address')));
-        //autocomplete.addListener('place_changed', fillInAddress);
+  var autocomplete;
+  function initAutocomplete() {
+    var geocoder = new google.maps.Geocoder();
+    var autocomplete = new google.maps.places.Autocomplete((document.getElementById('origin_address')));
+    var autocomplete_destination = new google.maps.places.Autocomplete((document.getElementById('destination_address')));
+    //autocomplete.addListener('place_changed', fillInAddress);
+  }
+
+  function codeAddress(address) {
+    var geocoder;
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == 'OK') {
+        alert(results[0].geometry.location);
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  }
+
+  var editor_config = {
+    path_absolute : "/",
+    selector: "textarea.editor",
+    plugins: ["template"],
+    toolbar: "insertfile undo redo | template | bold italic strikethrough | alignleft aligncenter alignright alignjustify | ltr rtl | bullist numlist outdent indent removeformat formatselect| link image media | emoticons charmap | code codesample | forecolor backcolor",
+    external_plugins: { "nanospell": "{{asset('js/tinymce/plugins/nanospell/plugin.js')}}" },
+    nanospell_server:"php",
+    browser_spellcheck: true,
+    relative_urls: false,
+    remove_script_host: false,
+    file_browser_callback : function(field_name, url, type, win) {
+      var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
+      var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
+
+      var cmsURL = editor_config.path_absolute + 'laravel-filemanager?field_name=' + field_name;
+      if (type == 'image') {
+        cmsURL = cmsURL + "&type=Images";
+      } else {
+        cmsURL = cmsURL + "&type=Files";
       }
 
-      function codeAddress(address) {
-        var geocoder;
-        geocoder.geocode( { 'address': address}, function(results, status) {
-          if (status == 'OK') {
-            alert(results[0].geometry.location);
-          } else {
-            alert('Geocode was not successful for the following reason: ' + status);
-          }
-        });
-      }
-
-      var editor_config = {
-        path_absolute : "/",
-        selector: "textarea.editor",
-        plugins: ["template"],
-        toolbar: "insertfile undo redo | template | bold italic strikethrough | alignleft aligncenter alignright alignjustify | ltr rtl | bullist numlist outdent indent removeformat formatselect| link image media | emoticons charmap | code codesample | forecolor backcolor",
-        external_plugins: { "nanospell": "{{asset('js/tinymce/plugins/nanospell/plugin.js')}}" },
-        nanospell_server:"php",
-        browser_spellcheck: true,
-        relative_urls: false,
-        remove_script_host: false,
-        file_browser_callback : function(field_name, url, type, win) {
-          var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
-          var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
-
-          var cmsURL = editor_config.path_absolute + 'laravel-filemanager?field_name=' + field_name;
-          if (type == 'image') {
-            cmsURL = cmsURL + "&type=Images";
-          } else {
-            cmsURL = cmsURL + "&type=Files";
-          }
-
-          tinymce.activeEditor.windowManager.open({
-                    file: '<?= route('elfinder.tinymce4') ?>',// use an absolute path!
-                    title: 'File manager',
-                    width: 900,
-                    height: 450,
-                    resizable: 'yes'
-                  }, {
-                    setUrl: function (url) {
-                      win.document.getElementById(field_name).value = url;
-                    }
-                  });
+      tinymce.activeEditor.windowManager.open({
+        file: '<?= route('elfinder.tinymce4') ?>',// use an absolute path!
+        title: 'File manager',
+        width: 900,
+        height: 450,
+        resizable: 'yes'
+      }, {
+        setUrl: function (url) {
+          win.document.getElementById(field_name).value = url;
         }
-      };
-
-      tinymce.init(editor_config);
-
-      $('.btn-open').on('click', function(){
-        var element = $(this).attr('id');
-
-        if(element == 'pay')
-        {
-          $('.pay').toggle();
-        }
-        if(element == 'terms')
-        {
-          $('.terms').toggle();
-        }
-        if(element == 'remks')
-        {
-          $('.remks').toggle();
-        }
-
       });
+    }
+  };
+
+  tinymce.init(editor_config);
+
+  $('.btn-open').on('click', function(){
+    var element = $(this).attr('id');
+
+    if(element == 'pay')
+    {
+      $('.pay').toggle();
+    }
+    if(element == 'terms')
+    {
+      $('.terms').toggle();
+    }
+    if(element == 'remks')
+    {
+      $('.remks').toggle();
+    }
+
+  });
 
   function AbrirModal(action,id){
     if(action == "edit"){
@@ -304,5 +304,13 @@
       });
     }
   }
+
+  if(history.forward(1)){
+    history.replace(history.forward(1));
+  }
+
+
+
+
 </script>
 @stop
