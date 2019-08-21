9d8ff49a031f26ebb98c88b50ed6a839dbb577ad
luis <luedblra@gmail.com>
2019-08-21 11:05:50 -0400

    se agrega condicionamiento para job assciado a la autoimportacion
    Former-commit-id: fd7053e50675fd7da202cd989e07e57d1a2f40c0

--- app/ImportationJob.php
+++ app/ImportationJob.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class ImportationJob extends Model
 {
-    //
+    protected $ftable   = 'importation_jobs';
+    protected $fillable = ['id', 'queue', 'payload','attempts','reserved_at','available_at','created_at']; 
 }
