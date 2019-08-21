5882d08b108ff11420802affc9db970a17ee04c9
luis <luedblra@gmail.com>
2019-08-21 11:05:50 -0400

    se agrega condicionamiento para job assciado a la autoimportacion
    Former-commit-id: 985f034c4ca467fa521f76459af2b60acc5796da [formerly fd7053e50675fd7da202cd989e07e57d1a2f40c0]
    Former-commit-id: c1f683dc726e41335561b2ea2b2379baee9287c8

--- app/Helpers/HelperValidations.php
+++ app/Helpers/HelperValidations.php
@@ -3,6 +3,8 @@
 namespace App\Helpers;
 
 use App\Job;
+use App\ImportationJob;
+use App\NewContractRequest;
 
 class HelperValidations {
 
@@ -25,12 +27,34 @@ class HelperValidations {
                 $job_id = $job->id;
             }
         }
+
+        if($bool == false){
+            $request_id = null;
+            $data       = NewContractRequest::where('contract_id',$contract_id)->first();
+            $request_id = $data->id;
+            $jobs       = ImportationJob::where('payload','LIKE','%id%')->get();
+            foreach($jobs as $job){
+                $poscion    = null;
+                $json       = json_decode($job['payload']);
+                $poscion    = strripos($json->{'data'}->{'command'},'id');
+                $data       = substr($json->{'data'}->{'command'},$poscion,100);
+                $data       = explode(";",$data);
+                $poscion    = strripos($data[1],':"');
+                $data       = substr($data[1],$poscion,100);
+                $data       = str_replace([':','"'],'',$data);
+                if($data == $request_id){
+                    $bool   = true;
+                    $job_id = $job->id;
+                }
+            }
+        }
+
         $data = ['bool' => $bool,'job_id' => $job_id];
         //dd($jobs[0]->toArray());
 
         return $data;
     }
-    
+
     // VALIDATE IF A JOB IS ASSOCIATED WITH AN ACOUNT GC
     public static function AcountWithJob($account_id){
         $bool        = false;
