a6e52dfc258885fe1b9f2f72ceae10ab46b95394
Alejandro G <ale1211@gmail.com>
2019-09-03 09:33:40 -0400

    se comenta funciones intercom
    Former-commit-id: a4b4f025e67007eeedc489e9a214251d160be718 [formerly 98153b58cfa70ea456d049807245e7b3bb09cfd9]
    Former-commit-id: a26cf20ee4ea897fab1ab75117ea1c95631b72b2

--- app/Helpers/HelperEventIntercom.php
+++ app/Helpers/HelperEventIntercom.php
@@ -17,111 +17,111 @@ class Intercom{
   // EVENTOS RATES
   public static function event_selectRate(){
     $obj  = self::$client;    
-    $obj->events->create([
+   /* $obj->events->create([
       "event_name" => "SELECT RATE",
       "created_at" => strtotime("now"),
       "email" =>  \Auth::user()->email,
       "metadata" => [
         "order_date" => strtotime("now")
       ]
-    ]);
+    ]);*/
   }
   public static function event_searchRate(){
     $obj  = self::$client;    
-    $obj->events->create([
+   /* $obj->events->create([
       "event_name" => "SEARCH RATE",
       "created_at" => strtotime("now"),
       "email" =>  \Auth::user()->email,
       "metadata" => [
         "order_date" => strtotime("now")
       ]
-    ]);
+    ]);*/
   }
   //EVENTOS QUOTE 
   public static function event_quoteEmail(){
     $obj  = self::$client;    
-    $obj->events->create([
+  /*  $obj->events->create([
       "event_name" => "QUOTE SEND EMAIL",
       "created_at" => strtotime("now"),
       "email" =>  \Auth::user()->email,
       "metadata" => [
         "order_date" => strtotime("now")
       ]
-    ]);
+    ]);*/
   }
   public static function event_quotePdf(){
     $obj  = self::$client;    
-    $obj->events->create([
+   /* $obj->events->create([
       "event_name" => "QUOTE PDF",
       "created_at" => strtotime("now"),
       "email" =>  \Auth::user()->email,
       "metadata" => [
         "order_date" => strtotime("now")
       ]
-    ]);
+    ]);*/
   }
   public static function event_quoteAutomaticFcl(){
     $obj  = self::$client;    
-    $obj->events->create([
+    /*$obj->events->create([
       "event_name" => "QUOTE AUTOMATIC FCL",
       "created_at" => strtotime("now"),
       "email" =>  \Auth::user()->email,
       "metadata" => [
         "order_date" => strtotime("now")
       ]
-    ]);
+    ]);*/
   }
   public static function event_quoteAutomaticLcl(){
     $obj  = self::$client;    
-    $obj->events->create([
+    /*$obj->events->create([
       "event_name" => "QUOTE AUTOMATIC LCL",
       "created_at" => strtotime("now"),
       "email" =>  \Auth::user()->email,
       "metadata" => [
         "order_date" => strtotime("now")
       ]
-    ]);
+    ]);*/
   }
 
   public static function event_quoteManualFcl(){
     $obj  = self::$client;    
-    $obj->events->create([
+  /*  $obj->events->create([
       "event_name" => "QUOTE MANUAL FCL",
       "created_at" => strtotime("now"),
       "email" =>  \Auth::user()->email,
       "metadata" => [
         "order_date" => strtotime("now")
       ]
-    ]);
+    ]);*/
   }
 
   public static function event_quoteManualLcl(){
     $obj  = self::$client;    
-    $obj->events->create([
+    /*$obj->events->create([
       "event_name" => "QUOTE MANUAL LCL",
       "created_at" => strtotime("now"),
       "email" =>  \Auth::user()->email,
       "metadata" => [
         "order_date" => strtotime("now")
       ]
-    ]);
+    ]);*/
   }
 
   public static function event_quoteManualAir(){
     $obj  = self::$client;    
-    $obj->events->create([
+    /*$obj->events->create([
       "event_name" => "QUOTE MANUAL AIR",
       "created_at" => strtotime("now"),
       "email" =>  \Auth::user()->email,
       "metadata" => [
         "order_date" => strtotime("now")
       ]
-    ]);
+    ]);*/
   }
   // EVENTOS CONTRACTS
   public static function event_contractFcl(){
     $obj  = self::$client;    
-    $users = User::all()->where('company_user_id','=', \Auth::user()->company_user_id);
+   /* $users = User::all()->where('company_user_id','=', \Auth::user()->company_user_id);
     foreach ($users as $u) {
       $obj->events->create([
         "event_name" => "CONTRACT FCL",
@@ -131,11 +131,11 @@ class Intercom{
           "order_date" => strtotime("now")
         ]
       ]);
-    }
+    }*/
   }
   public static function event_contractLcl(){
     $obj  = self::$client;    
-    $users = User::all()->where('company_user_id','=', \Auth::user()->company_user_id);
+    /*$users = User::all()->where('company_user_id','=', \Auth::user()->company_user_id);
     foreach ($users as $u) {
       $obj->events->create([
         "event_name" => "CONTRACT LCL",
@@ -145,12 +145,12 @@ class Intercom{
           "order_date" => strtotime("now")
         ]
       ]);
-    }
+    }*/
   }
   // EVENTOS GLOBAL CHARGES
   public static function event_globalChargesFcl(){
     $obj  = self::$client;    
-    $users = User::all()->where('company_user_id','=', \Auth::user()->company_user_id);
+   /* $users = User::all()->where('company_user_id','=', \Auth::user()->company_user_id);
     foreach ($users as $u) {
       $obj->events->create([
         "event_name" => "GLOBALCHARGES FCL",
@@ -160,11 +160,11 @@ class Intercom{
           "order_date" => strtotime("now")
         ]
       ]);
-    }
+    }*/
   }
   public static function event_globalChargesLcl(){
     $obj  = self::$client;    
-    $users = User::all()->where('company_user_id','=', \Auth::user()->company_user_id);
+   /* $users = User::all()->where('company_user_id','=', \Auth::user()->company_user_id);
     foreach ($users as $u) {
       $obj->events->create([
         "event_name" => "GLOBALCHARGES LCL",
@@ -174,13 +174,13 @@ class Intercom{
           "order_date" => strtotime("now")
         ]
       ]);
-    }
+    }*/
   }
   // INLANDS
 
   public static function event_inlands(){
     $obj  = self::$client;    
-    $users = User::all()->where('company_user_id','=', \Auth::user()->company_user_id);
+    /*$users = User::all()->where('company_user_id','=', \Auth::user()->company_user_id);
     foreach ($users as $u) {
       $obj->events->create([
         "event_name" => "INLANDS",
@@ -190,31 +190,31 @@ class Intercom{
           "order_date" => strtotime("now")
         ]
       ]);
-    }
+    }*/
   }
 
   // COMPAÑIAS y CONTACTOS
   public static function event_companies(){
     $obj  = self::$client;    
-    $obj->events->create([
+    /*$obj->events->create([
       "event_name" => "COMPANIES",
       "created_at" => strtotime("now"),
       "email" =>  \Auth::user()->email,
       "metadata" => [
         "order_date" => strtotime("now")
       ]
-    ]);
+    ]);*/
   }
   public static function event_contacts(){
     $obj  = self::$client;    
-    $obj->events->create([
+    /*$obj->events->create([
       "event_name" => "CONTACTS",
       "created_at" => strtotime("now"),
       "email" =>  \Auth::user()->email,
       "metadata" => [
         "order_date" => strtotime("now")
       ]
-    ]);
+    ]);*/
   }
 
   // PRICING 
@@ -222,21 +222,21 @@ class Intercom{
 
   public static function event_pricing(){
     $obj  = self::$client;    
-    $obj->events->create([
+   /* $obj->events->create([
       "event_name" => "PRICING",
       "created_at" => strtotime("now"),
       "email" =>  \Auth::user()->email,
       "metadata" => [
         "order_date" => strtotime("now")
       ]
-    ]);
+    ]);*/
   }
 
   // REQUEST CONTRACT 
 
   public static function event_requestDone($idUser){
     $usercreador = User::find($idUser);
-    $obj  = self::$client;    
+   /* $obj  = self::$client;    
     $users = User::all()->where('company_user_id','=', $usercreador->company_user_id);
     foreach ($users as $u) {
       $obj->events->create([
@@ -247,35 +247,29 @@ class Intercom{
           "order_date" => strtotime("now")
         ]
       ]);
-    }
+    }*/
   }
 
   public static function event_newRequest(){
     $obj  = self::$client;    
-    $obj->events->create([
+   /* $obj->events->create([
       "event_name" => "NEW REQUEST",
       "created_at" => strtotime("now"),
       "email" =>  \Auth::user()->email,
       "metadata" => [
         "order_date" => strtotime("now")
       ]
-    ]);
+    ]);*/
   }
   public static function event_newRequestLCL(){
     $obj  = self::$client;    
-    $obj->events->create([
+   /* $obj->events->create([
       "event_name" => "NEW REQUEST LCL",
       "created_at" => strtotime("now"),
       "email" =>  \Auth::user()->email,
       "metadata" => [
         "order_date" => strtotime("now")
       ]
-    ]);
+    ]);*/
   }
-
-
-
-
-
-
 }
