eac70de83d953a3347c8d5f8372f9a2217224b9f
luis <luedblra@gmail.com>
2019-09-12 15:58:00 -0400

    se crea migracion de procedure ghadm
    Former-commit-id: cada37e49e7084f10e5668f469b269100dc51253 [formerly 4318fb906fb3b998185cf7370514e32ff67850df]
    Former-commit-id: 607c778d079ba0cdc65cb2270375b044aa3a1c91

---
+++ database/migrations/2019_09_12_183858_updated_procedure_gcfcladm_table.php
@@ -0,0 +1,97 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdatedProcedureGcfcladmTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::unprepared("DROP PROCEDURE IF EXISTS select_globalcharge_adm; CREATE  PROCEDURE `select_globalcharge_adm`(IN comp_id int,IN carr_code int)
+SELECT 
+        `gc`.`id` AS `id`,
+        `sr`.`name` AS `charge`,
+        `td`.`description` AS `charge_type`,
+        `ctype`.`name` AS `calculation_type`,
+        (SELECT 
+                GROUP_CONCAT(DISTINCT `har`.`code`
+                        SEPARATOR ', ')
+            FROM
+                (`globalcharport` `gcP`
+                JOIN `harbors` `har` ON ((`har`.`id` = `gcP`.`port_orig`)))
+            WHERE
+                (`gcP`.`globalcharge_id` = `gc`.`id`)) AS `origin_port`,
+        (SELECT 
+                GROUP_CONCAT(DISTINCT `har`.`code`
+                        SEPARATOR ', ')
+            FROM
+                (`globalcharport` `gcP`
+                JOIN `harbors` `har` ON ((`har`.`id` = `gcP`.`port_dest`)))
+            WHERE
+                (`gcP`.`globalcharge_id` = `gc`.`id`)) AS `destination_port`,
+        (SELECT 
+                GROUP_CONCAT(DISTINCT `coun`.`name`
+                        SEPARATOR ', ')
+            FROM
+                (`globalcharcountry` `gcCO`
+                JOIN `countries` `coun` ON ((`coun`.`id` = `gcCO`.`country_orig`)))
+            WHERE
+                (`gcCO`.`globalcharge_id` = `gc`.`id`)) AS `origin_country`,
+        (SELECT 
+                GROUP_CONCAT(DISTINCT `counD`.`name`
+                        SEPARATOR ', ')
+            FROM
+                (`globalcharcountry` `gcCD`
+                JOIN `countries` `counD` ON ((`counD`.`id` = `gcCD`.`country_dest`)))
+            WHERE
+                (`gcCD`.`globalcharge_id` = `gc`.`id`)) AS `destination_country`,
+        (SELECT 
+                GROUP_CONCAT(DISTINCT `carr`.`name`
+                        SEPARATOR ', ')
+            FROM
+                (`globalcharcarrier` `gcC`
+                JOIN `carriers` `carr` ON ((`carr`.`id` = `gcC`.`carrier_id`)))
+            WHERE
+                (`gcC`.`globalcharge_id` = `gc`.`id`)) AS `carrier`,
+        (SELECT 
+                GROUP_CONCAT(DISTINCT `carr`.`uncode`
+                        SEPARATOR ', ')
+            FROM
+                (`globalcharcarrier` `gcC`
+                JOIN `carriers` `carr` ON ((`carr`.`id` = `gcC`.`carrier_id`)))
+            WHERE
+                (`gcC`.`globalcharge_id` = `gc`.`id`)) AS `carriers`,
+        `gc`.`ammount` AS `amount`,
+        `cur`.`alphacode` AS `currency_code`,
+        `gc`.`validity` AS `valid_from`,
+        `gc`.`expire` AS `valid_until`,
+        `gc`.`company_user_id` AS `company_user_id`,
+        `cmpu`.`name` AS `company_user`
+    FROM
+        (((((`globalcharges` `gc`
+        JOIN `surcharges` `sr` ON ((`sr`.`id` = `gc`.`surcharge_id`)))
+        JOIN `typedestiny` `td` ON ((`td`.`id` = `gc`.`typedestiny_id`)))
+        JOIN `currency` `cur` ON ((`cur`.`id` = `gc`.`currency_id`)))
+        JOIN `calculationtype` `ctype` ON ((`ctype`.`id` = `gc`.`calculationtype_id`)))
+        JOIN `company_users` `cmpu` ON ((`cmpu`.`id` = `gc`.`company_user_id`))
+        JOIN `globalcharcarrier` `gcC` ON ((`gc`.`id` = `gcC`.`globalcharge_id`)))
+        where cmpu.id = comp_id AND gcC.carrier_id = carr_code;
+");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
