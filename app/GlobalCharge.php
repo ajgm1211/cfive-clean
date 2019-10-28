3855767b2339dcbd46393fc4a103bc5706d6686c
Alejandro G <ale1211@gmail.com>
2019-10-28 11:50:41 -0400

    Migraciones y modelos de pais a puerto y viceversa de globalcharges
    Former-commit-id: 16bbf9d2bd9b63ce4e43bbc56c641e56483d193a

--- app/GlobalCharge.php
+++ app/GlobalCharge.php
@@ -7,55 +7,75 @@ use OwenIt\Auditing\Contracts\Auditable;
 
 class GlobalCharge extends Model implements Auditable
 {
-	use \OwenIt\Auditing\Auditable;
-	protected $table    = "globalcharges";
-	protected $fillable = 
-	['id','surcharge_id','typedestiny_id','company_user_id','calculationtype_id','ammount','validity','expire','currency_id','account_importation_globalcharge_id','created_at','updated_at'];
-	public function companyUser()
-	{
-		return $this->belongsTo('App\CompanyUser');
-	}
+  use \OwenIt\Auditing\Auditable;
+  protected $table    = "globalcharges";
+  protected $fillable = 
+    ['id','surcharge_id','typedestiny_id','company_user_id','calculationtype_id','ammount','validity','expire','currency_id','account_importation_globalcharge_id','created_at','updated_at'];
+  public function companyUser()
+  {
+    return $this->belongsTo('App\CompanyUser');
+  }
 
-	public function currency(){
+  public function currency(){
 
-		return $this->belongsTo('App\Currency');
+    return $this->belongsTo('App\Currency');
 
-	}
-	public function calculationtype(){
+  }
+  public function calculationtype(){
 
-		return $this->belongsTo('App\CalculationType');
+    return $this->belongsTo('App\CalculationType');
 
-	}
-	public function surcharge(){
+  }
+  public function surcharge(){
 
-		return $this->belongsTo('App\Surcharge');
+    return $this->belongsTo('App\Surcharge');
 
-	}
-	public function globalcharport(){
+  }
+  // Puerto a Puerto
+  public function globalcharport(){
 
-		return $this->hasMany('App\GlobalCharPort','globalcharge_id');
+    return $this->hasMany('App\GlobalCharPort','globalcharge_id');
 
-	}
-	public function globalcharcountry(){
+  }
+  // Pais a Pais
+  public function globalcharcountry(){
 
-		return $this->hasMany('App\GlobalCharCountry','globalcharge_id');
+    return $this->hasMany('App\GlobalCharCountry','globalcharge_id');
 
-	}
-	public function globalcharcarrier(){
-		return $this->hasMany('App\GlobalCharCarrier','globalcharge_id');
+  }
+  // Pais a puerto 
+  public function globalcharcountryport(){
 
-	}
-	public function carrier(){
-		return $this->hasOne('App\GlobalCharCarrier','globalcharge_id');
+    return $this->hasMany('App\GlobalCharCountryPort','globalcharge_id');
 
-	}
-	public function globalCharPortCarriers(){
+  }
+  
+  // Puerto a Pais
+  public function globalcharportcountry(){
 
-		return $this->hasMany('App\GlobalCharPortCarrier','port');
+    return $this->hasMany('App\GlobalCharPortCountry','globalcharge_id');
 
-	}
-	public function typedestiny(){
-		return $this->belongsTo('App\TypeDestiny');
+  }
+  public function globalcharcountry(){
 
-	}
+    return $this->hasMany('App\GlobalCharCountry','globalcharge_id');
+
+  }
+  public function globalcharcarrier(){
+    return $this->hasMany('App\GlobalCharCarrier','globalcharge_id');
+
+  }
+  public function carrier(){
+    return $this->hasOne('App\GlobalCharCarrier','globalcharge_id');
+
+  }
+  public function globalCharPortCarriers(){
+
+    return $this->hasMany('App\GlobalCharPortCarrier','port');
+
+  }
+  public function typedestiny(){
+    return $this->belongsTo('App\TypeDestiny');
+
+  }
 }
