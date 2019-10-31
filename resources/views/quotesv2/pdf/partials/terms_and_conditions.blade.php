3326c924ce7635f3a99077e7d49b7603fc10d7c8
Julio <javila3090@gmail.com>
2019-10-31 11:11:19 -0300

    Modified Terms and Conditions in PDF
    Former-commit-id: 800e0f843ae3453103961e899782e1bc61080a73

--- resources/views/quotesv2/pdf/partials/terms_and_conditions.blade.php
+++ resources/views/quotesv2/pdf/partials/terms_and_conditions.blade.php
@@ -1,44 +1,38 @@
 @if($quote->terms_and_conditions!='' || $quote->terms_english!='' || $quote->terms_portuguese!='')
     <div class="clearfix">
-        <table class="table-border table-no-split" border="0" cellspacing="0" cellpadding="0">
-            <thead class="title-quote header-table">
-                <tr>
-                    @switch($quote->pdf_option->language)
-                        @case("English")
-                            <th class="unit text-left" {{$quote->pdf_option->language=='English' ? '':'hidden'}}><b>&nbsp;&nbsp;&nbsp;Terms and conditions</b></th>
-                        @break
-                        @case("Portuguese")
-                            <th class="unit text-left" {{$quote->pdf_option->language=='Portuguese' ? '':'hidden'}}><b>&nbsp;&nbsp;&nbsp;Termos e Condições</b></th>
-                        @break
-                        @case("Spanish")
-                            <th class="unit text-left" {{$quote->pdf_option->language=='Spanish' ? '':'hidden'}}><b>&nbsp;&nbsp;&nbsp;Términos y condiciones</b></th>
-                        @break
-                    @endswitch
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td style="padding:15px;">
-                        @switch($quote->pdf_option->language)
-                            @case("English")
-                                <span class="text-justify">{!! @$quote->terms_english != "" ? @$quote->terms_english:@$quote->terms_and_conditions !!}</span>
-                            @break
+        
+        @switch($quote->pdf_option->language)
+            @case("English")
+                <span class="color-title text-left"><b>Terms and conditions</b><br><br/></span>
+                @break
+            @case("Portuguese")
+                <span class="color-title text-left"><b>Termos e Condições</b><br><br/></span><br/>
+                @break
+            @case("Spanish")
+                <span class="color-title text-left"><b>Términos y condiciones</b><br><br/></span><br/>
+                @break
+            @default
+                <span class="color-title text-left"><b>Terms and conditions</b><br><br/></span><br/>
+                @break
+        @endswitch
+        
+        @switch($quote->pdf_option->language)
+            @case("English")
+                <span class="text-justify">{!! @$quote->terms_english != "" ? @$quote->terms_english:@$quote->terms_and_conditions !!}</span>
+                @break
 
-                            @case("Portuguese")
-                                <span class="text-justify">{!! @$quote->terms_portuguese != "" ? @$quote->terms_portuguese:@$quote->terms_and_conditions !!}</span>
-                            @break
+            @case("Portuguese")
+                <span class="text-justify">{!! @$quote->terms_portuguese != "" ? @$quote->terms_portuguese:@$quote->terms_and_conditions !!}</span>
+                @break
 
-                            @case("Spanish")
-                                <span class="text-justify">{!! @$quote->terms_and_conditions  !!}</span>
-                            @break
+            @case("Spanish")
+                <span class="text-justify">{!! @$quote->terms_and_conditions  !!}</span>
+                @break
 
-                            @default
-                                <span class="text-justify">{!! @$quote->terms_and_conditions  !!}</span>
-                        @endswitch
-                    </td>
-                </tr>                        
-            </tbody>
-        </table>
+            @default
+                <span class="text-justify">{!! @$quote->terms_and_conditions  !!}</span>
+                @endswitch
+                
     </div>
 @endif
 <br>
