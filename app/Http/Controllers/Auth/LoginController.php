8661dbbecd12f9bd0d6593c8bd7c768be36115fb
Alejandro G <ale1211@gmail.com>
2019-11-25 13:00:24 -0400

    Se agrega validacion en intercom para evitar duplicados
    Former-commit-id: a67f19c32c13512f8ccc868a1261641c83761647 [formerly 87edef489195ab1212456f9d598e175b1a95401b]
    Former-commit-id: 39fc525ffd495e2e0d26040eefa5826679877133

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -41,19 +41,37 @@ class LoginController extends Controller
     $this->middleware('guest')->except('logout');
   }
 
+
+  public function intercom($client,$user){ 
+
+    $cliente =  $client->users->getUsers(["email" => $user->email]);
+    if($cliente->total_count > 1 ){
+      foreach($cliente->users as $u){
+        if($u->type == "user" ){
+          if($u->user_id != $user->id ){
+            $client->users->archiveUser($u->id);
+          }
+        }
+      }
+    }
+  }
+
   // @overwrite
   public function authenticated(Request $request, $user)
   {  
+
+
     $client = new IntercomClient('dG9rOmVmN2IwNzI1XzgwMmFfNDdlZl84NzUxX2JlOGY5NTg4NGIxYjoxOjA=', null, ['Intercom-Version' => '1.1']);
+    $this->intercom($client,$user);
     $client->users->create([
       "email" => $user->email,
+      "user_id" =>$user->id,
       "name" => $user->name,
     ]);
     // Crear hash id del usuario logueado 
-
-
     if($user->company_user_id != ""){
       setHashID();
+      $this->intercom($client,$user);
       $client->users->create([
         "email" => $user->email,
         "companies" => [
