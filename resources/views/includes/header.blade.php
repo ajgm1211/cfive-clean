8dfaee9d10b2ab5b99b618904205136d86afabe3
luis <luedblra@gmail.com>
2020-02-03 12:02:03 -0400

    se inicia split

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,9 +1,12 @@
 <header class="m-grid__item m-header" data-minimize-offset="200" data-minimize-mobile-offset="200" >
-   <div class="m-container m-container--fluid m-container--full-height">
+	<div class="m-container m-container--fluid m-container--full-height">
+		@if(Session::has('impersonate') || env('APP_VIEW') == 'local' || env('APP_VIEW') == 'prod' || env('APP_VIEW') == 'dev')
+		@include('includes.header_menu')
+		@elseif(env('APP_VIEW') == 'operaciones')
+		@include('includes.header_menu_operaciones')
+		@endif
+		<!-- END: Horizontal Menu -->
 
-      @include('includes.header_menu')
-      <!-- END: Horizontal Menu -->
-
-   </div>
+	</div>
 
 </header>
