c0d9633f835a3cfb146e3b5ba17dcbf2b0b757e6
Alejandro G <ale1211@gmail.com>
2020-02-05 14:58:46 -0400

    Se crea y asocian los contactos en crisp
    Former-commit-id: a6b34e9fe00458f3d9df9ffd2463785b0886e47e

---
+++ app/Helpers/HelperCrisp.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Helpers;
+
+class EventCrisp{
+
+  static $web_id = '011f006f-3864-44b5-9443-d700e87df5f7';
+  static $token_indentifier = '7012d8d8-532f-4d0e-83a5-407abd5f07d3';
+  static $token_key = 'bf601822e3743e4a611dc964254a71ac1347586ee691d60ce98f247e0047ae55';
+  static $CrispClient;
+
+  public function __construct(){
+
+    self::$CrispClient = new \Crisp();
+    self::$CrispClient->authenticate(self::$token_indentifier, self::$token_key);
+  }
+
+  public function findByEmail($email){
+    $obj  = self::$CrispClient;   
+    $people =   $obj->websitePeople->findByEmail(self::$web_id,$email);
+    return $people;
+  }
+
+  public function createProfile($params){
+    $obj  = self::$CrispClient;   
+    $people =  $obj->websitePeople->createNewPeopleProfile(self::$web_id, $params);
+    return $people;
+  }
+
+  public function updateProfile($params,$email){
+    $obj  = self::$CrispClient;
+    $people =  self::findByEmail($email);
+    $people = $obj->websitePeople->updatePeopleProfile(self::$web_id, $people['people_id'], $params);
+    return $people;
+    
+
+  }
+
+}
