2709006f41cc94f17959f915aede141deaf0d762
David <dcabanales@gmail.com>
2020-02-07 03:48:41 -0400

    Global Charge Api Module Created
    Former-commit-id: 2c42db7173ccc6a651d93a3601665180e6fcbe7e [formerly cd6a4578822c86b0d02b3f5a64271d1a23e6924a]
    Former-commit-id: 30335129ccf4c3253880e99eb327bf99fca36600

---
+++ app/GlobalChargePortApi.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class GlobalChargePortApi extends Model
+{
+    protected $table    = "global_charge_port_api";
+    protected $fillable =   ['id','port_orig','port_dest','typedestiny_id','globalcharge_id'];
+
+    public $timestamps = false;
+
+    public function globalcharge()
+    {
+        return $this->belongsTo('App\GlobalChargeApi','globalcharge_id');
+    }
+
+    public function portOrig(){
+        return $this->belongsTo('App\Harbor','port_orig');
+    }
+
+    public function portDest(){
+        return $this->belongsTo('App\Harbor','port_dest');
+
+    }
+
+    public function typedestiny(){
+        return $this->belongsTo('App\TypeDestiny');
+    }
+
+    public function globalCarrier(){
+        return $this->hasManyThrough('App\GlobalCharCarrier','App\GlobalCharge','id','globalcharge_id');
+    }
+}
