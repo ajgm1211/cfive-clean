f14accbcf495900ba57aa69007853cd63507d3c0
luis <luedblra@gmail.com>
2020-02-10 08:52:44 -0400

    se crea if de comparacion de files search quoete}
    Former-commit-id: da645314a38fb0f0cf8acc213cc3cb53d74380e9 [formerly 02589c7ae30c7283405bfbc96f1db8d1218772e2]
    Former-commit-id: 5f28e1c7de7df2251b8c425394a8cba8e19655f2

--- resources/views/quotesv2/search.blade.php
+++ resources/views/quotesv2/search.blade.php
@@ -965,12 +965,20 @@
 												<div class="col-lg-2 no-padding d-flex justify-content-end align-items-center">
 													@if(($arr->excelRequest !="0") || ($arr->excelRequestFCL !="0") || ($arr->totalItems !="0") )
 													<div class="downexcel" style="margin-right: 10px;">
-														
+
+														@if($arr->idContract !="0")
+														<a  id='excel_l{{$loop->iteration}}' href="{{route('quotes-v2.excel',[$arr->excelRequest,$arr->excelRequestFCL,$arr->idContract])}}" class="l detailed-cost"  title="Cancel" >
+															<span class="workgreen"><i class="icon-excel"></i></span>
+
+															<i class="la la-file-excel-o"></i>
+														</a>
+														@else
 														<a  id='excel_l{{$loop->iteration}}' href="#" onclick="downlodRequest({{ $arr->excelRequest }},{{ $arr->excelRequestFCL }},{{ $arr->idContract }})" class="l detailed-cost"  title="Cancel" >
 															<span class="workgreen"><i class="icon-excel"></i></span>
 
 															<i class="la la-file-excel-o"></i>
 														</a>
+														@endif
 													</div>
 													@endif
 													<div class="btn-detail__quotes btn-d">
