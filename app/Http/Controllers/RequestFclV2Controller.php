6610342b34e32940af18f21475526366744bae7e
luis <luedblra@gmail.com>
2020-03-04 16:06:03 -0400

    se terminan selects
    Former-commit-id: 9999a06a5141a0001c007f969693caf38585e94a [formerly fb5b9e8a79deac6b4565caba9163f1251da82c2d]
    Former-commit-id: 39a72626957d5631b0b542b9e69093d828654112

--- app/Http/Controllers/RequestFclV2Controller.php
+++ app/Http/Controllers/RequestFclV2Controller.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use App\User;
 use App\Harbor;
+use HelperAll;
 use App\Carrier;
 use App\Contract;
+use App\Container;
 use App\Direction;
 use \Carbon\Carbon;
 use App\CompanyUser;
@@ -46,16 +48,12 @@ class RequestFclV2Controller extends Controller
 
 	public function newRequest(Request $request){
 		$carrier        = carrier::all()->pluck('name','id');
-		$direction      = [null=>'Please Select'];
-		$direction2     = Direction::all();
-		$groupContainer	= GroupContainer::pluck('name','id');
+		$direction		= HelperAll::addOptionSelect(Direction::all(),'id','name');
+		$groupContainer	= HelperAll::addOptionSelect(GroupContainer::all(),'id','name');
+		$containers		= Container::pluck('name','id');
 		$user           = \Auth::user();
-		foreach($direction2 as $d){
-			//dd($direction2);
-			$direction[$d['id']]=$d->name;
-		}
-		//dd($direction);
-		return view('RequestV2.Fcl.index',compact('carrier','user','direction','groupContainer'));
+
+		return view('RequestV2.Fcl.index',compact('carrier','user','direction','groupContainer','containers'));
 	}
 	
     public function show($id)
@@ -76,5 +74,31 @@ class RequestFclV2Controller extends Controller
     public function destroy($id)
     {
         //
+    }
+	
+	public function getContainers(Request $request){
+		$groupContainers = $request->groupContainers;
+		$containers 	 = Container::where('gp_container_id',$groupContainers)->pluck('id');
+		return response()->json(['success' => true,'data' => ['values' => $containers->all() ]]);
+	}
+	
+	public function storeMedia(Request $request)
+    {
+        $path = storage_path('tmp/uploads');
+
+        if (!file_exists($path)) {
+            mkdir($path, 0777, true);
+        }
+
+        $file = $request->file('file');
+
+        $name = uniqid() . '_' . trim($file->getClientOriginalName());
+
+        $file->move($path, $name);
+
+        return response()->json([
+            'name'          => $name,
+            'original_name' => $file->getClientOriginalName(),
+        ]);
     }
 }
