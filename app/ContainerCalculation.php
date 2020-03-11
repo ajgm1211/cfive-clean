2580d50ddda270f668f11add88472aa737204740
Luis <luedblra@gmail.com>
2020-03-11 15:04:37 -0400

    se termina crud de containers-calculations
    Former-commit-id: 8d138f1f381766fb600f85ad22d6d2ff195193e9

--- app/ContainerCalculation.php
+++ app/ContainerCalculation.php
@@ -6,14 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class ContainerCalculation extends Model
 {
-	protected $fillable = ['id','container_id','calculationtype_id',];
+    protected $fillable = ['id','container_id','calculationtype_id'];
+    public $timestamps = false;
+    public function container(){
+        return $this->belongsTo('App\Container');
+    }
 
-	public function container(){
-		return $this->belongsTo('App\Container');
-	}
-
-	public function calculationtype(){
-		return $this->belongsTo('App\CalculationType');
-	}
+    public function calculationtype(){
+        return $this->belongsTo('App\CalculationType');
+    }
 
 }
