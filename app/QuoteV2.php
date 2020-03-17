1b336b1d0557bcb1010b5340760af3e9380e7cb7
Julio <javila3090@gmail.com>
2020-03-17 19:53:48 -0300

    Working on API Integration by state feature
    Former-commit-id: e28f269451fb63db051abc2325778045057f9f56 [formerly 060adc33ffbd17c025abf8e2c4ed9f375e2c503c]
    Former-commit-id: 7569d720dd34babd642990d9dc482a7e8ddd8f12

--- app/QuoteV2.php
+++ app/QuoteV2.php
@@ -97,6 +97,11 @@ class QuoteV2 extends Model
         return $this->hasOne('App\PackageLoadV2','quote_id','id');
     }
 
+    public function integration()
+    {
+        return $this->hasOne('App\IntegrationQuoteStatus','quote_id','id');
+    }
+
     public function scopeExclude($query,$value = array()) 
     {
         return $query->select( array_diff( $this->columns,(array) $value) );
