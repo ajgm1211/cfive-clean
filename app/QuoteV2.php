0383e87ff576889503f741d4ff4a2e8805f50b7a
Alejandro Gonz <ale1211@gmail.com>
2020-03-18 15:56:11 -0400

    Se genera el pdf y se sube a s3 para el endpoint de pdf api , se crea trait para reutilizar funcionalidad
    Former-commit-id: ec14e7f38964bbbdec63a362d87cc93d7906c3dd

--- app/QuoteV2.php
+++ app/QuoteV2.php
@@ -4,10 +4,13 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\MediaLibrary\HasMedia\HasMedia;
+use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
 
-class QuoteV2 extends Model
+class QuoteV2 extends Model  implements HasMedia
 {
     use SoftDeletes;
+    use HasMediaTrait;
 
     /**
      * The attributes that should be mutated to dates.
