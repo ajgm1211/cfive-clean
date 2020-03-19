3c5a3b40a3630aa30b674b19852f3b98fa700701
Luis <luedblra@gmail.com>
2020-03-19 16:24:42 -0400

    se seigue trabajando en back... implementado: media-s3, phpoffice y se efectuan cambios de procesamiento
    Former-commit-id: 9ceac7c7530213ac4decb6c6b28e4ff8428dd89c [formerly a7d9c64fd7881f0270c13acbaaaee305b70d1be1]
    Former-commit-id: 6598e9c3528fcf0bb93b5cfc011532efd4532d4d

--- config/filesystems.php
+++ config/filesystems.php
@@ -2,7 +2,7 @@
 
 return [
 
-  /*
+    /*
     |--------------------------------------------------------------------------
     | Default Filesystem Disk
     |--------------------------------------------------------------------------
@@ -13,9 +13,9 @@ return [
     |
     */
 
-  'default' => env('FILESYSTEM_DRIVER', 'local'),
+    'default' => env('FILESYSTEM_DRIVER', 'local'),
 
-  /*
+    /*
     |--------------------------------------------------------------------------
     | Default Cloud Filesystem Disk
     |--------------------------------------------------------------------------
@@ -26,9 +26,9 @@ return [
     |
     */
 
-  'cloud' => env('FILESYSTEM_CLOUD', 's3'),
+    'cloud' => env('FILESYSTEM_CLOUD', 's3'),
 
-  /*
+    /*
     |--------------------------------------------------------------------------
     | Filesystem Disks
     |--------------------------------------------------------------------------
@@ -41,179 +41,189 @@ return [
     |
     */
 
-  'disks' => [
+    'disks' => [
 
-    'local' => [
-      'driver' => 'local',
-      'root' => public_path().'/storage',
+        'local' => [
+            'driver' => 'local',
+            'root' => public_path().'/storage',
+        ],
+
+        'public' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+        ],
+
+        'UpLoadFile' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public'),
+            'url' => 'storage',
+            'visibility' => 'public',
+        ],
+
+        //  PARA BORRAR - AUN NO
+        //    'FclAccount' => [
+        //      'driver' => 'local',
+        //      'root' => storage_path('app/public/Account/Fcl'),
+        //      'url' => 'storage/app/public/Account/Fcl',
+        //      'visibility' => 'public',
+        //    ],
+        'FclAccount' => [
+            'driver' => 's3',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'root' => '/Account/FCL',
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'bucket' => env('AWS_UPLOAD'),
+        ],  
+
+
+        'FclImport' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/Import/Fcl'),
+            'url' => storage_path('app/public/Import/Fcl'),
+            'visibility' => 'public',
+        ],
+
+        'FclRequest' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/Request/Fcl'),
+            //'url' => 'storage/app/public/Request/Fcl',
+            'url' => '/storage/Request/Fcl',
+            'visibility' => 'public',
+        ],
+
+        'LclAccount' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/Account/Lcl'),
+            'url' => 'storage/app/public/Account/Lcl',
+            'visibility' => 'public',
+        ],
+
+        'LclImport' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/Import/Lcl'),
+            'url' => 'storage/app/public/Import/Lcl',
+            'visibility' => 'public',
+        ],
+
+        'LclRequest' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/Request/Lcl'),
+            //'url' => 'storage/app/public/Request/Lcl',
+            'url' => '/storage/Request/Lcl',
+            'visibility' => 'public',
+        ],
+
+        'GCRequest' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/Request/GC'),
+            //'url' => 'storage/app/public/Request/GC',
+            'url' => '/storage/Request/GC',
+            'visibility' => 'public',
+        ],
+
+        'GCRequestLcl' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/Request/GC-LCL'),
+            //'url' => 'storage/app/public/Request/GC-LCL',
+            'url' => '/storage/Request/GC-LCL',
+            'visibility' => 'public',
+        ],
+
+        'GCAccount' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/Account/GC'),
+            'url' => 'storage/app/public/Account/GC',
+            'visibility' => 'public',
+        ],
+
+        'GCAccountLcl' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/Account/GC-LCL'),
+            'url' => 'storage/app/public/Account/GC-LCL',
+            'visibility' => 'public',
+        ],
+
+        'GCImport' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/Import/GC'),
+            'url' => 'storage/app/public/Import/GC',
+            'visibility' => 'public',
+        ],
+
+        'GCImportLcl' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/Import/GC-LCL'),
+            'url' => 'storage/app/public/Import/GC-LCL',
+            'visibility' => 'public',
+        ],
+
+        'DownLoadFile' => [
+            'driver' => 'local',
+            'root' => storage_path('app/files'),
+            'url' => 'storage',
+            'visibility' => 'public',
+        ],
+
+        'RequestFiles' => [
+            'driver' => 'local',
+            'root' => storage_path('app/exports'),
+            'url' => 'storage',
+            'visibility' => 'public',
+        ],
+
+        'image' => [
+            'driver' => 'local',
+            'root' => public_path(),
+            'url' => 'public',
+            'visibility' => 'public',
+        ],
+
+        'carriers' => [
+            'driver' => 'local',
+            'root' => public_path('imgcarrier'),
+            'url' => '/imgcarrier/',
+            'visibility' => 'public',
+        ],
+
+        'logos' => [
+            'driver' => 'local',
+            'root' => storage_path('app/logos'),
+            'url' => '/storage',
+            'visibility' => 'public',
+        ],
+
+        's3' => [
+            'driver' => 's3',
+            'key' 	  => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'bucket' => env('AWS_BUCKET'),
+        ],
+
+        's3_upload' => [
+            'driver' => 's3',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'bucket' => env('AWS_UPLOAD'),
+        ],   
+        'contracts3' => [
+            'driver' => 's3',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'root' => '/contract_manual/',
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'bucket' => env('AWS_UPLOAD'),
+        ],  
+
+        'media' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/manual'),
+            'url' => '/storage',
+            'visibility' => 'public',
+        ],
     ],
-
-    'public' => [
-      'driver' => 'local',
-      'root' => storage_path('app/public'),
-      'url' => env('APP_URL').'/storage',
-      'visibility' => 'public',
-    ],
-
-    'UpLoadFile' => [
-      'driver' => 'local',
-      'root' => storage_path('app/public'),
-      'url' => 'storage',
-      'visibility' => 'public',
-    ],
-
-    'FclAccount' => [
-      'driver' => 'local',
-      'root' => storage_path('app/public/Account/Fcl'),
-      'url' => 'storage/app/public/Account/Fcl',
-      'visibility' => 'public',
-    ],
-
-    'FclImport' => [
-      'driver' => 'local',
-      'root' => storage_path('app/public/Import/Fcl'),
-      'url' => 'storage/app/public/Import/Fcl',
-      'visibility' => 'public',
-    ],
-
-    'FclRequest' => [
-      'driver' => 'local',
-      'root' => storage_path('app/public/Request/Fcl'),
-      //'url' => 'storage/app/public/Request/Fcl',
-      'url' => '/storage/Request/Fcl',
-      'visibility' => 'public',
-    ],
-
-    'LclAccount' => [
-      'driver' => 'local',
-      'root' => storage_path('app/public/Account/Lcl'),
-      'url' => 'storage/app/public/Account/Lcl',
-      'visibility' => 'public',
-    ],
-
-    'LclImport' => [
-      'driver' => 'local',
-      'root' => storage_path('app/public/Import/Lcl'),
-      'url' => 'storage/app/public/Import/Lcl',
-      'visibility' => 'public',
-    ],
-
-    'LclRequest' => [
-      'driver' => 'local',
-      'root' => storage_path('app/public/Request/Lcl'),
-      //'url' => 'storage/app/public/Request/Lcl',
-      'url' => '/storage/Request/Lcl',
-      'visibility' => 'public',
-    ],
-
-    'GCRequest' => [
-      'driver' => 'local',
-      'root' => storage_path('app/public/Request/GC'),
-      //'url' => 'storage/app/public/Request/GC',
-      'url' => '/storage/Request/GC',
-      'visibility' => 'public',
-    ],
-
-    'GCRequestLcl' => [
-      'driver' => 'local',
-      'root' => storage_path('app/public/Request/GC-LCL'),
-      //'url' => 'storage/app/public/Request/GC-LCL',
-      'url' => '/storage/Request/GC-LCL',
-      'visibility' => 'public',
-    ],
-
-    'GCAccount' => [
-      'driver' => 'local',
-      'root' => storage_path('app/public/Account/GC'),
-      'url' => 'storage/app/public/Account/GC',
-      'visibility' => 'public',
-    ],
-
-    'GCAccountLcl' => [
-      'driver' => 'local',
-      'root' => storage_path('app/public/Account/GC-LCL'),
-      'url' => 'storage/app/public/Account/GC-LCL',
-      'visibility' => 'public',
-    ],
-
-    'GCImport' => [
-      'driver' => 'local',
-      'root' => storage_path('app/public/Import/GC'),
-      'url' => 'storage/app/public/Import/GC',
-      'visibility' => 'public',
-    ],
-
-    'GCImportLcl' => [
-      'driver' => 'local',
-      'root' => storage_path('app/public/Import/GC-LCL'),
-      'url' => 'storage/app/public/Import/GC-LCL',
-      'visibility' => 'public',
-    ],
-
-    'DownLoadFile' => [
-      'driver' => 'local',
-      'root' => storage_path('app/files'),
-      'url' => 'storage',
-      'visibility' => 'public',
-    ],
-
-    'RequestFiles' => [
-      'driver' => 'local',
-      'root' => storage_path('app/exports'),
-      'url' => 'storage',
-      'visibility' => 'public',
-    ],
-
-    'image' => [
-      'driver' => 'local',
-      'root' => public_path(),
-      'url' => 'public',
-      'visibility' => 'public',
-    ],
-
-    'carriers' => [
-      'driver' => 'local',
-      'root' => public_path('imgcarrier'),
-      'url' => '/imgcarrier/',
-      'visibility' => 'public',
-    ],
-
-    'logos' => [
-      'driver' => 'local',
-      'root' => storage_path('app/logos'),
-      'url' => '/storage',
-      'visibility' => 'public',
-    ],
-
-    's3' => [
-      'driver' => 's3',
-      'key' 	  => env('AWS_ACCESS_KEY_ID'),
-      'secret' => env('AWS_SECRET_ACCESS_KEY'),
-      'region' => env('AWS_DEFAULT_REGION'),
-      'bucket' => env('AWS_BUCKET'),
-    ],
-
-    's3_upload' => [
-      'driver' => 's3',
-      'key' => env('AWS_ACCESS_KEY_ID'),
-      'secret' => env('AWS_SECRET_ACCESS_KEY'),
-      'region' => env('AWS_DEFAULT_REGION'),
-      'bucket' => env('AWS_UPLOAD'),
-    ],   
-    'contracts3' => [
-      'driver' => 's3',
-      'key' => env('AWS_ACCESS_KEY_ID'),
-      'root' => '/contract_manual/',
-      'secret' => env('AWS_SECRET_ACCESS_KEY'),
-      'region' => env('AWS_DEFAULT_REGION'),
-      'bucket' => env('AWS_UPLOAD'),
-    ],  
-
-    'media' => [
-      'driver' => 'local',
-      'root' => storage_path('app/public/manual'),
-      'url' => '/storage',
-      'visibility' => 'public',
-    ],
-  ],
 ];
