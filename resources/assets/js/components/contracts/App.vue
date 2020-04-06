e0225aca8b31d09e2a6b650cb3185a4d4af24cb3
genesis <genesis@genesis.cantv.net>
2020-04-06 17:22:44 -0400

    api contracts view
    Former-commit-id: 3d2a48a84cc44c8fa7c3946b136a178d946cf5fd [formerly a7ad512599f11cc96b251c32799c44b2e377a6a5]
    Former-commit-id: 61a72d76457ff15e5882df919ac8fba69cd03f32

--- resources/assets/js/components/contracts/App.vue
+++ resources/assets/js/components/contracts/App.vue
@@ -1,17 +1,18 @@
 <template>
-  <div class="container bg-container">
+  <div class="container">
     <div class="row mt-5">
   <div class="col-12">
     <b-card>
     <div class="row">
       <div class="col-6"><b-card-title>FCL Contracts</b-card-title></div>
       <div class="col-6"><div class="float-right"><button class="btn btn-link">+ Add Contract</button>
-        <button class="btn btn-primary">+ Import Contract</button></div></div>
+        <button class="btn btn-primary btn-bg">+ Import Contract</button></div></div>
     </div>
 
     <div class="row my-3">
       <div class="col-4">
          <b-form inline>
+             <i class="fa fa-search" aria-hidden="true"></i>
             <b-input
               id="inline-form-input-name"
               class="mb-2 mr-sm-2 mb-sm-0"
@@ -20,8 +21,20 @@
           </b-form>
       </div>
     </div>
-    
-      <b-table borderless head-variant="light" hover :fields="fields" :items="data"></b-table>
+      <b-table borderless hover :fields="fields" :items="data" :current-page="currentPage"></b-table>
+      <b-button id="popover-button-variant" class="action-app" href="#" tabindex="0"><i class="fa fa-ellipsis-h" aria-hidden="true"></i>
+</b-button>
+  <b-popover target="popover-button-variant" class="btns-action" variant="" triggers="focus" placement="bottomleft">
+<button class="btn-action">Edit</button>
+      <button class="btn-action">Duplicate</button>
+      <button class="btn-action">Delete</button>
+       </b-popover>
+       <span class="status-st published">published</span>
+        <span class="status-st expired">expired</span>
+        <span class="status-st incompleted">incompleted</span>
+        <input type="checkbox" class="input-check" id="check">
+        <label  for="check"></label>
+      <b-pagination v-model="currentPage" :total-rows="rows" align="right"></b-pagination>
   </b-card>
     
   </div>
