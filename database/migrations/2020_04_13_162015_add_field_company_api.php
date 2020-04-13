85ee1f431be4fc5805a9220f3eaf70652e591034
Alejandro Gonz <ale1211@gmail.com>
2020-04-13 12:50:20 -0400

    nuevos campos bymaster
    Former-commit-id: cbfe7851c3e337cbbb9baa48432e9d3d128431ba

--- database/migrations/2020_04_13_162015_add_field_company_api.php
+++ database/migrations/2020_04_13_162015_add_field_company_api.php
@@ -6,23 +6,26 @@ use Illuminate\Database\Migrations\Migration;
 
 class AddFieldCompanyApi extends Migration
 {
-    /**
+  /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
-        //
-    }
+  public function up()
+  {
+    Schema::table('companies', function (Blueprint $table){
+      $table->json('options')->nullable()->after('payment_conditions');
 
-    /**
+    });
+  }
+
+  /**
      * Reverse the migrations.
      *
      * @return void
      */
-    public function down()
-    {
-        //
-    }
+  public function down()
+  {
+    //
+  }
 }
--- database/migrations/2020_04_13_162028_add_field_contact_api.php
+++ database/migrations/2020_04_13_162028_add_field_contact_api.php
@@ -6,23 +6,26 @@ use Illuminate\Database\Migrations\Migration;
 
 class AddFieldContactApi extends Migration
 {
-    /**
+  /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
-        //
-    }
+  public function up()
+  {
+    Schema::table('contacts', function (Blueprint $table){
+      $table->json('options')->nullable()->after('company_id');
 
-    /**
+    });
+  }
+
+  /**
      * Reverse the migrations.
      *
      * @return void
      */
-    public function down()
-    {
-        //
-    }
+  public function down()
+  {
+    //
+  }
 }
