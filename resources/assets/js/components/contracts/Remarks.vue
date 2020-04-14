58adecdd150abcc0aeb61d3117efceef347a6159
genesis <genesis@genesis.cantv.net>
2020-04-14 10:36:01 -0400

    contract view

---
+++ resources/assets/js/components/contracts/Remarks.vue
@@ -0,0 +1,17 @@
+<template>
+    <b-card>
+        <div class="row">
+           <div class="col-12">
+               <h6>Remarks</h6>
+           </div>
+            <div class="col-12">
+                <b-input  id="inline-form-input-name"
+                         class="mb-2 mr-sm-2 mb-sm-0 remarks"
+                         ></b-input>
+                         <span class="fa fa-repeat update-remark"></span>
+            </div>
+        </div>
+    </b-card>
+</template>
+
+
