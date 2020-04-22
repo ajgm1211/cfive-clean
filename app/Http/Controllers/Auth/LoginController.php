0f7212d73d692b181a5f8b6690ccfb7b555dad18
Julio <javila3090@gmail.com>
2020-04-22 16:09:46 -0400

    Modified Login Controller
    Former-commit-id: 82c57bbc704ab698a039b6b84c7169c2f3b32d58 [formerly 30ea1a476ce803c1ee00b5f3895edd5530eeb304]
    Former-commit-id: f8395dab6a7e39a863179d64bc2676ea69e60f70

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -98,12 +98,11 @@ class LoginController extends Controller
     }else if($user->state!=1){
       auth()->logout();
       return back()->with('warning', 'This user has been disabled.');
-    }else if(env('APP_VIEW') == 'operaciones' && !$user->hasRole('administrator')){
+    }else if(env('APP_VIEW') == 'operaciones' && ($user->hasRole('administrator') || $user->hasRole('data_entry'))){
       auth()->logout();
       return back()->with('warning', 'This user does not have administrator permission.');
     }else  if($user->company_user_id==''){
       return redirect('/settings');
-
     }
 
     return redirect()->intended($this->redirectPath());
