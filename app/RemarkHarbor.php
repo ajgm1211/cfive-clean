a9b0b790f5465ebcb265d191113de08cc35833a3
Julio <javila3090@gmail.com>
2020-04-28 15:22:05 -0400

    Fixed bug with remarks process

--- app/RemarkHarbor.php
+++ app/RemarkHarbor.php
@@ -6,13 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class RemarkHarbor extends Model
 {
-  protected $table = "remark_harbors";
-  protected $fillable = ['id', 'port_id', 'remark_condition_id'];
+    protected $table = "remark_harbors";
+    protected $fillable = ['id', 'port_id', 'remark_condition_id'];
 
-  public function remark(){
-    return $this->belongsTo('App\RemarkCondition', 'remark_condition_id');
-  }
-  public function port(){
-    return $this->belongsTo('App\Harbor', 'port_id');
-  }
+    public function remark()
+    {
+        return $this->belongsTo('App\RemarkCondition', 'remark_condition_id');
+    }
+    public function port()
+    {
+        return $this->belongsTo('App\Harbor', 'port_id');
+    }
 }
