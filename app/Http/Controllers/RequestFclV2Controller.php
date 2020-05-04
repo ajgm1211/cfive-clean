4af01c354e6d75892903ffded3e78d1a3dee2386
Luis <luedblra@gmail.com>
2020-05-04 16:55:48 -0400

    se busca de hacer cambios pero la libreria no lo permite
    Former-commit-id: 428e813eb9451dc058f46f5b363a5ed5bf4e0da5

--- app/Http/Controllers/RequestFclV2Controller.php
+++ app/Http/Controllers/RequestFclV2Controller.php
@@ -554,6 +554,7 @@ class RequestFclV2Controller extends Controller
         $groupContainers = $request->groupContainers;
         $containers 	 = Container::where('gp_container_id',$groupContainers)->where('name','!=','45 HC')->where('name','!=','40 NOR')->pluck('id');
         return response()->json(['success' => true,'data' => ['values' => $containers->all() ]]);
+        //return view('RequestV2.Fcl.select',compact('containers'));
     }
 
     public function storeMedia(Request $request){
