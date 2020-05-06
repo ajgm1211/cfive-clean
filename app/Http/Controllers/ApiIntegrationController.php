39644770c98a26162b8dbd7d68a35ce3e36cd190
Julio <javila3090@gmail.com>
2020-05-05 20:34:21 -0400

    Modified Api Integration Controller

--- app/Http/Controllers/ApiIntegrationController.php
+++ app/Http/Controllers/ApiIntegrationController.php
@@ -122,7 +122,78 @@ class ApiIntegrationController extends Controller
 
     public function getCompanies()
     {
-        SyncCompaniesJob::dispatch();
+        //SyncCompaniesJob::dispatch();
+        $api = ApiIntegrationSetting::where('company_user_id', \Auth::user()->company_user_id)->first();
+
+        $endpoint = $api->url . "ent_m?" . $api->key_name . "=" . $api->api_key;
+
+        $client = new Client([
+            'verify' => false,
+            'headers' => ['Content-Type' => 'application/json', 'Accept' => '*/*'],
+        ]);
+
+        try {
+
+            $response = $client->get($endpoint, [
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                    'X-Requested-With' => 'XMLHttpRequest',
+                ]
+            ]);
+
+            $api_response = json_decode($response->getBody());
+
+            $this->syncCompanies($api_response);
+
+            return response()->json(['message' => 'Ok']);
+        } catch (GuzzleHttp\Exception\BadResponseException $e) {
+            return "Error: " . $e;
+        }
+    }
+
+    public function syncCompanies($response)
+    {
+        $i = 0;
+        foreach ($response->ent_m as $item) {
+            if ($item->es_emp) {
+
+                $exist_com = Company::where('business_name', $item->nom_com)->get();
+                
+                if ($exist_com->count() == 0) {
+                    $company = new Company();
+                    $company->business_name = $item->nom_com;
+                    $company->phone = $item->tlf;
+                    $company->address = $item->address;
+                    $company->email = $item->eml;
+                    $company->company_user_id = \Auth::user()->company_user_id;
+                    $company->owner = \Auth::user()->id;
+                    $company->api_id = $item->id;
+                    $company->api_status = 'created';
+                    $company->save();
+
+                    /*$contacts = $this->getContacts($item->id);
+                
+                foreach($contacts->ent_rel_m as $v){
+                    $exist_cont = Contact::where('api_id',$item->ent_rel)->count();
+
+                    if($exist_cont==0){
+                        $contact = new Contact();
+                        $contact->first_name = $v->name;
+                        $contact->phone = $item->tlf;
+                        $contact->email = $item->eml;
+                        $contact->position = $v->dsc;
+                        $contact->company_id = $v->ent_rel;
+                        $contact->api_id = $v->ent_rel;
+                        $contact->save();
+                    }
+                }*/
+                }
+            }
+
+            $i++;
+        }
+
+        return 'Done';
     }
 
     public function getContacts($company_id)
