a280ad2530ee356a1ce91f30984c6429d99bca89
Alejandro G <ale1211@gmail.com>
2020-05-07 16:56:18 -0400

    delete inland range and table dinamic

--- app/Http/Controllers/InlandRangeController.php
+++ app/Http/Controllers/InlandRangeController.php
@@ -7,14 +7,26 @@ use App\Inland;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Resources\InlandRangeResource;
+use App\Container;
+use Illuminate\Support\Collection as Collection;
 
 class InlandRangeController extends Controller
 {
 
     public function list(Request $request, Inland $inland)
     {
+        $available_containers = Container::all()->pluck('code');
         $results = InlandRange::filterByInland($inland->id)->filter($request);
     	return InlandRangeResource::collection($results);
     }
+
+    public function deleteRange(InlandRange $range)
+    {
+        
+        $range->delete();
+    }
+
+
+
 
 }
