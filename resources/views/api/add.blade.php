14be6f9f7615a79abf8841f76a5561bd145333eb
Julio <javila3090@gmail.com>
2020-05-20 16:42:53 -0400

    Stand by
    Former-commit-id: d4c86fd989c253f6fa3ce7494f08727e6ec2bd96 [formerly cc66d79cd220e51c7206cd1a575d878d3a458eb3]
    Former-commit-id: 455960a71d25c8737fcebd5b98b0f3d4952314d9

---
+++ resources/views/api/add.blade.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Julio
+ * Date: 04/06/2018
+ * Time: 05:45 PM
+ */
+?>
+<div class="modal fade" id="AddIntegrationModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
+    aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content" style="min-width: 700px; right: 100px;">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">
+                    <b>Add Integration</b>
+                </h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            {!! Form::open(['route' => ['UserConfiguration.update',@$user],'method' => 'put'])!!}
+            <div class="modal-body">
+                <div class="form-group m-form__group">
+                    <label style="letter-spacing: 0.7px"><b>Name</b></label>
+                    {{ Form::text('name', null,['placeholder' => 'Please enter a name','class'=>'form-control']) }}
+                </div>
+                <div class="form-group m-form__group">
+                    <label style="letter-spacing: 0.7px"><b>URL</b></label>
+                    {{ Form::text('url', null,['placeholder' => 'Please enter an URL','class'=>'form-control']) }}
+                </div>
+                <div class="form-group m-form__group">
+                    <label style="letter-spacing: 0.7px"><b>API Key</b></label>
+                    {{ Form::text('api_key', null,['placeholder' => 'Please enter an API Key','class'=>'form-control']) }}
+                </div>
+                <hr>
+                <div class="form-group m-form__group">
+                    <button type="submit" class="btn btn-primary">Save</button>
+                    <button type="button" data-toggle="modal" data-target="#AddIntegrationModal" class="btn btn-danger">Cancel</button>
+                </div>
+                <br>
+            </div>
+            {!! Form::close()!!}
+        </div>
+    </div>
+</div>
--- resources/views/api/index.blade.php
+++ resources/views/api/index.blade.php
@@ -8,7 +8,8 @@
 
 <div class="m-content">
     @if(Session::has('message.nivel'))
-    <div class="m-alert m-alert--icon m-alert--outline alert alert-{{ session('message.nivel') }} alert-dismissible fade show" role="alert">
+    <div class="m-alert m-alert--icon m-alert--outline alert alert-{{ session('message.nivel') }} alert-dismissible fade show"
+        role="alert">
         <div class="m-alert__icon">
             <i class="la la-warning"></i>
         </div>
@@ -23,13 +24,13 @@
         </div>
     </div>
     @endif
-    {!! Form::open(['route' => ['UserConfiguration.update',@$user],'method' => 'put'])!!}
+    
     <div class="m-portlet m-portlet--mobile">
         <div class="m-portlet__head">
             <div class="m-portlet__head-caption">
                 <div class="m-portlet__head-title">
                     <h3 class="m-portlet__head-text">
-                        <b>External API Settings</b> 
+                        <b>External API Settings</b>
                     </h3>
                 </div>
             </div>
@@ -43,7 +44,8 @@
                             <div class="col-2">
                                 <label class=" col-form-label">
                                     Enable External API Integration
-                                    <input type="hidden" name="company_user_id" value="{{\Auth::user()->company_user_id}}" id="company_user_id">
+                                    <input type="hidden" name="company_user_id"
+                                        value="{{\Auth::user()->company_user_id}}" id="company_user_id">
                                 </label>
                             </div>
                         </div>
@@ -51,7 +53,8 @@
                             <div class="col-2">
                                 <span class="m-switch m-switch--icon">
                                     <label>
-                                        <input type="checkbox" name="enable" id="enable_api" {{@$api->enable==1 ? 'checked':''}}>
+                                        <input type="checkbox" name="enable" id="enable_api"
+                                            {{@$api->enable==1 ? 'checked':''}}>
                                         <span></span>
                                     </label>
                                 </span>
@@ -59,45 +62,84 @@
                         </div>
                     </div>
                 </div>
+                <hr>
                 <!--begin: Search Form -->
-                <div class="{{@$api->enable==1 ? '':'hide'}}" id="api-table">
+                <!--<div class="{{@$api->enable==1 ? '':'hide'}}" id="api-table">
                     <hr>
                     <div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
                         <div class="row align-items-center">
                             <div class="col-xl-3">
                                 <div class="input-group mb-3">
-                                    <input type="text" class="form-control" id="api_key" name="api_key" value="{{@$api->api_key}}" placeholder="Api key" aria-label="Api key" aria-describedby="basic-addon2">
+                                    <input type="text" class="form-control" id="api_key" name="api_key"
+                                        value="{{@$api->api_key}}" placeholder="Api key" aria-label="Api key"
+                                        aria-describedby="basic-addon2">
                                 </div>
                             </div>
                             <div class="col-xl-3">
                                 <div class="input-group mb-3">
-                                    <input type="text" class="form-control" id="key_name" name="key_name" value="{{@$api->key_name}}" placeholder="Key name" aria-label="Key name" aria-describedby="basic-addon2">
+                                    <input type="text" class="form-control" id="key_name" name="key_name"
+                                        value="{{@$api->key_name}}" placeholder="Key name" aria-label="Key name"
+                                        aria-describedby="basic-addon2">
                                 </div>
                             </div>
                             <div class="col-xl-4">
                                 <div class="input-group mb-3">
-                                    <input type="text" class="form-control" id="url" name="url" value="{{@$api->url}}" placeholder="URL" aria-label="URL" aria-describedby="basic-addon2">
+                                    <input type="text" class="form-control" id="url" name="url" value="{{@$api->url}}"
+                                        placeholder="URL" aria-label="URL" aria-describedby="basic-addon2">
                                 </div>
                             </div>
                         </div>
                         <br>
                         <div class="row align-items-center">
                             <div class="col-xl-3">
-                                <button class="btn btn-primary" id="store_api_key" type="button">Save <i class="fa fa-save"></i></button>
+                                <button class="btn btn-primary" id="store_api_key" type="button">Save <i
+                                        class="fa fa-save"></i></button>
                             </div>
                         </div>
+                    </div>
+                </div>-->
+                <div class="row">
+                    <div class="col-4">
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#AddIntegrationModal">Add Integration &nbsp;<i class="fa fa-plus"></i></button>
+                    </div>
+                </div>
+                <br>
+                <div class="row">
+                    <div class="col-12">
+                        <table class="m-datatable" id="html_table" width="100%">
+                            <thead>
+                                <tr>
+                                    <th title="Field #1">
+                                        Name
+                                    </th>
+                                    <th title="Field #2">
+                                        URL
+                                    </th>
+                                    <th title="Field #3">
+                                        API Key
+                                    </th>
+                                    <th title="Field #4">
+                                        Options
+                                    </th>
+
+                                </tr>
+                            </thead>
+                            <tbody>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    {!! Form::close()!!}
+    
 </div>
-
+@include('api.add')
 @endsection
 
 @section('js')
 @parent
 <script src="{{asset('js/api-settings.js')}}" type="text/javascript"></script>
-<script src="/assets/demo/default/custom/components/datatables/base/html-table-oauth.js" type="text/javascript"></script>
+<script src="/assets/demo/default/custom/components/datatables/base/html-table-oauth.js" type="text/javascript">
+</script>
 @stop
