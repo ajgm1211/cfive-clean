fcfa34aa3aaeee5de6cc26b47538f8db827ceb44
Alejandro G <ale1211@gmail.com>
2020-05-25 13:06:22 -0400

    Se comenta el meta para trabajar en dev
    Former-commit-id: 72b0ab486b26a4b173c3d2d772cc95497340e920

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -7,7 +7,8 @@
   <meta name="description" content="Latest updates and statistic charts">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
+  
+  <!-- <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> -->
 
   @section('css')
 
