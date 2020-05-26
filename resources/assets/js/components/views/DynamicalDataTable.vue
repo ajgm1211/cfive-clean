e793a68d5c411f49263f73ae54535d1134b5ff2b
David <dcabanales@gmail.com>
2020-05-25 21:35:15 -0400

    Inland Updated
    Former-commit-id: 6085bdc69c9d143c3ce4130d5ec862047d58c5ad [formerly 3b38037ddff8844ca2d4a2e28b3092865ee3ea80]
    Former-commit-id: 6c5d7d40658af8018e361effe02a8d15ff86e333

--- resources/assets/js/components/views/DynamicalDataTable.vue
+++ resources/assets/js/components/views/DynamicalDataTable.vue
@@ -6,7 +6,9 @@
         :inputFields="form_fields"
         :vdatalists="datalists"
         :actions="actions"
+        :massiveactions="['changecontainersview', 'delete']"
         @onEdit="onEdit"
+        @onChangeContainersView="onChangeContainersView"
         >
     </DataTable>
 
@@ -26,13 +28,19 @@
             datalists: Object,
             actions: Object,
             initialFields: Array,
-            initialFormFields: Object
+            initialFormFields: Object,
+            onLast: {
+                type: Boolean,
+                default: false,
+                required: false
+            }
         },
         data() {
             return {
                 fields: [],
                 form_fields: {},
                 isLoaded: true,
+                extra_field_state: false
             }
         },
         created() { 
@@ -62,9 +70,49 @@
 
             },
 
+            /* Set middle fields */
+            setMiddleColumns(equipment, component){
+                
+                let rate = '';
+                let containers = {};
+                
+                this.datalists.containers.forEach(function(item){
+                    
+                    if(item.gp_container_id === equipment.id)
+                    {
+                        rate = 'rates_'+item.code;
+                        component.fields.push({ key: rate, label: item.name });
+                        component.form_fields[rate] = { type: 'text', label: item.name, placeholder: item.name };
+                        containers[rate] = { type: 'text', label: item.name, placeholder: item.name };
+                    }
+
+                });
+
+                return containers;
+
+            },
+
+            /* Set extra field */
+            setExtraColumns(equipment, component){
+
+                let containers = {};
+
+                component.fields.push({ key: 'per_container', label: 'Per container '+equipment.name });
+                component.form_fields['per_container'] = { type: 'text', label: 'Per container '+equipment.name, placeholder: '0' };
+                containers['per_container'] = { type: 'text', label: 'Per container'+equipment.name, placeholder: '0' };
+
+                return containers;
+
+            },
+
             /* Set lasts fields */
             setLastColumns(component){
-                let fields = this.initialFields.slice(2);
+                let fields = [];
+
+                if(this.initialFields.length > 1)
+                    fields = this.initialFields.slice(2);
+                else
+                    fields = this.initialFields;
 
                 fields.forEach(function (item){
                     component.fields.push(item);
@@ -79,31 +127,31 @@
                 this.resetValues();
 
                 let component = this;
-
-                this.setFirstColumns(component);
-
-                let rate = '';
                 let containers = {};
 
-                this.datalists.containers.forEach(function(item){
-                    if(item.gp_container_id === equipment.id)
-                    {
-                        rate = 'rates_'+item.code;
-                        component.fields.push({ key: rate, label: item.name });
-                        component.form_fields[rate] = { type: 'text', label: item.name, placeholder: item.name };
-                        containers[rate] = { type: 'text', label: item.name, placeholder: item.name };
-                    }
-                });
+                if(!this.onLast)
+                    this.setFirstColumns(component);
+
+                if(this.extra_field_state)
+                    containers = this.setExtraColumns(equipment, component);
+                else
+                    containers = this.setMiddleColumns(equipment, component);
 
                 this.setLastColumns(component);
                 this.isLoaded = true;
                 this.$emit('onFormFieldUpdated', containers);
             },
+            onChangeContainersView(value){
+                this.extra_field_state = !this.extra_field_state;
+            }
            
         },
         watch: {
             equipment: function(val, oldVal) {
                 this.setColumns(val)
+            },
+            extra_field_state: function(val, oldVal) {
+                this.setColumns(this.equipment)
             }
         }
     }
