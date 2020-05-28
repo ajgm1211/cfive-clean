7128b0028569ae4bfe0e0d5b2228d0b652c7daea
Alejandro G <ale1211@gmail.com>
2020-05-28 10:35:30 -0400

    Archivos dev
    Former-commit-id: e0d1bde3f0d13977e24ac3ba9f1e48fb685510b7 [formerly 2819b93e7776f8d9984ccabd26e0c821c164c47c]
    Former-commit-id: 203e5c814f33c0c70a4c7b584eec2b84cb21e898

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -8,7 +8,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   
-  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
+  <!-- <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> -->
 
   @section('css')
 
