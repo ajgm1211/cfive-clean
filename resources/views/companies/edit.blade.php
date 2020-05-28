8904640644f48ea913014949ec19cd9ba0f1670d
Julio <javila3090@gmail.com>
2020-05-28 19:29:43 -0400

    Added options field to company add and edit process
    Former-commit-id: b338b2c1bc8331474d757dc2c1ce306da95330fd

--- resources/views/companies/edit.blade.php
+++ resources/views/companies/edit.blade.php
@@ -25,9 +25,80 @@
         <div class="form-group m-form__group">
             {!! Form::label('logo', 'Logo (Max size 1 mb)') !!}
             <br>
-            {!! Form::file('logo', null, ['placeholder' => 'Please upload a logo','class' => 'form-control m-input logo_input','required' => 'required','id'=>'logo']) !!}
+            {!! Form::file('logo', null, ['placeholder' => 'Please upload a logo','class' => 'form-control m-input
+            logo_input','required' => 'required','id'=>'logo']) !!}
             <div id="logo-error" class="hide"><b>Image size can not be bigger than 1 mb</b></div>
-        </div>        
+        </div>
+        <br>
+        <hr>
+        <div class="form-group m-form__group">
+            <div class="row">
+                <div class="col-12">
+                    {!! Form::label('extra_fields', 'Extra fields') !!}
+                    <button type="button" class="btn btn-primary btn-sm pull-right" onclick="addExtraField()">Add <i
+                            class="fa fa-plus"></i></button>
+                </div>
+            </div>
+            <br>
+            @forelse(json_decode($company->options, true) as $key=>$value)
+                <div class="clone">
+                    <div class="row">
+                        <div class="col-6">
+                            {!! Form::text('key_name[]', $key, ['placeholder' => 'Please enter a key name','class' =>
+                            'form-control
+                            m-input']) !!}
+                        </div>
+                        <div class="col-6">
+                            <div class="input-group mb-3">
+                                {!! Form::text('key_value[]', $value, ['placeholder' => 'Please enter a value','class' =>
+                                'form-control
+                                m-input']) !!}
+                                <div class="input-group-append">
+                                    <a href="#" class="btn btn-danger btn-sm deleter"><i class="fa fa-close"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @empty
+                <div class="row">
+                    <div class="col-6">
+                        {!! Form::text('key_name[]', null, ['placeholder' => 'Please enter a key name','class' =>
+                        'form-control
+                        m-input']) !!}
+                    </div>
+                    <div class="col-6">
+                        <div class="input-group mb-3">
+                            {!! Form::text('key_value[]', null, ['placeholder' => 'Please enter a value','class' =>
+                            'form-control
+                            m-input']) !!}
+                            <div class="input-group-append">
+                                <a href="#" class="btn btn-danger btn-sm deleter"><i class="fa fa-close"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforelse
+            <div class="hide" id="hide_extra_field">
+                <div class="row" style="margin-top:3px;">
+                    <div class="col-6">
+                        {!! Form::text('key_name[]', null, ['placeholder' => 'Please enter a key name','class' =>
+                        'form-control
+                        m-input']) !!}
+                    </div>
+                    <div class="col-6">
+                        <div class="input-group mb-3">
+                            {!! Form::text('key_value[]', null, ['placeholder' => 'Please enter a value','class' =>
+                            'form-control
+                            m-input']) !!}
+                            <div class="input-group-append">
+                                <a href="#" class="btn btn-danger btn-sm deleter"><i class="fa fa-close"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 <div class="m-portlet__foot m-portlet__foot--fit">
@@ -51,6 +122,6 @@
 </script>
 <script src="{{asset('js/base.js')}}" type="text/javascript"></script>
 <script src="{{asset('js/companies.js')}}"></script>
-<script src="/assets/demo/default/custom/components/forms/widgets/bootstrap-daterangepicker.js" type="text/javascript"></script>
+<script src="/assets/demo/default/custom/components/forms/widgets/bootstrap-daterangepicker.js" type="text/javascript">
+</script>
 <script src="/assets/demo/default/custom/components/forms/widgets/select2.js" type="text/javascript"></script>
-
