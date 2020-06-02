0d8d5342049400143a37de3384535ce54a72404a
David <dcabanales@gmail.com>
2020-06-02 05:41:21 -0400

    Inland Module Finished
    Former-commit-id: 1c0c6a3694dc4adedf2863a1c501b467f73b6eda [formerly b4e2202859f99451e85fbbb22a77586cece9c6dd]
    Former-commit-id: 64dcc31450c4c0b292105f6512d18c2947e1aa36

--- app/Inland.php
+++ app/Inland.php
@@ -14,70 +14,81 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 
-
 class Inland extends Model implements Auditable
 {
-  use \OwenIt\Auditing\Auditable;
+	use \OwenIt\Auditing\Auditable;
 
-  protected $table    = "inlands";
-  protected $fillable =   [
-                            'id', 
-                            'reference', 
-                            'status', 
-                            'inland_type_id', 
-                            'validity', 
-                            'expire', 
-                            'company_user_id', 
-                            'gp_container_id', 
-                            'direction_id' 
-                          ];
+	protected $table    = "inlands";
+	protected $fillable =   [
+		'id', 
+		'provider', 
+		'status', 
+		'inland_type_id', 
+		'validity', 
+		'expire', 
+		'company_user_id', 
+		'gp_container_id', 
+		'direction_id' 
+	];
 
-  public function inlandadditionalkms(){
+	public function inlandRange()
+	{
+		return $this->hasMany('App\InlandRange');
+	}
 
-    return $this->hasOne('App\InlandAdditionalKm');
+	public function companyUser()
+	{
+		return $this->belongsTo('App\CompanyUser');
+	}
 
-  }
-  public function inlandports(){
+	public function inland_type()
+	{
+		return $this->belongsTo('App\CompanyUser');
+	}
 
-    return $this->hasMany('App\InlandPort');
+	public function direction()
+	{
+		return $this->belongsTo('App\Direction');
+	}
 
-  }
-  public function inlandRange(){
+	public function inland_company_restriction()
+	{
+		return $this->HasMany('App\InlandCompanyRestriction');
+	}
 
-    return $this->hasMany('App\InlandRange');
+	public function scopeFilterByCurrentCompany( $query )
+	{
+		$company_id = Auth::user()->company_user_id;
 
-  }
-  public function inlanddetails(){
+		return $query->where('company_user_id', '=', $company_id);
+	}
 
-    return $this->hasMany('App\InlandDetail');
-
-  }
-  public function companyUser()
-  {
-    return $this->belongsTo('App\CompanyUser');
-  }
-  public function inland_company_restriction(){
-
-    return $this->HasMany('App\InlandCompanyRestriction');
-
-  }
-
-  public function scopeFilterByCurrentCompany( $query )
-  {
-
-    $company_id = Auth::user('web')->company_user_id;
-  
-    return $query->where('company_user_id', '=',$company_id);
-  }
-
-  public function gpContainer()
+	public function gpContainer()
 	{
 		return $this->belongsTo('App\GroupContainer');
-  }
-  public function scopeFilter(Builder $builder, Request $request)
+	}
+
+	public function scopeFilter(Builder $builder, Request $request)
 	{
 		return (new InlandFilter($request, $builder))->filter();
 	}
 
+	/**
+	* Sync Inland Company Restrictions
+	*
+	* @param  Array $companies
+	* @return void
+	*/
+	public function InlandRestrictionsSync($companies)
+	{
+		DB::table('inlands_company_restrictions')->where('inland_id', '=', $this->id)->delete(); 
+
+		foreach($companies as $company_id){
+			InlandCompanyRestriction::create([
+				'company_id'    => $company_id,
+				'inland_id'   => $this->id
+			]);
+		}
+	}
 
 }
