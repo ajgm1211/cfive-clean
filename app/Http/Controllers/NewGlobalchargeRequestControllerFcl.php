0b9e05f9645b13af6b85fbb963affac355f4cec6
Luis <luedblra@gmail.com>
2020-06-02 18:07:39 -0400

    se agrega configuracion de equiment a globals

--- app/Http/Controllers/NewGlobalchargeRequestControllerFcl.php
+++ app/Http/Controllers/NewGlobalchargeRequestControllerFcl.php
@@ -7,6 +7,7 @@ use App\Harbor;
 use App\Carrier;
 use Carbon\Carbon;
 use App\CompanyUser;
+use App\GroupContainer;
 use Illuminate\Http\Request;
 use App\Jobs\NotificationsJob;
 use App\Notifications\N_general;
@@ -35,13 +36,13 @@ class NewGlobalchargeRequestControllerFcl extends Controller
 		return view('RequestGlobalChargeFcl.indexClient',compact('company_userid'));
 	}
 
-
 	public function create()
 	{
-		$harbor         = harbor::all()->pluck('display_name','id');
-		$carrier        = carrier::all()->pluck('name','id');
-		$user   = \Auth::user();
-		return view('RequestGlobalChargeFcl.NewRequest',compact('harbor','carrier','user'));
+		$harbor       = harbor::all()->pluck('display_name','id');
+		$carrier      = carrier::all()->pluck('name','id');
+        $equiments    = GroupContainer::pluck('name','id');
+		$user         = \Auth::user();
+		return view('RequestGlobalChargeFcl.NewRequest',compact('harbor','carrier','user','equiments'));
 	}
 
 	public function listClient($id){
@@ -194,17 +195,21 @@ class NewGlobalchargeRequestControllerFcl extends Controller
 		$file   = $request->file('file');
 		$ext    = strtolower($file->getClientOriginalExtension());
 		//obtenemos el nombre del archivo
-		$nombre = $file->getClientOriginalName();
-		$nombre = $now.'_'.$nombre;
+		$nombre   = $file->getClientOriginalName();
+		$nombre   = $now.'_'.$nombre;
+        $equiment = GroupContainer::with('containers')->find($request->groupContainers);
+        $containers_ar = $equiment->containers->pluck('code','id');
+        $ArrayData['containers'] = [];
+        foreach($containers_ar as $key => $container){
+            $ArrayData['containers'][] = ['id' => $key,'name'=>$container];
+        }
+        $ArrayData['group_containers'] = ['id' => $equiment->id,'name'=>$equiment->name];
+       // dd(json_encode($ArrayData));
+        
 		$fileBoll = \Storage::disk('GCRequest')->put($nombre,\File::get($file));
-
-		$typeVal = 1;
+		$typeVal  = 1;
 		$arreglotype = '';
 
-		$data	= '';
-		$type	= '';
-		$type	= json_encode($type);
-		$data	= json_encode($data);
 		if($fileBoll){
 			$Ncontract  = new NewGlobalchargeRequestFcl();
 			$Ncontract->name			    = $request->name;
@@ -213,8 +218,8 @@ class NewGlobalchargeRequestControllerFcl extends Controller
 			$Ncontract->namefile        = $nombre;
 			$Ncontract->user_id         = $request->user;
 			$Ncontract->created         = $now2;
-			$Ncontract->type            = $type;
-			$Ncontract->data            = $data;
+			$Ncontract->type            = '{}';
+			$Ncontract->data            = json_encode($ArrayData);
 			$Ncontract->save();
 
 			if(env('APP_VIEW') == 'operaciones') {
@@ -236,10 +241,6 @@ class NewGlobalchargeRequestControllerFcl extends Controller
 			]);
 
 			foreach($admins as $userNotifique){
-				/*\Mail::to($userNotifique->email)->send(new NewRequestGlobalChargeToAdminMail(
-					$userNotifique->toArray(),
-					$user->toArray(),
-					$Ncontract->toArray()));*/
 				$userNotifique->notify(new N_general($user,$message));
 			}
 
@@ -290,19 +291,15 @@ class NewGlobalchargeRequestControllerFcl extends Controller
 
 	}
 
-
-
 	public function edit($id)
 	{
 		//
 	}
 
-
 	public function update(Request $request, $id)
 	{
 		//
 	}
-
 
 	public function destroy($id)
 	{
