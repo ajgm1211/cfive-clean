9d84e4fd5e8abe57164c985ee6a9a25ee8f04d9a
Julio <javila3090@gmail.com>
2020-06-03 15:26:17 -0400

    Added Form Request to Remarks
    Former-commit-id: f266d1900812353d742575cc1ee905f4e59004df [formerly f99718f4124a595656a1f99b617395acc8364191]
    Former-commit-id: 7f5ca42235863d5b2194706fac31e0b6e35cd142

--- resources/views/remarks/add.blade.php
+++ resources/views/remarks/add.blade.php
@@ -1,10 +1,21 @@
 @extends('layouts.app')
-@section('title', 'Add new term & condition')
+@section('title', 'Add Remark')
 @section('content')
 <div class="m-portlet">
+
+
     <!--begin::Form-->
     {!! Form::open(['route' => 'remarks.store']) !!}
     <div class="m-portlet__body">
+        @if ($errors->any())
+            <div class="alert alert-danger alert-dismissable alert-dismissible">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
         <div class="m-form__section m-form__section--first">
             <div class="form-group m-form__group">
                 @include('remarks.partials.form_terms')
@@ -14,7 +25,7 @@
             <br>
             <div class="m-form__actions m-form__actions">
                 {!! Form::submit('Save', ['class'=> 'btn btn-primary']) !!}
-                <a class="btn btn-danger" href="{{url()->previous()}}">
+                <a class="btn btn-danger" href="/remarks/list">
                     Cancel
                 </a>
             </div>
@@ -29,9 +40,8 @@
 @section('js')
 @parent
 <script>
-   $('.m-select2-general').select2({
+    $('.m-select2-general').select2({
        placeholder: "Select an option"
    });
 </script>
 @stop
-
