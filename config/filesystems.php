1b422063260e21a3740c622317bdf999d128b08c
Luis <luedblra@gmail.com>
2020-06-09 16:07:33 -0400

    se agrega visivilidad publica

--- config/filesystems.php
+++ config/filesystems.php
@@ -93,6 +93,7 @@ return [
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
             'region' => env('AWS_DEFAULT_REGION'),
             'bucket' => env('AWS_UPLOAD'),
+            'visibility' => 'public',
         ],
 
 // FCL -------------------------------------------------
