9de4c414f8007cbcc5ba252113eaa82732332629
luis avila <luisaviloreto@gmail.com>
2020-06-10 17:06:52 -0400

    Added form request to store email template
    Former-commit-id: c07a0cd4b89214b9195b1e92e1b9d240f7ccff0c [formerly cabb22c97fc850fe35b4c6733723643bbd25786f]
    Former-commit-id: 6ba7fbcad93c81d09de63bd46af112f61c833a70

--- resources/views/users/indexhtml.blade.php
+++ resources/views/users/indexhtml.blade.php
@@ -12,7 +12,17 @@
                     </div>
                 </div>
             </div>
-
+            <br>
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+            
             @if(Session::has('message.nivel'))
                 <div class="col-md-12">
                     <br>
