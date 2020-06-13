bb868e62592489ebd02a7d47827552a7b09f0196
David <dcabanales@gmail.com>
2020-06-13 16:05:07 -0400

    Changes Updated
    Former-commit-id: f2ad0a063511cb21c0ba04ab95bbe97ed294e903 [formerly 47736715ddede0242d942c4872dad7380ffc3381]
    Former-commit-id: 209bcd72cf52d1b7820b8f5b517d8e11ced2799e

--- app/Http/Resources/LocalChargeResource.php
+++ app/Http/Resources/LocalChargeResource.php
@@ -14,16 +14,16 @@ class LocalChargeResource extends JsonResource
      */
     public function toArray($request)
     {
-        if($ports = $this->localcharports->count() > 0){
+        if($this->localcharports->count() > 0){
             $ports = $this->localcharports;
-            $origin = $ports->pluck('portOrig')->unique('name');
-            $destination = $ports->pluck('portDest')->unique('name');
+            $origin = $ports->pluck('portOrig')->unique('id');
+            $destination = $ports->pluck('portDest')->unique('id');
             $route = [ 'id' => 'port', 'name' => 'Port', 'vselected' => 'harbors' ];
         }
         else {
             $countries = $this->localcharcountries;
-            $origin = $countries->pluck('countryOrig')->unique('name')->renameIndex('name', 'display_name');
-            $destination = $countries->pluck('countryDest')->unique('name')->renameIndex('name', 'display_name');
+            $origin = $countries->pluck('countryOrig')->unique('id')->renameIndex('name', 'display_name');
+            $destination = $countries->pluck('countryDest')->unique('id')->renameIndex('name', 'display_name');
 
             $route = [ 'id' => 'country', 'name' => 'Country', 'vselected' => 'countries' ];
         }
