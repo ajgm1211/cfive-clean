46057027fced374806d3c5aafa960a55b9029abc
David <dcabanales@gmail.com>
2020-06-15 23:52:56 -0400

    LocalCharge Updated
    Former-commit-id: 0f02c7de47a5d637f8609620bd096b8ba9c9b87d [formerly f591884153ae32324caf9d6a4804793ee5710f92]
    Former-commit-id: 5f0db269edbf2370e208d3b22b8fcdb0393a6dbd

--- app/Http/Resources/LocalChargeResource.php
+++ app/Http/Resources/LocalChargeResource.php
@@ -14,16 +14,25 @@ class LocalChargeResource extends JsonResource
      */
     public function toArray($request)
     {
-        if($ports = $this->localcharports->count() > 0){
+        if($this->localcharports->count() > 0){
             $ports = $this->localcharports;
-            $origin = $ports->pluck('portOrig')->unique('id');
-            $destination = $ports->pluck('portDest')->unique('id');
+            $origin = $ports->pluck('portOrig')->unique('id')->values();
+            $destination = $ports->pluck('portDest')->unique('id')->values();
             $route = [ 'id' => 'port', 'name' => 'Port', 'vselected' => 'harbors' ];
         }
         else {
             $countries = $this->localcharcountries;
-            $origin = $countries->unique('id')->pluck('countryOrig');
-            $destination = $countries->unique('id')->pluck('countryDest');
+
+            $origin = $countries->pluck('countryOrig')->unique('id')->values()->map(function ($country) {
+                $country['display_name'] = $country['name'];
+                return $country->only(['id', 'display_name', 'name']);
+            });
+
+            $destination = $countries->pluck('countryDest')->unique('id')->values()->map(function ($country) {
+                $country['display_name'] = $country['name'];
+                return $country->only(['id', 'display_name', 'name']);
+            });
+
             $route = [ 'id' => 'country', 'name' => 'Country', 'vselected' => 'countries' ];
         }
 
