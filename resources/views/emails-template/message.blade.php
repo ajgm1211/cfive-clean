995e786e91a00d10c52f3f9aa0ca23bc61af9bdd
Julio <javila3090@gmail.com>
2020-06-15 20:04:50 -0400

    Fixed bug in Email templates store and edit
    Former-commit-id: 3c675a407e50b20e95f4efb7816c13e858471f89 [formerly b507653878301c69af61e0a2f57a0fa407a3cdf2]
    Former-commit-id: 64ed65bdb7c7f1cc0a2d53ca18219cc1c003b355

--- resources/views/emails-template/message.blade.php
+++ resources/views/emails-template/message.blade.php
@@ -1,20 +1,20 @@
 <!--begin::Form-->
 {!! Form::open(['route' => ['delete-emails-template', $id],'method' => 'PUT']) !!}
-   
-   <div class="m-form__section m-form__section--first">
-       <div class="form-group m-form__group">
-          are you sure you want to delete the template?
 
-       </div>
-   </div>
-   <div class="m-portlet__foot m-portlet__foot--fit">
-       <div class="m-form__actions m-form__actions">
-           {!! Form::submit('Yes', ['class'=> 'btn btn-primary  btn-sm']) !!}
-           <a class="btn btn-success btn-sm" href="{{url()->previous()}}">
-               No
-           </a>
-       </div>
-   </div>
+<div class="m-form__section m-form__section--first">
+    <div class="form-group m-form__group">
+        <h5>Are you sure?</h5>
+    </div>
+</div>
+<br>
+
+<div class="m-form__actions m-form__actions">
+    {!! Form::submit('Yes', ['class'=> 'btn btn-primary']) !!}
+    <a class="btn btn-danger" href="{{url()->previous()}}">
+        No
+    </a>
+</div>
+<br>
 
 {!! Form::close() !!}
 <!--end::Form-->
