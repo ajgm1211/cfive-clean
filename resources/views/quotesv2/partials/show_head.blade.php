aeeb088ea3d4641c8f22d2ed1f1860f1efd9f972
Julio <javila3090@gmail.com>
2020-06-17 08:36:46 -0400

    Change status winner by win
    Former-commit-id: 8624b32aebf20b63dd16e8e4eac77596d0135e34

--- resources/views/quotesv2/partials/show_head.blade.php
+++ resources/views/quotesv2/partials/show_head.blade.php
@@ -90,7 +90,7 @@
                         <div class="col-md-4">
                             <br>
                             <label class="title-quote"><b>Status:&nbsp;&nbsp;</b></label>
-                            {{ Form::select('status',['Draft'=>'Draft','Winner'=>'Winner','Sent'=>'Sent'],$quote->status,['class'=>'form-control status select2','hidden','']) }}
+                            {{ Form::select('status',['Draft'=>'Draft','Win'=>'Win','Sent'=>'Sent'],$quote->status,['class'=>'form-control status select2','hidden','']) }}
                             <span class="status_span Status_{{$quote->status}}" style="border-radius: 10px;">{{$quote->status!='' ? $quote->status:'Winner' }} <i class="fa fa-check"></i></span>
                         </div>
                         @if($quote->type!='AIR')
