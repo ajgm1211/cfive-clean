ad55304c58602ea10943a9ec2e01848646d8da8f
Alejandro G <ale1211@gmail.com>
2020-06-18 12:22:21 -0400

    Solapamiento y ordenamiento

--- resources/views/quotesv2/search.blade.php
+++ resources/views/quotesv2/search.blade.php
@@ -1261,8 +1261,8 @@
                           <span class="portalphacode" style="margin-right:15px; white-space:nowrap"> >  Via: </span> {{  $arr->via }}
                         </div>-->
                         @endif
-                        <div class="col-lg-3 d-flex align-items-center justify-content-end">
-                        <span class="portalphacode" style="margin-right:15px; text-align: right">Contract: </span>  <span class="truncate" title="{{$arr->contract->name}}">{{ $arr->contract->name }}</span> / {{ $arr->contract->number }}
+                        <div class="col-lg-3 d-flex align-items-center justify-content-start">
+                        <span class="portalphacode" style="margin-right:15px; text-align: right">Contract: </span> {{ $arr->contract->company_user_id }} &nbsp;<span class="truncate" title="{{$arr->contract->name}}">{{ $arr->contract->name }}</span> / {{ $arr->contract->number }}
                         </div>
                        
                         <div class="col-lg-1 no-padding d-flex justify-content-end align-items-center">
