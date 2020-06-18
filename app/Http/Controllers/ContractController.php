4cb4b759d1bb235b295060a711f43e220d014d08
Julio <javila3090@gmail.com>
2020-06-18 17:26:45 -0400

    Added public-read acl when upload contracts files
    Former-commit-id: 2208514ba5d6df1eaf5e8efee808fe4306389c95 [formerly 15a9275d4a7b0b2a944ad8ea0982b2b291ede214]
    Former-commit-id: a936da17f0d49a108d4d9cbd1fbd69a926907f70

--- app/Http/Controllers/ContractController.php
+++ app/Http/Controllers/ContractController.php
@@ -346,7 +346,9 @@ class ContractController extends Controller
 
         $file->move($path, $name);
 
-        $media = $contract->addMedia(storage_path('tmp/uploads/' . $name))->toMediaCollection('document', 'contracts3');
+        $media = $contract->addMedia(storage_path('tmp/uploads/' . $name))->addCustomHeaders([
+            'ACL' => 'public-read'
+        ])->toMediaCollection('document', 'contracts3');
 
         return response()->json([
             'contract' => new ContractResource($contract),
