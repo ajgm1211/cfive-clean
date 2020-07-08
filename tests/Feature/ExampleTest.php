e795142880012028636234837feece55acb67798
luis avila <luisaviloreto@gmail.com>
2020-07-08 09:39:44 -0400

    Added tests
    Former-commit-id: 2e08e92de4d9515b60202e66314f84553c614552 [formerly 3276ba90092ac942f4478e67c3f6a27b447436db]
    Former-commit-id: 567a5f6ffe691a927b436def753934e1f3d36657

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,21 +1,25 @@
 <?php
 
-namespace Tests\Feature;
+// namespace Tests\Feature;
 
-use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+// use Tests\TestCase;
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+// use Tests\Feature\ExampleTest;
+// use Illuminate\Auth\AuthenticationException;
 
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
-    }
-}
+// class ExampleTest extends TestCase
+// {
+//     /**
+//      * A basic test example.
+//      *
+//      * @return void
+//      */
+//     public function testBasicTest()
+//     {
+//         $code=200;
+//         $response = $this->get('/v2/quotes/search');
+
+//          $response->assertResponseStatus($code); // con assertStatus me d error , pero con assertResponseStatus no :( 
+//     }
+// }
---
+++ tests/Unit/contactsTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Contact;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ContacsTest extends TestCase
+{
+    // use DatabaseTransactions;
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testcontacts()
+    {
+      $contacts = factory(Contact::class)->create();
+       $this->seeInDatabase('contacts', $contacts->toArray());
+
+    }
+
+
+    public function testcontactsupdated()
+    {      
+        $model = factory(Contact::class)->create();
+
+        $contacts = Contact::find($model->id);
+
+        $contacts->first_name='luisito';
+
+        $contacts->update();
+
+        // $model
+     
+        $this->seeInDatabase('contacts', [
+            'first_name' => 'luisito']);
+
+    }
+
+}
