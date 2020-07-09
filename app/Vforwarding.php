7e7b669e1ba385cecf1d02bacc7cabd4f62de0c2
Julio <javila3090@gmail.com>
2020-07-08 20:19:54 -0400

    Modified integration api process
    Former-commit-id: 217c59d6ae95d596a523d856dcc0019de1f08c1c

---
+++ app/Vforwarding.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App;
+
+use App\Jobs\SyncCompaniesJob;
+use App\Partner;
+use App\Http\Requests\StoreApiIntegration;
+use GuzzleHttp\Client;
+
+class Vforwarding
+{
+    public function getData($client, $endpoint, $setting)
+    {
+        try {
+
+            $response = $client->get($endpoint);
+
+            $type = $response->getHeader('content-type');
+
+            $type = explode(';', $type[0]);
+
+            $api_response = $response->getBody()->getContents();
+
+            if ($type[1] == 'charset=iso-8859-1') {
+                $api_response = iconv("iso-8859-1", "UTF-8", $api_response);
+            }
+
+            $result = json_decode($api_response, true);
+
+            SyncCompaniesJob::dispatch($result, \Auth::user(), $setting->partner);
+
+            return true;
+
+        } catch (\Exception $e) {
+            \Log::error($e->getMessage());
+            return false;
+        }
+    }
+}
