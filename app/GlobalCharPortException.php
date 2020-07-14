9ed7e6261faea1895c66d268293517f6355cbe9d
Alejandro G <ale1211@gmail.com>
2020-07-14 16:06:23 -0400

    Country exception store

--- app/GlobalCharPortException.php
+++ app/GlobalCharPortException.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class GlobalCharPortException extends Model
 {
-    //
+    protected $table    = "global_char_port_exceptions";
+    protected $fillable =   ['id','port_orig','port_dest','globalcharge_id'];
+    public $timestamps = false;
+    public function globalcharge()
+    {
+        return $this->belongsTo('App\GlobalCharge','globalcharge_id');
+    }
+    public function portOrig(){
+        return $this->belongsTo('App\Harbor','port_orig');
+    }
+    public function portDest(){
+        return $this->belongsTo('App\Harbor','port_dest');
+
+    }
 }
