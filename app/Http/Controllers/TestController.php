de1c30d196a43a196b56b05eef7bd8633fba2360
Alejandro G <ale1211@gmail.com>
2020-07-29 15:41:35 -0400

    Se duplica la funcion activaroCountry para el correcto funcionamiento de los adm
    Former-commit-id: d22677a4291d98f0ce81f43ce15d1dafe9d6e447 [formerly dd048b522a0686c55f8443e2331f9fc2f027bc86]
    Former-commit-id: e765ee7ccaa4bba823a0d7b7e7779ecaff6d7e59

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -1,26 +1,18 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\User;
-use App\Harbor;
-use App\Country;
-use Goutte\Client;
-use GuzzleHttp\Psr7;
-use GuzzleHttp\Cookie;
-use GuzzleHttp\Cookie\CookieJar;
-use GuzzleHttp\Cookie\FileCookieJar;
-use App\Jobs\TestJob;
+
 use App\AutoImportation;
-use App\RequetsCarrierFcl;
-use PrvUserConfigurations;
-use App\NewContractRequest;
-use Illuminate\Http\Request;
 use App\Jobs\SendEmailAutoImporJob;
 use App\Jobs\SendEmailRequestFclJob;
-use App\Jobs\SelectionAutoImportJob;
-use Symfony\Component\DomCrawler\Crawler;
+use App\Jobs\TestJob;
+use App\NewContractRequest;
+use App\User;
+use Goutte\Client;
+use GuzzleHttp\Cookie\FileCookieJar;
 use GuzzleHttp\Exception\RequestException;
-
+use Illuminate\Http\Request;
+use Intercom\IntercomClient;
 
 class TestController extends Controller
 {
@@ -32,18 +24,18 @@ class TestController extends Controller
     public function index(Request $request)
     {
 
-         $client = new \GuzzleHttp\Client();
-        $url    =  'https://www.maersk.com/webuser-rest-war/loginwithusernamepassword';
-        $array  = [
-             'body' => 'userName=juanfrag&password=Gencomex18%24&skipCustCodeSelect=N&timestamp=1567673525531'
+        $client = new \GuzzleHttp\Client();
+        $url = 'https://www.maersk.com/webuser-rest-war/loginwithusernamepassword';
+        $array = [
+            'body' => 'userName=juanfrag&password=Gencomex18%24&skipCustCodeSelect=N&timestamp=1567673525531',
         ];
 
-        $response = $client->request('POST',$url, [
+        $response = $client->request('POST', $url, [
             'body' => 'userName=juanfrag&password=Gencomex18%24&skipCustCodeSelect=N&timestamp=1567673525531',
             'headers' => [
-                'Content-Type'     => 'application/x-www-form-urlencoded',
-            ]
-        ]);/*
+                'Content-Type' => 'application/x-www-form-urlencoded',
+            ],
+        ]); /*
         $client = new \GuzzleHttp\Client(["base_uri" => "https://www.maersk.com"]);
         $response = $client->post("/webuser-rest-war/loginwithusernamepassword");*/
         $body = $response->getBody();
@@ -57,26 +49,27 @@ class TestController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function sendJob($user_id,$request){
-         SendEmailRequestFclJob::dispatch($user_id,$id);
+    public function sendJob($user_id, $request)
+    {
+        SendEmailRequestFclJob::dispatch($user_id, $id);
     }
     public function create(Request $request)
     {
         $client = new Client(['cookies' => true]);
         $path = '/var/www/html/cargofive/storage/app/public/cookies.json';
-        $cookFi = new FileCookieJar($path,true);
+        $cookFi = new FileCookieJar($path, true);
         //$cookFi->save($path);
-        $crawler = $client->request('GET', 'https://www.cma-cgm.com/ebusiness/my-prices/GetQuoteLines/0005926016/ST/2019-09-20/CNSHA/ARBUE',['cookies' => $cookFi]);
+        $crawler = $client->request('GET', 'https://www.cma-cgm.com/ebusiness/my-prices/GetQuoteLines/0005926016/ST/2019-09-20/CNSHA/ARBUE', ['cookies' => $cookFi]);
 
         //dd($wa.'\n'.$wresult.'\n'.$wctx);
         $crawler = $client->getResponse()->getContent();
         dd($crawler);
 
         /*SelectionAutoImportJob::dispatch($request->text1,'fcl');
-        $request->session()->flash('message.nivel', 'success');
-        $request->session()->flash('message.content', 'OK');
+    $request->session()->flash('message.nivel', 'success');
+    $request->session()->flash('message.content', 'OK');
 
-        return back();*/
+    return back();*/
     }
 
     /**
@@ -101,54 +94,54 @@ class TestController extends Controller
         $req_id = $id;
         $request_cont = NewContractRequest::find($req_id);
         $request_cont->load('Requestcarriers');
-        $user_adm_rq = User::where('email','admin@example.com')->orWhere('email','info@cargofive.com')->first();
-        $admins = User::where('type','admin')->get();
-        if(count($request_cont->Requestcarriers) == 1){
-            $autoImp = AutoImportation::whereHas('carriersAutoImportation',function($query) use($request_cont) {
-                $query->whereIn('carrier_id',$request_cont->Requestcarriers->pluck('carrier_id'));
-            })->where('status',1)->first();
-            if(!empty($autoImp)){
-                try{
-                    if(env('APP_ENV') == 'local'){
-                        $client = new Client(['base_uri' => 'http://contractsai/']);                            
-                    }else if(env('APP_ENV') == 'developer'){
+        $user_adm_rq = User::where('email', 'admin@example.com')->orWhere('email', 'info@cargofive.com')->first();
+        $admins = User::where('type', 'admin')->get();
+        if (count($request_cont->Requestcarriers) == 1) {
+            $autoImp = AutoImportation::whereHas('carriersAutoImportation', function ($query) use ($request_cont) {
+                $query->whereIn('carrier_id', $request_cont->Requestcarriers->pluck('carrier_id'));
+            })->where('status', 1)->first();
+            if (!empty($autoImp)) {
+                try {
+                    if (env('APP_ENV') == 'local') {
+                        $client = new Client(['base_uri' => 'http://contractsai/']);
+                    } else if (env('APP_ENV') == 'developer') {
                         $client = new Client(['base_uri' => 'http://dev.contractsai.cargofive.com/']);
-                    }else{
+                    } else {
                         $client = new Client(['base_uri' => 'http://prod.contractsai.cargofive.com/']);
                     }
                     //$response = $client->get('login?email=admin@example.com&password=secret');
                     //$response = $client->request('GET','ConverterFile/CFIndex', [
-                    $response = $client->request('GET','ConverterFile/CFDispatchJob/'.$req_id, [
+                    $response = $client->request('GET', 'ConverterFile/CFDispatchJob/' . $req_id, [
                         'headers' => [
                             //'Authorization' => $auth->api_key,
-                            'Authorization' => 'Bearer '.$user_adm_rq->api_token,
-                            'Accept'        => 'application/json',
-                        ]
+                            'Authorization' => 'Bearer ' . $user_adm_rq->api_token,
+                            'Accept' => 'application/json',
+                        ],
                     ]);
-                    $dataGen = json_decode($response->getBody()->getContents(),true);
+                    $dataGen = json_decode($response->getBody()->getContents(), true);
                     dd($dataGen);
                     //return $dataGen;
                 } catch (RequestException $e) {
                     //Enviar correo falla de conexion
-                    $message = 'connection failure, Request Id: '.$req_id.' I qualify for Auto-Import ENV: '.env('APP_ENV');
+                    $message = 'connection failure, Request Id: ' . $req_id . ' I qualify for Auto-Import ENV: ' . env('APP_ENV');
                     dd($message);
-                    foreach($admins as $userNotifique){
-                        SendEmailAutoImporJob::dispatch($userNotifique->email,$message);
+                    foreach ($admins as $userNotifique) {
+                        SendEmailAutoImporJob::dispatch($userNotifique->email, $message);
                     }
                 }
             }
-        } else{
-            $autoImp = AutoImportation::whereHas('carriersAutoImportation',function($query) use($request_cont) {
-                $query->whereIn('carrier_id',$request_cont->Requestcarriers->pluck('carrier_id'));
-            })->where('status',1)->first();
-            if(!empty($autoImp)){
+        } else {
+            $autoImp = AutoImportation::whereHas('carriersAutoImportation', function ($query) use ($request_cont) {
+                $query->whereIn('carrier_id', $request_cont->Requestcarriers->pluck('carrier_id'));
+            })->where('status', 1)->first();
+            if (!empty($autoImp)) {
                 //Enviar correo
-                $message = 'There is more than one carrier and one of them are listed in the Auto Import. Request Id: '.$req_id;
+                $message = 'There is more than one carrier and one of them are listed in the Auto Import. Request Id: ' . $req_id;
                 dd($message);
-                foreach($admins as $userNotifique){
-                    SendEmailAutoImporJob::dispatch($userNotifique->email,$message);
+                foreach ($admins as $userNotifique) {
+                    SendEmailAutoImporJob::dispatch($userNotifique->email, $message);
                 }
-            } 
+            }
         }
     }
 
@@ -158,7 +151,7 @@ class TestController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id,Request $request)
+    public function edit($id, Request $request)
     {
         //dd($request);
 
@@ -189,5 +182,67 @@ class TestController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    public function createIntercom()
+    {
+
+        /*$user = User::find(1);
+        dd($user);*/
+        $client = new IntercomClient('dG9rOmVmN2IwNzI1XzgwMmFfNDdlZl84NzUxX2JlOGY5NTg4NGIxYjoxOjA=', null, ['Intercom-Version' => '1.4']);
+        \DB::table('users')->chunkById(100, function ($users) use($client) {
+                foreach ($users as $user) {
+                    
+                    $this->intercom($client, $user);
+                }
+            });
+
+        echo "Finalizado";
+
+    }
+
+    public function intercom($client, $user)
+    {
+
+        try{
+            $cliente = $client->users->getUsers(["email" => $user->email]);
+        }catch(Exception $e){
+                echo  $user->email;
+        }
+        
+        if ($cliente->total_count > 1) {
+            foreach ($cliente->users as $u) {
+                if ($u->type == "user") {
+                    if ($u->user_id != $user->id) {
+                        $client->users->archiveUser($u->id);
+                    }
+                }
+            }
+        }
+
+        if ($cliente->total_count == 0) {
+
+            if ($user->company_user_id != "") {
+                //setHashID();
+
+                $client->users->create([
+                    "email" => $user->email,
+                    "companies" => [
+                        [
+                            "name" => $user->companyUser->name,
+                            "company_id" => $user->company_user_id,
+                        ],
+                    ],
+                ]);
+            } else {
+
+                $client->users->create([
+                    "email" => $user->email,
+                    "user_id" => $user->id,
+                    "name" => $user->name,
+                ]);
+            }
+
+        }
     }
 }
