eb802128f2c788e11e18a013e98d0b0973626202
Alejandro G <ale1211@gmail.com>
2020-07-29 15:41:35 -0400

    Se duplica la funcion activaroCountry para el correcto funcionamiento de los adm
    Former-commit-id: dd048b522a0686c55f8443e2331f9fc2f027bc86

--- public/js/globalcharges.js
+++ public/js/globalcharges.js
@@ -180,6 +180,116 @@ function hola(hi) {
     alert(hi);
 }
 
+
+
+function activarCountryOld(act){
+    var divCountry = $( ".divcountry");
+    var divport = $( ".divport");
+    var divportcountry = $( ".divportcountry");
+    var divcountryport = $( ".divcountryport");
+
+
+
+    var idPortOrig = $( "#port_orig"); 
+    var idCountryOrig = $( "#country_orig");  
+
+
+    var idPortDest = $( "#port_dest"); 
+    var idCountryDest = $( "#country_dest"); 
+
+
+    var portcountry_orig =  $( "#portcountry_orig") ;
+    var portcountry_dest =   $( "#portcountry_dest") ;
+
+    var countryport_orig=   $( "#countryport_orig"); 
+    var countryport_dest=    $( "#countryport_dest"); 
+
+
+    if(act == 'divcountry'){
+        divport.attr('hidden','true');
+        divportcountry.attr('hidden','true');
+        divcountryport.attr('hidden','true');
+
+        divCountry.removeAttr('hidden');
+
+        idCountryOrig.attr('required','true');
+        idCountryDest.attr('required','true');
+
+        idPortOrig.removeAttr('required');
+        idPortDest.removeAttr('required');
+
+        portcountry_orig.removeAttr('required');
+        portcountry_dest.removeAttr('required');
+
+        countryport_orig.removeAttr('required');
+        countryport_dest.removeAttr('required');
+
+
+
+    }else if(act == 'divport'){
+        divCountry.attr('hidden','true');
+        divportcountry.attr('hidden','true');
+        divcountryport.attr('hidden','true');
+
+        divport.removeAttr('hidden');
+
+        idPortOrig.attr('required','true');
+        idPortDest.attr('required','true');
+
+        idCountryOrig.removeAttr('required');
+        idCountryDest.removeAttr('required');
+
+        portcountry_orig.removeAttr('required');
+        portcountry_dest.removeAttr('required');
+
+        countryport_orig.removeAttr('required');
+        countryport_dest.removeAttr('required');
+
+
+    }else if(act == 'divportcountry'){
+        divCountry.attr('hidden','true');
+        divport.attr('hidden','true');
+        divcountryport.attr('hidden','true');
+        // Activo
+        divportcountry.removeAttr('hidden');
+        // Required
+        portcountry_orig.attr('required','true');
+        portcountry_dest.attr('required','true');
+        // No required 
+
+        idPortOrig.removeAttr('required');
+        idPortDest.removeAttr('required');
+
+        idCountryOrig.removeAttr('required');
+        idCountryDest.removeAttr('required');
+
+        countryport_orig.removeAttr('required');
+        countryport_dest.removeAttr('required');
+
+    }else if(act == 'divcountryport'){
+        divCountry.attr('hidden','true');
+        divport.attr('hidden','true');
+        divportcountry.attr('hidden','true');
+        // Activo
+        divcountryport.removeAttr('hidden');
+        // Required
+        countryport_orig.attr('required','true');
+        countryport_dest.attr('required','true');
+        // No required 
+
+        idPortOrig.removeAttr('required');
+        idPortDest.removeAttr('required');
+
+        idCountryOrig.removeAttr('required');
+        idCountryDest.removeAttr('required');
+
+        portcountry_orig.removeAttr('required');
+        portcountry_dest.removeAttr('required');
+
+
+    }
+}
+
 function activarCountry(act, notchange = true) {
     var divCountry = $(".divcountry");
     var divport = $(".divport");
