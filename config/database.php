2f97953efaa4f688e57c203f7771fdb0509e8639
Laravel Shift <shift@laravelshift.com>
2020-08-31 21:24:11 +0000

    Default config files
    In an effort to make upgrading the constantly changing config files
    easier, Shift defaulted them so you can review the commit diff for
    changes. Moving forward, you should use ENV variables or create a
    separate config file to allow the core config files to remain
    automatically upgradeable.
    
    
    Former-commit-id: cea6818c4e1005cb1ae6c890df600cd372e56644

--- config/database.php
+++ config/database.php
@@ -1,17 +1,8 @@
 <?php
-/*
-if (!defined('RDS_HOSTNAME')) {
-
-    define('RDS_HOSTNAME', $_SERVER['RDS_HOSTNAME']);
-    define('RDS_USERNAME', $_SERVER['RDS_USERNAME']);
-    define('RDS_PASSWORD', $_SERVER['RDS_PASSWORD']);
-    define('RDS_DB_NAME', $_SERVER['RDS_DB_NAME']);
-
-}*/
 
 return [
 
-   /*
+    /*
     |--------------------------------------------------------------------------
     | Default Database Connection Name
     |--------------------------------------------------------------------------
@@ -22,9 +13,9 @@ return [
     |
     */
 
-   'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
-   /*
+    /*
     |--------------------------------------------------------------------------
     | Database Connections
     |--------------------------------------------------------------------------
@@ -40,74 +31,60 @@ return [
     |
     */
 
-   'connections' => [
+    'connections' => [
 
-      'sqlite' => [
-         'driver' => 'sqlite',
-         'database' => env('DB_DATABASE', database_path('database.sqlite')),
-         'prefix' => '',
-      ],
+        'sqlite' => [
+            'driver' => 'sqlite',
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'prefix' => '',
+            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+        ],
 
-      /*
         'mysql' => [
             'driver' => 'mysql',
-            'host' => RDS_HOSTNAME,
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => RDS_DB_NAME,
-            'username' => RDS_USERNAME,
-            'password' => RDS_PASSWORD,
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
-            'strict' => false,
+            'prefix_indexes' => true,
+            'strict' => true,
             'engine' => null,
-
         ],
-        */
 
-      'mysql' => [
-         'driver' => 'mysql',
-         'host' => env('DB_HOST', '127.0.0.1'),
-         'port' => env('DB_PORT', '3306'),
-         'database' => env('DB_DATABASE', 'forge'),
-         'username' => env('DB_USERNAME', 'forge'),
-         'password' => env('DB_PASSWORD', ''),
-         'unix_socket' => env('DB_SOCKET', ''),
-         'charset' => 'utf8mb4',
-         'collation' => 'utf8mb4_unicode_ci',
-         'prefix' => '',
-         'strict' => true,
-         'engine' => 'InnoDB',
-      ],
+        'pgsql' => [
+            'driver' => 'pgsql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'schema' => 'public',
+            'sslmode' => 'prefer',
+        ],
 
-      'pgsql' => [
-         'driver' => 'pgsql',
-         'host' => env('DB_HOST', '127.0.0.1'),
-         'port' => env('DB_PORT', '5432'),
-         'database' => env('DB_DATABASE', 'forge'),
-         'username' => env('DB_USERNAME', 'forge'),
-         'password' => env('DB_PASSWORD', ''),
-         'charset' => 'utf8',
-         'prefix' => '',
-         'schema' => 'public',
-         'sslmode' => 'prefer',
-      ],
+        'sqlsrv' => [
+            'driver' => 'sqlsrv',
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', '1433'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+        ],
 
-      'sqlsrv' => [
-         'driver' => 'sqlsrv',
-         'host' => env('DB_HOST', 'localhost'),
-         'port' => env('DB_PORT', '1433'),
-         'database' => env('DB_DATABASE', 'forge'),
-         'username' => env('DB_USERNAME', 'forge'),
-         'password' => env('DB_PASSWORD', ''),
-         'charset' => 'utf8',
-         'prefix' => '',
-      ],
+    ],
 
-   ],
-
-   /*
+    /*
     |--------------------------------------------------------------------------
     | Migration Repository Table
     |--------------------------------------------------------------------------
@@ -118,30 +95,37 @@ return [
     |
     */
 
-   'migrations' => 'migrations',
+    'migrations' => 'migrations',
 
-   /*
+    /*
     |--------------------------------------------------------------------------
     | Redis Databases
     |--------------------------------------------------------------------------
     |
     | Redis is an open source, fast, and advanced key-value store that also
-    | provides a richer set of commands than a typical key-value systems
+    | provides a richer body of commands than a typical key-value system
     | such as APC or Memcached. Laravel makes it easy to dig right in.
     |
     */
 
-   'redis' => [
+    'redis' => [
 
-      'client' => 'predis',
+        'client' => 'predis',
 
-      'default' => [
-         'host' => env('REDIS_HOST', '127.0.0.1'),
-         'password' => env('REDIS_PASSWORD', null),
-         'port' => env('REDIS_PORT', 6379),
-         'database' => 0,
-      ],
+        'default' => [
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => env('REDIS_DB', 0),
+        ],
 
-   ],
+        'cache' => [
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => env('REDIS_CACHE_DB', 1),
+        ],
+
+    ],
 
 ];
