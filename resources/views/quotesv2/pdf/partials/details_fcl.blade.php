6f3a3db1543885684927e7871ec974745053c5f5
genesis <genesis@genesis.cantv.net>
2020-09-02 09:55:55 -0400

    pdf lcl
    Former-commit-id: 2a203c1dbeecf661174bbc0cc19dc766f3a6e3a1

--- resources/views/quotesv2/pdf/partials/details_fcl.blade.php
+++ resources/views/quotesv2/pdf/partials/details_fcl.blade.php
@@ -19,27 +19,34 @@
                         <p><b>{{__('pdf.to')}}:</b> {{@$quote->contact->first_name.' '.@$quote->contact->last_name}}</p>
                     
                     @endif
-                    <!-- End Client Name -->
 
                     <!-- Client Email -->
                     <p style="line-height:10px;">{{@$quote->contact->email}}</p>
-                    <!-- End Client Email -->
+
                     <!-- Company Name -->
                     @if(strlen(@$quote->company->business_name)>49)
+                    
                         <p style="line-height:12px; text-align:justify;"><span style="color: #4e4e4e"><b>{{@$quote->company->business_name}}</b></span></p>
+                    
                     @else
+                       
                         <p style="line-height:10px;"><span style="color: #4e4e4e"><b>{{@$quote->company->business_name}}</b></span></p>
+                    
                     @endif
-                    <!-- End Company Name -->
 
                     <!-- Company Address -->
                     @if(strlen(@$quote->company->address)>49)
-                        <p style="line-height:12px; text-align:justify;">{{@$quote->company->address}}</p>
-                    @else
-                        <p style="line-height:10px;">{{@$quote->company->address}}</p>
-                    @endif
-                    <!-- End Company Address -->
 
+                        
+                        <p style="line-height:12px; text-align:justify;">{{@$quote->company->address}}</p>
+                   
+                    @else
+                       
+                        <p style="line-height:10px;">{{@$quote->company->address}}</p>
+                    
+                    @endif
+                    
+                    <!-- Company Phone -->
                     <p style="line-height:10px;">{{@$quote->contact->phone}}</p>
                    
 
@@ -50,10 +57,15 @@
                 <div class="client" style="line-height: 10px; width:300px; float:right">
 
                     <p><b>{{__('pdf.from')}}: </b>{{@$quote->user->name}} {{@$quote->user->lastname}}</p>
+                   
                     <p style="line-height:10px;">{{@$quote->user->email}}</p>
+                   
                     <p style="line-height:12px;"><span style="color: #4e4e4e"><b>{{$user->companyUser->name}}</b></span></p>
+                    
                     <p style="line-height:10px;">{{@$user->companyUser->address}}</p>
+                    
                     <p style="line-height:10px;">{{@$user->phone}}</p>
+                    
                     <p class="color-title"><b>{{__('pdf.validity')}}:</b> {{\Carbon\Carbon::parse( $quote->validity_start)->format('d/M/Y') }} -  {{\Carbon\Carbon::parse( $quote->validity_end)->format('d/M/Y') }}</p>
                 
                 </div>
@@ -67,7 +79,9 @@
                 <div style="margin-top: 25px;">
 
                     @if($quote->incoterm_id!='')
+                        
                         <p><span><b>Incoterm:</b> </span>{{@$quote->incoterm->name}}</p>
+                    
                     @endif
                 
                     <p>
@@ -107,13 +121,13 @@
 
                     @if($quote->origin_address!='')
                     
-                        <p><span class="title" >{{__('pdf.origin_address')}}: </span>{{@$quote->origin_address}}</p>
+                        <p><span><b>{{__('pdf.origin_address')}}: </b></span>{{@$quote->origin_address}}</p>
                     
                     @endif
                     
                     @if($quote->destination_address!='')
                     
-                        <p><span class="title" >{{__('pdf.destination_address')}}: </span>{{@$quote->destination_address}}</p>
+                        <p><span><b>{{__('pdf.destination_address')}}: </b></span>{{@$quote->destination_address}}</p>
                         
                     @endif
                 
