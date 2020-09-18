4d54d82a18309d4be97efc1f334227cc2ceeaec7
Julio <javila3090@gmail.com>
2020-09-17 22:35:12 -0300

    working on localcharges
    Former-commit-id: 47f1fa4d193f0aa065b92c757e81f56ef9a37573 [formerly f9f4a2cea704008c146c2b87a3a3c944b833805d]
    Former-commit-id: e4469596c3819ca0782778794c84d3100c6e8494

---
+++ database/migrations/2020_09_17_212423_create_local_charge_quotes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLocalChargeQuotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('local_charge_quotes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->json('price');
+            $table->json('profit');
+            $table->integer('surcharge_id')->unsigned()->nullable();
+            $table->foreign('surcharge_id')->references('id')->on('surcharges');
+            $table->integer('calculation_type_id')->unsigned()->nullable();
+            $table->foreign('calculation_type_id')->references('id')->on('calculationtype');
+            $table->integer('currency_id')->unsigned()->nullable();
+            $table->foreign('currency_id')->references('id')->on('currency');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('local_charge_quotes');
+    }
+}
