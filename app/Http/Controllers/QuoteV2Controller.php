15c00d1debec5afcb384fdd6cf9149426f0d3f29
Alejandro Gonzalez <ale1211@gmail.com>
2020-09-22 14:44:16 -0400

    Condiciones faltantes
    Former-commit-id: 819937a77c74d6f1bc38d2a0a750c5b350ef1c5c [formerly 59d39326a5f5f8c910747a707630b36cffc531a4]
    Former-commit-id: 0eca2593a27a504259e02ecb2468f0bc9f8718b4

--- app/Http/Controllers/QuoteV2Controller.php
+++ app/Http/Controllers/QuoteV2Controller.php
@@ -73,6 +73,7 @@ use Spatie\MediaLibrary\MediaStream;
 use Spatie\MediaLibrary\Models\Media;
 use Yajra\DataTables\DataTables;
 
+
 class QuoteV2Controller extends Controller
 {
 
@@ -2901,7 +2902,7 @@ class QuoteV2Controller extends Controller
                     if ($company_setting->future_dates == 1) {
                         $q->where(function ($query) use ($dateSince) {
                             $query->where('validity', '>=', $dateSince)->orwhere('expire', '>=', $dateSince);
-                        })->where('company_user_id', '=', $company_user_id)->where('gp_container_id', '=', $validateEquipment['gpId']);
+                        })->where('company_user_id', '=', $company_user_id)->where('status', '!=','incomplete')->where('gp_container_id', '=', $validateEquipment['gpId']);
                     } else {
                         $q->where(function ($query) use ($dateSince, $dateUntil) {
                             $query->where('validity', '<=', $dateSince)->where('expire', '>=', $dateUntil);
@@ -2919,7 +2920,7 @@ class QuoteV2Controller extends Controller
                     if ($company_setting->future_dates == 1) {
                         $q->where(function ($query) use ($dateSince) {
                             $query->where('validity', '>=', $dateSince)->orwhere('expire', '>=', $dateSince);
-                        })->where('company_user_id', '=', $company_user_id)->where('gp_container_id', '=', $validateEquipment['gpId']);
+                        })->where('company_user_id', '=', $company_user_id)->where('status', '!=','incomplete')->where('gp_container_id', '=', $validateEquipment['gpId']);
                     } else {
                         $q->where(function ($query) use ($dateSince, $dateUntil) {
                             $query->where('validity', '<=', $dateSince)->where('expire', '>=', $dateUntil);
