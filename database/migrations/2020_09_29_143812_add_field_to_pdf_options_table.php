742b262b5bdaed31d5138cf8f0f65fe51cb50970
Julio <javila3090@gmail.com>
2020-09-29 14:28:32 -0300

    Added option to not grouping freights by routes in PDF
    Former-commit-id: 790023c498723775dae44fc9dafdb21df733f738

---
+++ database/migrations/2020_09_29_143812_add_field_to_pdf_options_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFieldToPdfOptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('pdf_options', function (Blueprint $table) {
+            $table->boolean('freight_no_grouping')->default(0)->after('grouped_freight_charges');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('pdf_options', function (Blueprint $table) {
+            //
+        });
+    }
+}
