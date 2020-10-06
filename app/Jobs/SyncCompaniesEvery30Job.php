6299118a65cd0a739cca811082b870285ed13b35
Julio <javila3090@gmail.com>
2020-10-06 16:29:00 -0300

    Fixed contracts
    Former-commit-id: fdfba219b6ef8fa84b42a8aae24bff96ff21e6a3

---
+++ app/Jobs/SyncCompaniesEvery30Job.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Jobs;
+
+use App\ApiIntegration;
+use App\ApiIntegrationSetting;
+use App\Company;
+use App\Connection;
+use App\Visualtrans;
+use GuzzleHttp\Client;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class SyncCompaniesEvery30Job implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        try {
+
+            $integrations = ApiIntegration::where(['module' => 'Companies', 'frecuency' => 30])->with('partner')->get();
+
+            foreach ($integrations as $setting) {
+                if ($setting->partner->name == 'Visualtrans') {
+                    $this->setDataVs($setting);
+                }
+            }
+        } catch (\Exception $e) {
+            $e->getMessage();
+        }
+    }
+
+    public function setDataVs($setting)
+    {
+        $data = new Connection();
+
+        $page = 1;
+
+        do {
+
+            $uri =  $setting->url . '&k=' . $setting->api_key . '&p=' . $page;
+
+            $response = $data->getData($uri);
+
+            $max_page = ceil($response['count'] / 100);
+
+            foreach ($response['entidades'] as $item) {
+
+                if ($item['fecha-alta'] > '2019-12-31') {
+
+                    Company::updateOrCreate([
+                        'api_id' => $item['codigo']
+                    ], [
+                        'business_name' => $item['nombre-fiscal'],
+                        'tax_number' => $item['cif-nif'],
+                        'company_user_id' => $setting->company_user_id,
+                        'api_id' => $item['codigo'],
+                    ]);
+
+                }
+            }
+
+            $page += 1;
+        } while ($page <= $max_page);
+
+        \Log::info('Syncronization with Visualtrans completed successfully!');
+    }
+}
