2a636c100d339adc382a0ffff1a40df4dfb60287
Julio <javila3090@gmail.com>
2020-10-06 21:53:37 -0300

    Working on sync companies job
    Former-commit-id: f8a0e93cd99d95075d412dc9433ba48e586a328d

--- app/Jobs/SyncCompaniesEvery30Job.php
+++ app/Jobs/SyncCompaniesEvery30Job.php
@@ -45,7 +45,7 @@ class SyncCompaniesEvery30Job implements ShouldQueue
                 }
             }
         } catch (\Exception $e) {
-            $e->getMessage();
+            \Log::error($e->getMessage());
         }
     }
 
@@ -65,7 +65,7 @@ class SyncCompaniesEvery30Job implements ShouldQueue
 
             foreach ($response['entidades'] as $item) {
 
-                if ($item['fecha-alta'] > '2019-12-31') {
+                if ($item['fecha-alta'] >= '2020-01-01') {
 
                     Company::updateOrCreate([
                         'api_id' => $item['codigo']
@@ -74,8 +74,8 @@ class SyncCompaniesEvery30Job implements ShouldQueue
                         'tax_number' => $item['cif-nif'],
                         'company_user_id' => $setting->company_user_id,
                         'api_id' => $item['codigo'],
+                        'api_status' => 'created',
                     ]);
-
                 }
             }
 
