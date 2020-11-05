e50305187bb506699c7cb913d115a687a4dd640f
Julio <javila3090@gmail.com>
2020-11-05 18:50:25 -0300

    Working on Local charges LCL
    Former-commit-id: 394b5869f347d8ff9f90576552463191ba615cd3

--- app/Http/Controllers/LocalChargeQuotationLclController.php
+++ app/Http/Controllers/LocalChargeQuotationLclController.php
@@ -11,6 +11,7 @@ use App\Harbor;
 use App\Http\Requests\StoreLocalChargeQuote;
 use App\Http\Resources\SaleTermChargeResource;
 use App\LocalChargeQuote;
+use App\LocalChargeQuoteLcl;
 use App\LocalChargeQuoteTotal;
 use App\SaleTermCharge;
 use App\SaleTermCode;
@@ -19,5 +20,19 @@ use App\Surcharge;
 
 class LocalChargeQuotationLclController extends Controller
 {
-    //
+    /**
+     * get previous stored local charges
+     *
+     * @param  mixed $request
+     * @return void
+     */
+    public function storedCharges(Request $request)
+    {
+        $local_charge_quotes = LocalChargeQuoteLcl::where([
+            'quote_id' => $request->quote_id, 'type_id' => $request->type_id,
+            'port_id' => $request->port_id
+        ])->with('surcharge', 'calculation_type', 'currency')->get();
+
+        return $local_charge_quotes;
+    }
 }
