711ba312a903fe4f5bdc190952b838445228d7eb
Julio <javila3090@gmail.com>
2020-11-16 20:36:34 -0300

    Fixed bug with LCL quotation localcharges
    Former-commit-id: ce1000afb0629b70e9b0a9bce4386f7967268582 [formerly 5bcfd89a2840597443da259496c450514c72d472]
    Former-commit-id: 310f06ce906e07b364863bbaaeabab447d1d98bb

--- app/LocalChargeQuoteLclTotal.php
+++ app/LocalChargeQuoteLclTotal.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class LocalChargeQuoteLclTotal extends Model
 {
@@ -16,5 +17,29 @@ class LocalChargeQuoteLclTotal extends Model
     public function quote()
     {
         return $this->belongsTo('App\QuoteV2', 'quote_id');
+    }
+
+    public function quotev2()
+    {
+        return $this->belongsTo('App\QuoteV2', 'quote_id');
+    }
+
+    public function totalize()
+    {
+        $charges = LocalChargeQuoteLcl::where(['quote_id' => $this->quote_id, 'type_id' => $this->type_id, 'port_id' =>  $this->port_id])->get();
+
+        $totals = 0;
+        
+        foreach ($charges as $charge) {
+            if ($charge->total != null) {
+                $exchange = ratesCurrencyFunction($charge->currency_id, $this->currency->alphacode);
+                $total_w_exchange = $charge->total / $exchange;
+                $totals += number_format((float)$total_w_exchange, 2, '.', '');
+            }
+        }
+
+        $this->update([
+            'total' => $totals,
+        ]);
     }
 }
