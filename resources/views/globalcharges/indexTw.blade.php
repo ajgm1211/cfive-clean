d34591363260633796adfdf641268ae3a37ac0f1
unknown <luisaviloreto@gmail.com>
2020-11-17 16:48:31 -0400

    fixed filter globalcharges
    Former-commit-id: 2f1cee1dfe2d7edc55ec7af25a030d748c7413f4 [formerly 21fc623786a9b5f3b0b1b6b0e59529cf5f44861b]
    Former-commit-id: 792bc8b739a0f89bfe5f0755ed76b4e832c66230

--- resources/views/globalcharges/indexTw.blade.php
+++ resources/views/globalcharges/indexTw.blade.php
@@ -270,7 +270,7 @@ New \ Status Import  &nbsp;
                 { data: 'action', name: 'action', orderable: false, searchable: false }
             ],
             initComplete: function () {
-                this.api().columns([1,2,3,4,5,6,7,8,9]).every(function () {
+                this.api().columns([1,2,3,4,5,6,7,8,9,10]).every(function () {
                     var column = this;
                     $('#requesttable .head .head_hide').html('');
 
@@ -290,17 +290,18 @@ New \ Status Import  &nbsp;
                     });
                 });
             },
-            "order": [[0, 'des']],
             "lengthChange": false,
             "searching": true,
             "ordering": false,
-            "width": true,
-            "autoWidth": false,
-            "stateSave": true,
+            "info": true,
+            "autoWidth": true, 
+            "deferLoading": 57,   
             "processing": true,
-            "serverSide": true,
-            "paging": true
-        });
+            "dom": 'Bfrtip',
+            "paging": true,
+            "scrollX": true,
+            "stateSave": false, 
+        }); 
 
     });
 
