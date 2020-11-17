8d68e0e81234d34002e255d490ab40f10a7649ae
unknown <luisaviloreto@gmail.com>
2020-11-17 16:43:08 -0400

    test quotes, ocean freight and localC  90%
    Former-commit-id: bd9d90417fdeecfffb2efe064abddea3bcf0adec

---
+++ database/factories/QuotesFclFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\QuoteV2::class, function (Faker $faker) {
+    return [
+         
+  'company_user_id'=>1,
+  'quote_id'=>'CA-testluis',
+  'type'=>'FCL',
+  'validity_start'=>date('Y-m-d'),
+  'validity_end'=>date('Y-m-d'),
+  'equipment'=>"[\"1\",\"2\",\"3\"]",
+  'delivery_type'=>1,
+  'user_id'=>1,
+  'date_issued'=>'2020-10-20',
+  'status'=>'Draft',
+       
+    ];
+});
