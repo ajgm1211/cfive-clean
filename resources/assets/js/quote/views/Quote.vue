efceca4b5b29bbd7e844728781653c52ecc4e29f
Alejandro G <ale1211@gmail.com>
2020-11-20 11:02:57 -0400

    arreglo de totals
    Former-commit-id: e46d6a129d0f4369943b35150abe45a259278ebf [formerly 30e32d4455a572ed094a7bc54597e050979c39e3]
    Former-commit-id: 99483963be87c364e777b3b3873cbf937d4bd996

--- resources/assets/js/quote/views/Quote.vue
+++ resources/assets/js/quote/views/Quote.vue
@@ -104,10 +104,6 @@
                             ></Inland>
                         </b-tab>
 
-                        <b-tab title="mañana">
-                            <p>sdsadjsaid</p>
-                        </b-tab>
-
                         <b-tab title="Totals" @click="changeView('totals')">
                             <Total v-if="totals"
                             :currentQuoteData="currentData"
