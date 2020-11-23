693ccd1cfa2081f9e1983a6081c4a0aaabc8dbf7
genesis <genesis@genesis.cantv.net>
2020-11-23 14:31:51 -0400

    detalles arreglado y sticky menu agregado
    Former-commit-id: 68596e81891b3ade1a8c29312906d3966ac185c4

--- resources/assets/js/search/views/Search.vue
+++ resources/assets/js/search/views/Search.vue
@@ -5,32 +5,42 @@
 
             <div class="row mr-0 ml-0">
 
-                <div class="col-12 col-sm-1">
-                    <multiselect
-                        v-model="type"
-                        :multiple="false"
-                        :close-on-select="true"
-                        :clear-on-select="true"
-                        :show-labels="false"
-                        :options="optionsType"
-                        class="s-input no-select-style"
-                    >
-                    </multiselect>
+                <div class="col-12 col-sm-3 d-flex">
+
+                        <div style="width: 18% !important; position:relative">
+                            <multiselect
+                                v-model="type"
+                                :multiple="false"
+                                :close-on-select="true"
+                                :clear-on-select="true"
+                                :show-labels="false"
+                                :options="optionsType"
+                                placeholder=""
+                                class="s-input no-select-style"
+                            >
+                            </multiselect>
+                            <b-icon icon="caret-down-fill" aria-hidden="true" class="type-mode"></b-icon>
+                        </div>
+
+                        <div style="width: 36% !important; position:relative ">
+                            <multiselect
+                                v-model="deliveryType"
+                                :multiple="false"
+                                :close-on-select="true"
+                                :clear-on-select="true"
+                                :show-labels="false"
+                                :options="optionsDeliveryType"
+                                placeholder=""
+                                class="s-input no-select-style "
+                            >
+                            </multiselect>
+                            <b-icon icon="caret-down-fill" aria-hidden="true" class="delivery-type"></b-icon>
+                        </div>
+
+
                 </div>
-                <div class="col-12 col-sm-2">
-                    <multiselect
-                        v-model="deliveryType"
-                        :multiple="false"
-                        :close-on-select="true"
-                        :clear-on-select="true"
-                        :show-labels="false"
-                        :options="optionsDeliveryType"
-                        class="s-input no-select-style delivery-type"
-                    >
-                    </multiselect>
-                </div> 
                 <div class="col-12 col-sm-9">
-                    <b-button v-b-toggle.collapse-1 class="btn-aditonal-services">aditional services <b-icon icon="caret-down-fill" class="ml-1"></b-icon></b-button>
+                    <b-button v-b-toggle.collapse-1 class="btn-aditonal-services">additional services <b-icon icon="caret-down-fill" class="ml-1"></b-icon></b-button>
                 </div>
 
             </div>
@@ -46,7 +56,7 @@
                         button-variant="outline-primary"
                         size="lg"
                         name="direction"
-                        class="radio-direction"
+                        class="radio-direction type-style"
                     ></b-form-radio-group>
 
                 </div>
@@ -120,7 +130,7 @@
 
             <b-collapse id="collapse-1" class="mt-2">
 
-                <h6 class="t-as mt-3 mb-3">ADDITIONAL SERVICES</h6>
+                <h6 class="t-as mt-3 mb-3 ml-4">ADDITIONAL SERVICES</h6>
             
                 <div class="row mr-0 ml-0">
 
@@ -194,14 +204,14 @@
 
                 </div>
 
-                <div class="row mr-0 ml-0 mt-5 d-flex justify-content-start">
+                <div class="row mr-0 ml-4 mt-5 d-flex justify-content-start">
                     <b-form-checkbox
                         id="originCharges"
                         v-model="originCharges"
                         name="originCharges"
                         value="accepted"
                         unchecked-value="not_accepted"
-                        class="mr-5"
+                        class="mr-5 as-checkbox"
                     >
                         Include origin charges
                     </b-form-checkbox>
@@ -211,6 +221,7 @@
                         name="destinationCharges"
                         value="accepted"
                         unchecked-value="not_accepted"
+                        class="as-checkbox"
                     >
                         Include destination charges
                     </b-form-checkbox>
@@ -219,7 +230,7 @@
             </b-collapse>
 
             <div class="row justify-content-center mr-0 ml-0">
-                <div class="col-2"><button class="btn-search">SEARCH</button></div>
+                <div class="col-2 d-flex justify-content-center"><button class="btn-search">SEARCH</button></div>
             </div>
 
         </b-form>
@@ -274,7 +285,7 @@ export default {
             optionPriceLevel: ['Select option', 'options', 'selected', 'mulitple', 'label', 'searchable', 'clearOnSelect', 'hideSelected', 'maxHeight', 'allowEmpty', 'showLabels', 'onChange', 'touched'],
             optionCarriers: ['Select option', 'options', 'selected', 'mulitple', 'label', 'searchable', 'clearOnSelect', 'hideSelected', 'maxHeight', 'allowEmpty', 'showLabels', 'onChange', 'touched'],
             optionContainer: ['Select option', 'options', 'selected', 'mulitple', 'label', 'searchable', 'clearOnSelect', 'hideSelected', 'maxHeight', 'allowEmpty', 'showLabels', 'onChange', 'touched'],
-            optionsType: ['FCL', 'LCL', 'Air'],
+            optionsType: ['FCL', 'LCL', 'AIR'],
             optionsDeliveryType: ['PORT TO PORT', 'PORT TO DOOR', 'DOOR TO PORT', 'DOOR TO DOOR']
         }
     }
