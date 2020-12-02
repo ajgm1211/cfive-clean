1301bfd6bedc10eaa445186f11065585dabadd95
Julio <javila3090@gmail.com>
2020-12-02 11:45:20 -0300

    Fixed ApiController.php
    Former-commit-id: 7863ca0a003d4501c396e4f3b648f51411827cf1 [formerly fe1f89466e0d213c42a1af9245411009e0b1868b]
    Former-commit-id: 5d78abfd4e8532ca81640c5084b0d8ce8c81d675

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -560,7 +560,7 @@ class ApiController extends Controller
             $carriers = Carrier::all()->pluck('id')->toArray();
 
         }else{
-            $carriers = Carrier::where('name' , $carrierUrl)->pluck('id')->toArray();
+            $carriers = Carrier::where('name' , $carrierUrl)->orWhere('uncode' , $carrierUrl)->pluck('id')->toArray();
         }
     
 
