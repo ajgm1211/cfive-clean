4e692671dd323b2207521e81af329bb439fb9440
Julio <javila3090@gmail.com>
2020-12-17 18:11:55 -0300

    Modified meny

--- resources/views/includes/header_menu.blade.php
+++ resources/views/includes/header_menu.blade.php
@@ -86,20 +86,7 @@
                                 </a>
                             </li>
                             <li class="m-menu__item " data-redirect="true" aria-haspopup="true">
-                                <a href="{{url('/api/search')}}" class="m-menu__link ">
-                                    <i class="m-menu__link-icon flaticon-search-magnifier-interface-symbol"></i>
-                                    <span class="m-menu__link-text">
-                                        Search 
-                                        <span class="m-menu__link-badge">
-                                            <span class="m-badge m-badge--brand m-badge--wide">
-                                                new
-                                            </span>
-                                        </span>
-                                    </span>
-                                </a>
-                            </li>
-                            <li class="m-menu__item " data-redirect="true" aria-haspopup="true">
-                                <a href="{{route('quotes-v2.index')}}" class="m-menu__link ">
+                                <a href="{{url('/api/quotes')}}" class="m-menu__link ">
                                     <i class="m-menu__link-icon flaticon-list-1"></i>
                                     <span class="m-menu__link-text">
                                         Quotes
@@ -107,13 +94,13 @@
                                 </a>
                             </li>
                             <li class="m-menu__item " data-redirect="true" aria-haspopup="true">
-                                <a href="{{url('/api/quotes')}}" class="m-menu__link ">
+                                <a href="{{route('quotes-v2.index')}}" class="m-menu__link ">
                                     <i class="m-menu__link-icon flaticon-coins"></i>
                                     <span class="m-menu__link-text">
-                                        List of Quotes
+                                        Quotes V1
                                         <span class="m-menu__link-badge">
                                             <span class="m-badge m-badge--brand m-badge--wide">
-                                                new
+                                                Deprecated
                                             </span>
                                         </span>
                                     </span>
