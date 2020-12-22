0cfd47f7ad232bbdf5e34a624374f5b3db79d465
Julio <javila3090@gmail.com>
2020-12-22 16:57:42 -0300

    Modified process to get lcl rates by contracts in API
    Former-commit-id: 859e70dad1db1528e6758536f5230d3f0462fbab

--- database/migrations/2020_12_21_170637_create_contract_rate_lcl_apis_table.php
+++ database/migrations/2020_12_21_170637_create_contract_rate_lcl_apis_table.php
@@ -15,6 +15,18 @@ class CreateContractRateLclApisTable extends Migration
     {
         Schema::create('contract_rate_lcl_apis', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('origin_port');
+            $table->string('destiny_port');
+            $table->string('via')->nullable();
+            $table->float('minimum');
+            $table->float('total');
+            $table->string('currency');
+            $table->string('transit_time')->nullable();
+            $table->string('remarks')->nullable();
+            $table->unsignedInteger('contract_id');
+            $table->foreign('contract_id')
+            ->references('id')->on('contracts_lcl')
+            ->onDelete('cascade');
             $table->timestamps();
         });
     }
