c0241aa1e14a1def89e77d80eed2b7408cd133a3
Julio <javila3090@gmail.com>
2021-01-05 20:45:03 -0300

    stand by

--- app/IntegrationQuoteStatus.php
+++ app/IntegrationQuoteStatus.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class IntegrationQuoteStatus extends Model
 {
-    //
+    public function quote()
+    {
+        return $this->belongsTo('App\QuoteV2', 'id', 'quote_id');
+    }
 }
