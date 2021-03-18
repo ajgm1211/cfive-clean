0aed3c8cd053111816a2ba9a9997fdc576aca1cd
Alejandro Gonzalez <ale1211@gmail.com>
2021-03-18 11:35:25 -0400

    duplicados
    Former-commit-id: 9511be2ed756c0251f1835d7da885d3b2cad507c

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -6,14 +6,16 @@ use App\AutoImportation;
 use App\Jobs\SendEmailAutoImporJob;
 use App\Jobs\SendEmailRequestFclJob;
 use App\Jobs\TestJob;
-use App\LocalCharge;
 use App\NewContractRequest;
 use App\User;
+use App\Harbor;
 use Goutte\Client;
+use App\Duplicados;
 use GuzzleHttp\Cookie\FileCookieJar;
 use GuzzleHttp\Exception\RequestException;
 use Illuminate\Http\Request;
 use Intercom\IntercomClient;
+use Maatwebsite\Excel\Facades\Excel;
 
 class TestController extends Controller
 {
@@ -278,27 +280,58 @@ class TestController extends Controller
     public function contable()
     {
 
-        $localcharge = LocalCharge::where('id', 1)->first();
+        try
+        {
+            $filename = '/var/www/html/cargofive/public/duplicados.csv';
 
-        //dd($localcharge);
-        if (empty($localcharge)) {
-            dd("hola");
+            Excel::load($filename, function ($reader) {
+                $reader->each(function ($sheet) {
+                    // recorre las filas
+                    $sheet->each(function ($row) {
+                        $col = explode(';',$row);
+                     
+                        $original = $col[0];
+                        
+                        
+                        
+                        $duplicado = explode(' ',$col[1]);
+                        
+                        if(count($duplicado ) > 1){
+                           foreach($duplicado as $dupli){
+
+                                $duplicados = new Duplicados;
+
+                                $duplicados->id_original = $original;
+                              
+                                $duplicados->id_duplicado = $dupli;
+                                $variation = Harbor::where('id',$dupli)->first();
+                                dd(json_decode($variation->varation));
+                                //$duplicados->save();
+                            
+
+                                      
+
+                           }
+
+                        }else{
+
+                            $duplicados = new Duplicados;
+
+                            $duplicados->id_original = $original;
+                            $duplicados->id_duplicado = $duplicado[0];
+                           // $duplicados->save();
+
+                        }
+                    });
+                });
+            });
+            
+   
+
+        } catch (Illuminate\Filesystem\FileNotFoundException $exception) {
+            die("No existe el archivo");
         }
-        var_dump(
-            count(array(null)), // NULL si no es contable
-            count(array(1)), // integers no son contables
-            count(array('abc')), // strings no son contables
-            //count(new stdclass), // objetos que no implementen la interfaz Countable no son contables
-            count([1, 2]) // arrays son contables
-        );
-        $variable = "SAN JOSE  R50    50     EUR     DIRECTO                             SEMANAL               17
-            DIRECTO                             SEMANAL               18_E_E";
-
-            $variable2 = Quitar_Espacios($variable);
-
-            dd($variable,$variable2);
 
     }
 
-  
 }
