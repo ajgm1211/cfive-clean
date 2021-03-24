1ec67201f61f9253beec2b8ae551fa9d1d94999f
amiraniotm <hectordt92@gmail.com>
2021-03-24 18:19:22 -0400

    More adjustments

--- resources/js/search/views/Result.vue
+++ resources/js/search/views/Result.vue
@@ -5,7 +5,7 @@
         <div class="row mb-3" style="margin-top: 80px">
             <div class="col-12 col-sm-6 d-flex align-items-center result-and-filter">
                 <h2 class="mr-5 t-recent">results found: <b>{{rates.length}}</b></h2>
-                <div class="d-flex filter-search">
+                <div v-if="false" class="d-flex filter-search">
                     <b>filter by:</b>
                     <div style="width: 150px !important; height: 33.5px; position:relative; top: -4px ">
                             <multiselect
