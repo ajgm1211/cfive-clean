f446642ae92381f74a81c973d2168d9d07839558
Alejandro Gonzalez <ale1211@gmail.com>
2021-04-05 13:01:50 -0400

    al seleccionar lcl redirecciona al search viejo con lcl
    Former-commit-id: ed8d87dd14d4c4aa2979755c3384446e1e32b3df

--- resources/views/quotesv2/search.blade.php
+++ resources/views/quotesv2/search.blade.php
@@ -2079,6 +2079,15 @@ $(document).ready(function() {
         $('.c5-switch').prop('checked', true);
         $('.c5-switch').trigger('change');
     }
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    if(urlParams.has('opt')){
+        $('#quoteType').val('2');
+        $('#quoteType').trigger('change');
+    }
+        
+
+
 });
 /*** GOOGLE MAPS API ***/
 var autocomplete;
