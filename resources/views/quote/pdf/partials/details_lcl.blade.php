07a7bf3df88dce605ad0dcf3d90a00e8c2df0a24
Julio Avila <javila3090@gmai.com>
2021-04-05 20:00:39 -0400

    Fixed PDF

--- resources/views/quote/pdf/partials/details_lcl.blade.php
+++ resources/views/quote/pdf/partials/details_lcl.blade.php
@@ -67,22 +67,11 @@
 
                 <p style="line-height:10px;">{{@$quote->user->email}}</p>
 
-                <p style="line-height:12px;"><span style="color: #4e4e4e"><b>{{$user->companyUser->name}}</b></span></p>
+                <p style="line-height:12px;"><span style="color: #4e4e4e"><b>{{delegation->name ?? $user->companyUser->name}}</b></span></p>
 
-                <p style="line-height:10px;">{{@$user->companyUser->address}}</p>
+                <p style="line-height:10px;">{{$delegation->address ?? $user->companyUser->address ?? "--"}}</p>
 
-                <p style="line-height:10px;">{{@$user->companyUser->phone}}</p>
-
-                @if($delegation != null)
-                    <br>
-                    <p><b>{{__('pdf.delegation')}}:</b></p>
-
-                    <p style="line-height:12px;"><span style="color: #4e4e4e"><b>{{@$delegation->name}}</b></span></p>
-
-                    <p style="line-height:10px;">{{@$delegation->address}}</p>
-
-                    <p style="line-height:10px;">{{@$delegation->phone}}</p>
-                @endif
+                <p style="line-height:10px;">{{$delegation->phone ?? $user->companyUser->phone ?? "--"}}</p>
 
             </div>
             <!-- End only Client -->
@@ -178,7 +167,7 @@
 
         @if(($quote->delivery_type==2 || $quote->delivery_type==3 || $quote->delivery_type==4) && ($quote->origin_address!='' || $quote->destination_address!=''))
 
-            <div  class="incoterm">
+            <div class="incoterm" style="margin-top:10px;">
 
                 @if($quote->origin_address!='')
                 
