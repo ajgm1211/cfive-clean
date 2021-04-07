84b61e19a5c9a3f1ff7a1d06bfba467554aeeb3e
Julio Avila <javila3090@gmai.com>
2021-04-07 13:19:59 -0400

    Modified PDF
    Former-commit-id: ca25d4606a557eab6d9edfd694ab491d2ac1d2da

--- resources/views/quote/pdf/partials/terms.blade.php
+++ resources/views/quote/pdf/partials/terms.blade.php
@@ -1,24 +1,24 @@
 @if($quote->terms_and_conditions!='' || $quote->terms_english!='' || $quote->terms_portuguese!='')
     <div class="clearfix">
 
-        <p class="text-left" style="font-size: 14px !important; color: {{ @$user->companyUser->colors_pdf }}"><b>{{__('pdf.terms')}}</b></p>
+        <p class="text-left" style="font-size: 12px !important;"><b>{{__('pdf.terms')}}</b></p>
         <div style="width:100%">
             @switch($quote->language_id)
             
                 @case(1)
-                    <span style="word-wrap: break-word; text-align: justify;">{!! trim(@$quote->terms_english) !!}</span>
+                    <span style="word-wrap: break-word; text-align: justify; font-size: 10px !important;">{!! trim(@$quote->terms_english) !!}</span>
                 @break
 
                 @case(2)
-                    <span style="word-wrap: break-word; text-align: justify;">{!! trim(@$quote->terms_and_conditions)  !!}</span>
+                    <span style="word-wrap: break-word; text-align: justify; font-size: 10px !important;">{!! trim(@$quote->terms_and_conditions)  !!}</span>
                 @break
 
                 @case(3)
-                    <span style="word-wrap: break-word; text-align: justify;">{!! trim(@$quote->terms_portuguese) !!}</span>
+                    <span style="word-wrap: break-word; text-align: justify; font-size: 10px !important;">{!! trim(@$quote->terms_portuguese) !!}</span>
                 @break
 
                 @default
-                    <span style="word-wrap: break-word; text-align: justify;">{!! trim(@$quote->terms_and_conditions)  !!}</span>
+                    <span style="word-wrap: break-word; text-align: justify; font-size: 10px !important;">{!! trim(@$quote->terms_and_conditions)  !!}</span>
 
             @endswitch
         </div>  
