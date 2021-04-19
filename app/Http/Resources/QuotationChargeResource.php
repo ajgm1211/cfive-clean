a8629cc7fb9ad871505c3c5c5797778a2d04765b
Julio Avila <javila3090@gmai.com>
2021-04-19 11:38:46 -0400

    Added total to QuotationChargeResource

--- app/Http/Resources/QuotationChargeResource.php
+++ app/Http/Resources/QuotationChargeResource.php
@@ -21,22 +21,74 @@ class QuotationChargeResource extends JsonResource
             'charge' => $this->surcharge->name ?? 'Ocean Freight',
             'calculation_type' => $this->calculation_type->name ?? null,
             'port' => $this->getType($this->type_id),
-            'price' => $this->price ?? [],
-            'profit' => $this->profit ?? [],
-            'total' => $this->total_price ?? [],
+            'price' => $this->arrayToFloat($this->price) ?? [],
+            'profit' => $this->arrayToFloat($this->profit) ?? [],
+            'total' => $this->setTotal($this->price, $this->profit),
             'units' => $this->units ?? null,
             'currency' => $this->currency->alphacode ?? null,
             'provider' => $this->automatic_rate->carrier->name ?? null,
         ];
     }
-
-    public function getType($type){
-        if($type == 1){
+    
+    /**
+     * getType
+     *
+     * @param  mixed $type
+     * @return void
+     */
+    public function getType($type)
+    {
+        if ($type == 1) {
             return $this->automatic_rate->origin_port->display_name;
-        }elseif($type == 2){
+        } elseif ($type == 2) {
             return $this->automatic_rate->destination_port->display_name;
-        }else{
+        } else {
             return null;
         }
+    }
+
+    /**
+     * arrayToFloat
+     *
+     * @param  mixed $array
+     * @return void
+     */
+    public function arrayToFloat($array)
+    {
+
+        $new_array = [];
+
+        foreach ((array)$array as $key => $item) {
+            $new_array[$key] = (float) $item;
+        }
+
+        return $new_array;
+    }
+    
+    /**
+     * setTotal
+     *
+     * @param  mixed $price
+     * @param  mixed $profit
+     * @return void
+     */
+    public function setTotal($price, $profit)
+    {
+
+        $new_array = [];
+        
+        foreach ($price as $key => $item) {
+            foreach ($profit as $k => $value) {
+                $str1 = ltrim($key, 'c');
+                $str2 = ltrim($k, 'm');
+                $total = 0;
+                if($str1 == $str2){
+                    $total = (float) $item + (float) $value;
+                    $new_array[$key] = (float) $total;
+                }
+            }
+        }
+
+        return $new_array;
     }
 }
