661f42e53c5f64e463b0e8f2cd0a1d857ded08f5
amiraniotm <hectordt92@gmail.com>
2021-04-29 16:57:55 -0400

    correcting load times

--- resources/js/search/views/APIResults.vue
+++ resources/js/search/views/APIResults.vue
@@ -970,12 +970,6 @@
         <!--  End Modal  -->
         </div>
         <!-- FIN TARJETA MAERKS -->
-
-        <div v-if="!apiSearchDone" class="spinner-load" style="width: 100%">
-            <div class="spinner-border text-primary" role="status">
-                <span class="sr-only">Loading...</span>
-            </div> 
-        </div>
         
     </div>
 </template>
