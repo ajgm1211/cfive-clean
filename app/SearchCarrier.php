0b2cadb8ab5d09c065e91e441c85b12b016fee51
amiraniotm <hectordt92@gmail.com>
2021-04-29 06:05:51 -0400

    Final details maersk + cma
    Former-commit-id: 10aa8f5c63afa64b39ee1c57e73d2fc72d6cc067

--- app/SearchCarrier.php
+++ app/SearchCarrier.php
@@ -6,10 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class SearchCarrier extends Model
 {
-    protected $fillable = ['id', 'search_rate_id', 'carrier_id'];
+    protected $fillable = ['id', 'search_rate_id', 'provider_id','provider_type'];
 
     public function search_rate()
     {
         return $this->belongsTo('App\SearchRate');
+    }
+
+    public function provider()
+    {
+        return $this->morphTo();
     }
 }
