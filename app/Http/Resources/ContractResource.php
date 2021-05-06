94aea3bcd205e5c252b663060daac87d8950fe2d
Julio Avila <javila3090@gmai.com>
2021-05-06 19:07:41 -0400

    Fixed and Optimized Owners in FCL Contracts

--- app/Http/Resources/ContractResource.php
+++ app/Http/Resources/ContractResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 use App\User;
+
 class ContractResource extends JsonResource
 {
     /**
@@ -14,22 +15,20 @@ class ContractResource extends JsonResource
      */
     public function toArray($request)
     {
-        $created_at=$this->created_at;
-        $fecha=$this->formatear($created_at);
-        $user_id=$this->user_id;
-        $fullname=$this->fullname($user_id);
+        $created_at = $this->created_at;
+        $fecha = $this->formatear($created_at);
         
         return [
             'id' => $this->id,
             'name' => $this->name,
             'number' => $this->number,
             'company_user' => $this->companyUser,
-            'user_name' => $fullname,
+            'user_name' => $this->user->fullname ?? $this->user_from_request[0]->fullname ?? "---",
             'direction' => $this->direction,
             'status' => $this->status,
             'validity' => $this->validity,
             'expire' => $this->expire,
-            'created_at'=> $fecha,
+            'created_at' => $fecha,
             'remarks' => $this->remarks ? $this->remarks : '',
             'carriers' => $this->carriers->pluck('carrier'),
             'restrictions' => [
@@ -40,21 +39,11 @@ class ContractResource extends JsonResource
         ];
     }
 
-    public function formatear($created_at){
+    public function formatear($created_at)
+    {
 
-        $fecha=date('Y-m-d H:m:s', strtotime($created_at));
+        $fecha = date('Y-m-d H:m:s', strtotime($created_at));
 
         return $fecha;
-    }
-    public function fullname($user_id){
-
-        if(isset($user_id)){
-            $user=User::find($user_id);
-            $fullname=$user->name." ".$user->lastname;
-        }else{
-            $fullname='----';
-        }
-
-        return $fullname;
     }
 }
