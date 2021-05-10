c832b300d5b1eafc33b5fd712ecb27fd96af429d
Alejandro G <ale1211@gmail.com>
2021-05-10 17:32:13 -0400

    New get contract by api
    Former-commit-id: 2da4541a34bca2ef98f9c242a3d87e27fb174c2c [formerly 944c24eec89a686582e6bfe37abeb7002444374c]
    Former-commit-id: f01103f4ead058f30d2859eaaabb863cb8ecbac2

--- routes/api.php
+++ routes/api.php
@@ -75,6 +75,8 @@ Route::group(['prefix' => 'v1'], function () {
         //Rates
         Route::get('rates/{mode}/{code_origin}/{code_destination}/{inicio}/{fin}/{group}/{carrierUrl?}', ['as' => 'search.index.v2', 'uses' => 'ApiController@search']);
         Route::get('ratesLCL/{code_origin}/{code_destination}/', ['as' => 'searchLCL.index.v2', 'uses' => 'ApiController@searchLCL']);
+        Route::get('getContract/', ['as' => 'getContract.index.v2', 'uses' => 'ApiController@getContract']);
+
         Route::get('get_rates/{contract}', ['as' => 'search.contract.id', 'uses' => 'ApiController@processSearchByContract']);
         //Calculation types
         Route::get('calculationtypes/{type}', 'ApiCalculationTypeController@index');
