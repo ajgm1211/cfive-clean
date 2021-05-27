19fb0c00b9febc356ad4e2e40c6505d6e7ac5503
Luis José Avila Loreto <luisaviloreto@gmail.com>
2021-05-27 17:49:37 -0400

    created migrations , resource and controller for inland per location

---
+++ app/Http/Controllers/inlandPerLocationController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class inlandsPerLocationController extends Controller
+{
+    public function index(Request $request)
+    {
+        return view('inlandperlocation.index');
+    }
+
+    public function list(Request $request)
+    {
+        //
+    }
+
+    public function data(Request $request)
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function update(Request $request)
+    {
+        //
+    }
+
+    public function duplicate(Request $request )
+    {
+        //
+    }
+
+    public function destroy(Request $request )
+    {
+        //
+    }
+
+    public function destroyAll(Request $request)
+    {
+        //
+    }
+}
