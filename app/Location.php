25e2ebd96fdb6b72bd86b5536ddbc8ba4ecc2966
Luis José Avila Loreto <luisaviloreto@gmail.com>
2021-05-28 12:08:08 -0400

    added models and migrations

---
+++ app/Location.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Location extends Model
+{
+    protected $table = 'Location';
+    protected $fillable = ['id', 'name', 'province_id','identifier'];
+
+    public function provinces()
+    {
+        return $this->belongsTo('App\InlandsProvinces', 'province_id');
+    }
+}
