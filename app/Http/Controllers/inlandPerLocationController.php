8a71c65ea7747328992a434ad6d452d20c13beeb
Julio Avila <javila3090@gmai.com>
2021-06-23 19:53:25 -0400

    Bug fixes
    Former-commit-id: 734af2399a7ac9464eddfdec84fdd1c3bf1d2782 [formerly 2c92aef19b390e9894eb17fb35df13ad1d4f452b]
    Former-commit-id: dc6fbe8ee636828a7688dc4ca4906e94a8d18ba2

--- app/Http/Controllers/inlandPerLocationController.php
+++ app/Http/Controllers/inlandPerLocationController.php
@@ -17,14 +17,15 @@ class inlandPerLocationController extends Controller
         return view('inlands.location');
     }
 
-    public function list(Request $request, Inland $inland) {
-        
+    public function list(Request $request, Inland $inland)
+    {
+
         $results = InlandPerLocation::filterByInland($inland->id)->filter($request);
         return InlandPerLocationResource::collection($results);
     }
 
     public function store(Request $request, Inland $inland)
-    {    
+    {
         $available_containers = Container::where('gp_container_id', $inland->gp_container_id ?? 1)->get()->pluck('code');
 
         $data = $request->validate([
@@ -39,20 +40,20 @@ class inlandPerLocationController extends Controller
             $value = isset($request['rates_' . $code]) ? number_format(floatval($request['rates_' . $code]), 2, '.', '') : 0;
             $containers['C' . $code] = $value;
         }
-            $inlandPL = new InlandPerLocation();
-            $inlandPL->json_container=json_encode($containers);
-            $inlandPL->currency_id =$data['currency'];
-            $inlandPL->harbor_id =$data['port'];
-            $inlandPL->inland_id =$inland->id;
-            $inlandPL->location_id =$data['location']; 
-            $inlandPL->service_id=$data['service'];
-            // $inlandPL->type = ;
-            $inlandPL->save();
-    
+        $inlandPL = new InlandPerLocation();
+        $inlandPL->json_container = $containers;
+        $inlandPL->currency_id = $data['currency'];
+        $inlandPL->harbor_id = $data['port'];
+        $inlandPL->inland_id = $inland->id;
+        $inlandPL->location_id = $data['location'];
+        $inlandPL->service_id = $data['service'];
+        // $inlandPL->type = ;
+        $inlandPL->save();
+
         return new InlandPerLocationResource($inlandPL);
     }
 
-    public function update(Request $request, Inland $inland,  InlandPerLocation $location )
+    public function update(Request $request, Inland $inland,  InlandPerLocation $location)
     {
         $available_containers = Container::where('gp_container_id', $inland->gp_container_id ?? 1)->get()->pluck('code');
         $data = $request->validate([
@@ -61,24 +62,24 @@ class inlandPerLocationController extends Controller
             'service' => 'required',
             'location' => 'required',
         ]);
-        
+
         $containers = [];
         foreach ($available_containers as $code) {
             $value = isset($request['rates_' . $code]) ? number_format(floatval($request['rates_' . $code]), 2, '.', '') : 0;
             $containers['C' . $code] = $value;
         }
-        
-        $location->json_container = json_encode($containers);
+
+        $location->json_container = $containers;
         $location->currency_id = $data['currency'];
         $location->harbor_id = $data['port'];
         $location->location_id = $data['location'];
         $location->service_id = $data['service'];
         $location->update();
-            
+
         return new InlandPerLocationResource($location);
     }
 
-    public function duplicate(InlandPerLocation $location )
+    public function duplicate(InlandPerLocation $location)
     {
         $new_inland_range = $location->duplicate();
         return new InlandPerLocationResource($new_inland_range);
@@ -95,6 +96,4 @@ class inlandPerLocationController extends Controller
         DB::table('inland_location')->whereIn('id', $request->input('ids'))->delete();
         return response()->json(null, 204);
     }
-    
-
 }
