32bb5f9244991508d35a105c9d9a6220874e4989
Julio Avila <javila3090@gmai.com>
2021-06-22 20:49:10 -0400

    fixed bug

--- app/Location.php
+++ app/Location.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Location extends Model
 {
-    protected $table = 'Location';
+    protected $table = 'location';
     protected $fillable = ['id', 'name', 'province_id','identifier'];
 
     public function provinces()
