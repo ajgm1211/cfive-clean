1f764607e045b4d86c99f7602c939dc71b917251
Luis <luedblra@gmail.com>
2021-07-01 15:43:02 -0400

    se agrega endpoint en validacion de template
    Former-commit-id: d3bf16bac5fc8eaedb96fac0d51ff140e12d6f2c

--- app/Jobs/ValidateTemplateLclJob.php
+++ app/Jobs/ValidateTemplateLclJob.php
@@ -36,14 +36,14 @@ class ValidateTemplateLclJob implements ShouldQueue
             'Access-Control-Allow-Origin'    => '*',
             'Connection'    => 'keep-alive'
         ];
-        /* $endpoint_obj = EndpointTable::where("name","barracuaep-template")->first();
+        $endpoint_obj = EndpointTable::where("name","barracuaep-template-lcl")->first();
         if($endpoint_obj->status == 1){
-            $json = '{"spreadsheetData":false}';
+            $json = '{"spreadsheetData":false,"type":"LCL"}';
             $url = $endpoint_obj->url."contracts/processing/".$this->request_id;
 
             $response = $client->request('POST',$url,['headers' => $headers,'body'=>$json]);
             $response = json_decode($response->getBody()->getContents(),true);
-        } */
+        } 
 
         $endpoint_obj_cmpfile = EndpointTable::where("name","barracuaep-cmpfile-lcl")->first();
         if($endpoint_obj_cmpfile->status == 1){
