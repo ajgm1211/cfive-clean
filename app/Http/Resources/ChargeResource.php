b2a3c98100adf123e238e3fc96f6d9085567f722
Alejandro G <ajgm1211@gmail.com>
2021-08-19 10:39:56 -0400

    Quote rate change to surchage rate

--- app/Http/Resources/ChargeResource.php
+++ app/Http/Resources/ChargeResource.php
@@ -18,13 +18,13 @@ class ChargeResource extends JsonResource
             'id' => $this->id,
             'automatic_rate_id' => $this->automatic_rate()->first(),
             'type_id' => $this->type_id,
-            'surcharge_id' => is_null($this->surcharge_id) ? $this->surcharge_id : $this->surcharge()->first(), 
+            'surcharge_id' => ($this->surcharge_id == "15021") ? $this->surcharge_id : $this->surcharge()->first(), 
             'calculation_type_id' => $this->calculation_type,
             'amount' => $this->amount, 
             'markups' => $this->markups,
             'currency_id' => $this->currency()->first(),
             'total' => $this->total,
-            'fixed_surcharge' => is_null($this->surcharge_id) ? $this->surcharge_id : $this->surcharge()->first()->name,
+            'fixed_surcharge' => ($this->surcharge_id == "15021") ? $this->surcharge()->first()->name : $this->surcharge()->first()->name,
             'fixed_currency' => $this->currency()->first(),
             'fixed_calculation_type' => is_null($this->calculation_type) ? $this->calculation_type : $this->calculation_type()->first()->name
         ];
@@ -37,7 +37,7 @@ class ChargeResource extends JsonResource
         if($this->amount!= null){
             $charges = json_decode($data['amount']);
             foreach($charges as $key=>$value){
-                if($this->surcharge_id == "13885"){
+                if($this->surcharge_id == "15021"){
                     $fr_key = 'freights_'.str_replace('c','',$key);
                     $data[$fr_key] = isDecimal($value,true);
                 } else {
