04a70ef68dfa24db4a26b16acc76ed2d721a8566
Luis José Avila Loreto <luisaviloreto@gmail.com>
2021-08-20 09:47:33 -0400

    pdf template

--- app/PdfTemplate.php
+++ app/PdfTemplate.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class PdfTemplate extends Model
 {
-    //
+    protected $table = 'pdf_templates';
+    protected $fillable = ['id', 'name', 'description'];
 }
