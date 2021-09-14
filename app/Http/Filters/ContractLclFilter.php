d968271b74b10ca06a784c018831fad5193e20ed
Julio Avila <javila3090@gmail.com>
2021-09-14 19:25:18 -0300

    Added contract's code to list
    Former-commit-id: db5eef31df452d0dc1aecb22f72e938a497cb5a7

--- app/Http/Filters/ContractLclFilter.php
+++ app/Http/Filters/ContractLclFilter.php
@@ -7,7 +7,7 @@ use App\Http\Filters\AbstractFilter;
 class ContractLclFilter extends AbstractFilter
 {
     protected $paginate = 10;
-    protected $filter_by = ['name', 'status', 'validity', 'expire'];
+    protected $filter_by = ['name', 'status', 'validity', 'expire', 'contract_code'];
     protected $filter_by_relations = ['carriers.carrier__name', 'direction__name'];
     protected $default_filter_by = [];
     protected $with = [
