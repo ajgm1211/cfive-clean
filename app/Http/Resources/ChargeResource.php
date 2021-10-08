d6c42de681050992a3359a1904f9ebb855fa968a
amiraniotm <hectordt92@gmail.com>
2021-10-08 11:13:15 +0100

    assigning surcharge on ocean freight charges
    Former-commit-id: ae18f4b928249d4a37be184a0537e011e8b92824 [formerly 521fc6765d83270821cb6e2d8593d7c5e038a805]
    Former-commit-id: 92b5fb7672c315b252c948f3982483a1385dcf57

--- app/Http/Resources/ChargeResource.php
+++ app/Http/Resources/ChargeResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Surcharge;
 
 class ChargeResource extends JsonResource
 {
@@ -18,13 +19,13 @@ class ChargeResource extends JsonResource
             'id' => $this->id,
             'automatic_rate_id' => $this->automatic_rate()->first(),
             'type_id' => $this->type_id,
-            'surcharge_id' => is_null($this->surcharge_id) ? $this->surcharge_id : $this->surcharge()->first(), 
+            'surcharge_id' => $this->surcharge()->first(), 
             'calculation_type_id' => $this->calculation_type,
             'amount' => $this->amount, 
             'markups' => $this->markups,
             'currency_id' => $this->currency()->first(),
             'total' => $this->total,
-            'fixed_surcharge' => is_null($this->surcharge_id) ? $this->surcharge_id : $this->surcharge()->first()->name,
+            'fixed_surcharge' => $this->surcharge()->first()->name,
             'fixed_currency' => $this->currency()->first(),
             'fixed_calculation_type' => is_null($this->calculation_type) ? $this->calculation_type : $this->calculation_type()->first()->name
         ];
@@ -34,10 +35,11 @@ class ChargeResource extends JsonResource
 
     public function addContainers($data)
     {   
+        $ocean_surcharge = Surcharge::where([['name','Ocean Freight'],['company_user_id',null]])->first();
         if($this->amount!= null){
             $charges = json_decode($data['amount']);
             foreach($charges as $key=>$value){
-                if($this->surcharge_id == null){
+                if($this->surcharge_id == $ocean_surcharge->id){
                     $fr_key = 'freights_'.str_replace('c','',$key);
                     $data[$fr_key] = isDecimal($value,true);
                 } else {
