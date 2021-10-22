703d2b733d13c455d07ff2b72e06b7347d5e84ec
Julio Avila <javila3090@gmail.com>
2021-10-22 15:01:06 +0100

    Fixed issues with LCL Contracts
    Former-commit-id: 95bc7c96ac76c9537cd878b7ff43c6989478cf89 [formerly bba1f1005edd389a02ddb09f2d71defbe92efd97]
    Former-commit-id: 20278f2c084a00e7ebf879c0833b91c90087de75

--- app/Http/Controllers/ContractLclController.php
+++ app/Http/Controllers/ContractLclController.php
@@ -240,7 +240,6 @@ class ContractLclController extends Controller
      */
     public function destroy(ContractLcl $contract)
     {
-        $contract->contract_code = null;
         $contract->delete();
 
         return response()->json(null, 204);
