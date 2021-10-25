5342c8a06d57edcd054c47fad773bbbf229bcd54
Christopher Chirino <critijo@gmail.com>
2021-10-25 09:23:20 -0400

    changes
    Former-commit-id: dbee4cd77fe637e7bc0796617d1c910872924235

--- database/migrations/2021_10_25_131011_create_settings_whitelabels_table.php
+++ database/migrations/2021_10_25_131011_create_settings_whitelabels_table.php
@@ -15,6 +15,8 @@ class CreateSettingsWhitelabelsTable extends Migration
     {
         Schema::create('settings_whitelabels', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('url');
+            $table->integer('company_user_id')->unsigned()->nullable();
             $table->timestamps();
         });
     }
