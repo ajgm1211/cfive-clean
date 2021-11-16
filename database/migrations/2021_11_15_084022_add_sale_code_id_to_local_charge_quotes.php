d34602cf7693f9ac1a32eb8f56552e4ff0b18a75
amiraniotm <hectordt92@gmail.com>
2021-11-16 09:54:07 +0000

    hotfixes database
    Former-commit-id: e833605829d2561c3b44d158eb00f6498070f901 [formerly 8d7b91669985fb73863ac62fc07b57d3f47afa12]
    Former-commit-id: 02f4cbab372ff16e88f537df55593bc39815ff12

---
+++ database/migrations/2021_11_15_084022_add_sale_code_id_to_local_charge_quotes.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSaleCodeIdToLocalChargeQuotes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('local_charge_quotes', function (Blueprint $table) {
+            $table->integer('sale_term_code_id')->nullable()->unsigned()->after('calculation_type_id');
+            $table->foreign('sale_term_code_id')->references('id')->on('sale_term_codes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('local_charge_quotes', function (Blueprint $table) {
+            //
+        });
+    }
+}
