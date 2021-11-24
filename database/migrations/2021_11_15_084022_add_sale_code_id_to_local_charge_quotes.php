b9a3d14455606b52cee07741145a4d9f63c92e91
Julio Avila <javila3090@gmail.com>
2021-11-24 17:23:15 -0300

    Removed migrations
    Former-commit-id: 755038fbabe82a90da99a646c87fb2436e317c66

--- database/migrations/2021_11_15_084022_add_sale_code_id_to_local_charge_quotes.php
+++ database/migrations/2021_11_15_084022_add_sale_code_id_to_local_charge_quotes.php
@@ -13,10 +13,10 @@ class AddSaleCodeIdToLocalChargeQuotes extends Migration
      */
     public function up()
     {
-        Schema::table('local_charge_quotes', function (Blueprint $table) {
+        /*Schema::table('local_charge_quotes', function (Blueprint $table) {
             $table->integer('sale_term_code_id')->nullable()->unsigned()->after('calculation_type_id');
             $table->foreign('sale_term_code_id')->references('id')->on('sale_term_codes');
-        });
+        });*/
     }
 
     /**
