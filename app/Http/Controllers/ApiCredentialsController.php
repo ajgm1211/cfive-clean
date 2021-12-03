c410a86288e3dfbc571c7b6e66d6c267c1e37a3f
amiraniotm <hectordt92@gmail.com>
2021-12-03 12:04:23 +0000

    adding validation for avoiding null

--- app/Http/Controllers/ApiCredentialsController.php
+++ app/Http/Controllers/ApiCredentialsController.php
@@ -13,12 +13,13 @@ class ApiCredentialsController extends Controller
         $clients = CompanyUser::paginate(10);
         return $clients;
     }
+
     public function listAvailableApiProviders(Request $request){
         $company_user_id = $request->input("company_user_id");
         $company_user = CompanyUser::find($company_user_id);
         $options = $company_user->options;
-        $used_api_providers = $options["api_providers"];        
-        
+        $used_api_providers = $options["api_providers"];       
+                
         if(sizeof($used_api_providers)) {
             $apiProviders = ApiProvider::whereNotIn('id', $used_api_providers)->get();
         } else {
@@ -27,10 +28,12 @@ class ApiCredentialsController extends Controller
         
         return $apiProviders;
     }
+
     public function searchCompanyUsers($search) { 
         $clients = CompanyUser::where('name', 'like', '%'.$search.'%')->paginate(10);
         return $clients;
     }
+
     public function listApiProvidersByCompanyUser(CompanyUser $companyUser) {
         
         $companyUserId = $companyUser->id; 
@@ -54,6 +57,11 @@ class ApiCredentialsController extends Controller
     }
 
     public function store(Request $request){
+        $request->validate([
+            'model_id' => 'required',
+            'api_provider_id' => 'required'
+        ]);
+
         $company_user_id = $request->input("model_id");
         $api_provider_id = $request->input("api_provider_id");
 
