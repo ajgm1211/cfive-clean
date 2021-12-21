ff07daba2e4885b79b7c2c2bb844e0cd8c8a930d
josepetit-cargofive <josepetit.une123@gmail.com>
2021-12-21 09:19:01 -0400

    implementacion de custom env
    Former-commit-id: 859f679d95c553b1bf9e57a87958c77ea56d7415 [formerly 5d0c80858c597934afadbcea92d1387c22b2780b]
    Former-commit-id: 679904f8242a566ef496b0ba630a4af2369e3f57

--- config/custom.example.php
+++ config/custom.example.php
@@ -2,27 +2,35 @@
 
 switch (env('APP_ENV')) {
     case 'local':
-        $view  = 'local';
+        $view    = 'local';
         $appUrl  = 'http://cargofive.local';
-        $apiUrl = 'https://carriersdev.cargofive.com/api/pricing';
+        $apiUrl  = 'https://carriersdev.cargofive.com/api/pricing';
+        $baseUriDuplicates = 'http://duplicate-gc/DuplicateGCFCL/';
+        //$appView = 'local';
     break;
 
-    case 'demo':
-        $view  = 'demo';
+    case 'developer':
+        $view    = 'developer';
         $appUrl  = '';
-        $apiUrl = 'https://carriersdev.cargofive.com/api/pricing';
+        $apiUrl  = 'https://carriersdev.cargofive.com/api/pricing';
+        $baseUriDuplicates = 'http://duplicateds-globalchargers-dev.eu-central-1.elasticbeanstalk.com/DuplicateGCFCL/';
+        //$appView = 'local';
     break;
 
     case 'production':
-        $view  = 'prd';
+        $view    = 'prd';
         $appUrl  = 'https://app.cargofive.com';
-        $apiUrl = 'https://carriers.cargofive.com/api/pricing';
+        $apiUrl  = 'https://carriers.cargofive.com/api/pricing';
+        $baseUriDuplicates = 'http://prod.duplicatedscg.cargofive.com/DuplicateGCFCL/';
+        //$appView = 'local';
     break;
     
     default:
         $view = '';
         $appUrl  = '';
         $apiUrl = '';
+        $baseUriDuplicates ='';
+        //$appView = '';
         break;
 }
 
@@ -60,6 +68,30 @@ return [
     |
     */
 
-    'api_url' => $appUrl,
+    'api_url' => $apiUrl,
+
+    /*
+    |--------------------------------------------------------------------------
+    | BASE URL
+    |--------------------------------------------------------------------------
+    |
+    | This setups the base url for duplicates gateway so we can get it.
+    |
+    */
+
+    'base_uri_duplicates' => $baseUriDuplicates,
+
+    /*
+    |--------------------------------------------------------------------------
+    | BASE URL
+    |--------------------------------------------------------------------------
+    |
+    | This setups the base url for duplicates gateway so we can get it.
+    |
+    */
+
+    //'app_view' => env('APP_VIEW', $appView),
+
+    
 
 ];
