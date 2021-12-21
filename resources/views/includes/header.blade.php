fc6b3890e18f986b07955a7d2d02e5127a175639
josepetit-cargofive <josepetit.une123@gmail.com>
2021-12-21 09:19:01 -0400

    implementacion de custom env
    Former-commit-id: 5d0c80858c597934afadbcea92d1387c22b2780b

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -2,7 +2,7 @@
 	<div class="m-container m-container--fluid m-container--full-height">
 		@if(Session::has('impersonate') || config('custom.app_env') == 'local' || config('custom.app_env') == 'prod' || config('custom.app_env') == 'dev')
 		@include('includes.header_menu')
-		@elseif(env('APP_VIEW') == 'operaciones')
+		@elseif(config('custom.app_view') == 'operaciones')
 		@include('includes.header_menu_operaciones')
 		@endif
 		<!-- END: Horizontal Menu -->
