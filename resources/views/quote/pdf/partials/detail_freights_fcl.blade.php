2a1c67a9adb2c3b12ea4bb2c4b58679467276858
amiraniotm <hectordt92@gmail.com>
2022-02-16 10:45:41 +0000

    PDF freight table adjust
    Former-commit-id: 7d128214b1f9b13d363d3e662a4e95a03bee8886

--- resources/views/quote/pdf/partials/detail_freights_fcl.blade.php
+++ resources/views/quote/pdf/partials/detail_freights_fcl.blade.php
@@ -93,15 +93,19 @@
                                                         @if($c->code == $key)
                                                             <?php
                                                                 if($v->surcharge->name == "Ocean Freight" && $v->surcharge->company_user_id==null){
-                                                                    $total = $v->surcharge_id != '' ? $v->${'sum_amount_markup_'.$c->code}:$v->${'sum_amount_markup_'.$c->code}+@$r->total_rate->markups['m'.$c->code];
-                                                                    foreach($r['markups'] as $m=>$markups ){
-                                                                        $containerM=str_replace("m", "", $m);
-                                                                        if ($containerM==$c->code) {
-                                                                            $total_w_profit=$total+$markups;
+                                                                    //$total = $v->surcharge_id != '' ? $v->${'sum_amount_markup_'.$c->code}:$v->${'sum_amount_markup_'.$c->code}+@$r->total_rate->markups['m'.$c->code];
+                                                                    //foreach($r->total_rate['markups'] as $m=>$markups ){
+                                                                        //$containerM=str_replace("m", "", $m);
+                                                                        //if ($containerM==$c->code) {
+                                                                        if(isset($amount["c". $c->code]) && isset($r->total_rate->markups["m" . $c->code])){
+                                                                            $total_w_profit = $amount["c". $c->code] + $r->total_rate->markups["m" . $c->code];
                                                                         }
-                                                                    }
+                                                                        //}
+                                                                    //}
                                                                 }else{
-                                                                    $total_w_profit = $v->surcharge_id != '' ? $v->${'sum_amount_markup_'.$c->code}:$v->${'sum_amount_markup_'.$c->code}+@$r->total_rate->markups['m'.$c->code];
+                                                                    if(isset($amount["c". $c->code])){
+                                                                        $total_w_profit = $amount["c". $c->code];
+                                                                    }
                                                                 }
                                                             ?>
                                                             <!--<td {{ $hide }}>{{isDecimal($v->${'total_sum_'.$c->code}, true)}}</td>-->
