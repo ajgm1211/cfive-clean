9508b1451e934ace539f8180a965402e9211846c
Alejandro Gonzalez <ale1211@gmail.com>
2022-02-18 13:16:47 -0400

    Change ebextension

--- .platform/hooks/postdeploy/00_build_dependencies.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-   sudo mkdir /tmp/aca
-   sudo chmod -R 777 /tmp/aca
--- .platform/hooks/postdeploy/50_prepare_enviroment.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-    cd /var/www/html
-    sudo cp /var/www/html/config/custom.example.php /var/www/html/config/custom.php
-    sudo chmod 755 /var/www/html/config/custom.php
