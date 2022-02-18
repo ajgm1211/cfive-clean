f61aec27dd8883de22ecfc270aa38539b0b0fd3f
Alejandro Gonzalez <ale1211@gmail.com>
2022-02-18 10:50:57 -0400

    rebuild sh to apply config
    Former-commit-id: db7f424882d1c14cca87a32372a2c06329da7bf6

--- .platform/hooks/postdeploy/50_prepare_enviroment.sh
+++ .platform/hooks/postdeploy/50_prepare_enviroment.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-    sudo cp /var/www/html/config/custom.example.php /var/www/html/config/custom.php
-    sudo chmod 755 /var/www/html/config/custom.php
-    curl -Sl https://rpm.nodesource.com/setup_14.x  | sudo bash -
-    sudo yum -y install nodejs
-    npm install 
-    npm run dev
+    cd /var/www/html
+    sudo cp config/custom.example.php config/custom.php
+    sudo chmod 755 config/custom.php
