bdf0fa999eddb435daa93bd08aac08129a2bd08d
Alejandro Gonzalez <ale1211@gmail.com>
2022-02-18 10:58:32 -0400

    change postdeploy
    Former-commit-id: 202704056144d9f270773b2b421e5cbc84ccd662

--- .platform/hooks/postdeploy/00_build_dependencies.sh
+++ .platform/hooks/postdeploy/00_build_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
     curl -Sl https://rpm.nodesource.com/setup_14.x  | sudo bash -
     sudo yum -y install nodejs
-    cd /var/www/html
-    npm install 
-    npm run dev
+    cd /var/app/current/ && npm install &&  npm run dev
+    
+ 
