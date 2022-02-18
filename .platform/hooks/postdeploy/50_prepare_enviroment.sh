a9eb90cb6e670dd3f8983fa9b4c4aa661846e3e3
Alejandro Gonzalez <ale1211@gmail.com>
2022-02-18 11:35:51 -0400

    Try to execute postdeploy
    Former-commit-id: 94f0c9feed5642d13368a14523d176efdadeb2e3

--- .platform/hooks/postdeploy/50_prepare_enviroment.sh
+++ .platform/hooks/postdeploy/50_prepare_enviroment.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-    cd /var/app/current
-    sudo cp /var/app/current/config/custom.example.php /var/app/current/config/custom.php
-    sudo chmod 755 /var/app/current/config/custom.php
+    cd /var/www/html
+    sudo cp /var/www/html/config/custom.example.php /var/www/html/config/custom.php
+    sudo chmod 755 /var/www/html/config/custom.php
