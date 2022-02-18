45fb5d84f4042db70561796db8d908ce6b483f73
Alejandro Gonzalez <ale1211@gmail.com>
2022-02-18 11:35:51 -0400

    Try to execute postdeploy

--- .platform/hooks/postdeploy/00_build_dependencies.sh
+++ .platform/hooks/postdeploy/00_build_dependencies.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
     curl -Sl https://rpm.nodesource.com/setup_14.x  | sudo bash -
     sudo yum -y install nodejs
-    cd /var/app/current/ && npm install &&  npm run dev
+    cd /var/www/html/ && npm install &&    npm run dev 
+    
+ 
