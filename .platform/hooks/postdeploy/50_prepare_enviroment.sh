3b488fdca9c926d6465d5f6584ce19f9e05b504f
Alejandro Gonzalez <ale1211@gmail.com>
2022-02-18 10:22:46 -0400

    Fix prepare enviroment

--- .platform/hooks/postdeploy/50_prepare_enviroment.sh
+++ .platform/hooks/postdeploy/50_prepare_enviroment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
     sudo cp /var/www/html/config/custom.example.php /var/www/html/config/custom.php
-    sudo chmod 755 /var/www/html/custom.php
+    sudo chmod 755 /var/www/html/config/custom.php
     curl -Sl https://rpm.nodesource.com/setup_14.x  | sudo bash -
     sudo yum -y install nodejs
     npm install 
