011ad289de13d30257e3450bfe05886d8969f633
Luis José Avila Loreto <luisaviloreto@gmail.com>
2022-03-08 11:04:58 -0400

    created migration
    Former-commit-id: 902206dfa55f1112f39cee9672f28fd824e60a42

---
+++ database/migrations/2022_03_08_135812_remane_column_in_inland_per_location_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RemaneColumnInInlandPerLocationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('inland_per_location', function (Blueprint $table) {
+            $table->renameColumn('json_container', 'json_containers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('inland_per_location', function (Blueprint $table) {
+            //
+        });
+    }
+}
