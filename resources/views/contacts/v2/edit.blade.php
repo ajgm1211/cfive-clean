ed6c8056c8af7353f53ff50bef9fbb5ad38a5907
josePLogos <jpetit@logoscorp.com>
2022-03-09 21:28:59 -0400

    contact views
    Former-commit-id: f7596ad600488d2118186f088d4bccdcc5e4a0a7

---
+++ resources/views/contacts/v2/edit.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('title', 'Company Edit')
+@section('content')
+    <div id="app"></div>
+@endsection
+
+@section('js')
+@parent
+@endsection
---
+++ resources/views/contacts/v2/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('title', 'Companies')
+@section('content')
+    <div id="app"></div>
+@endsection
+
+@section('js')
+@parent
+@endsection
