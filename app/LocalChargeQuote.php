93f73e115fcd3e807ee2665c86aa3c739e02d45c
DiegoHuacanjulca <diant2405@gmail.com>
2022-04-28 08:54:02 -0500

    add log sumarize method

--- app/LocalChargeQuote.php
+++ app/LocalChargeQuote.php
@@ -65,31 +65,33 @@ class LocalChargeQuote extends Model implements Auditable
 
         $totals = [];
 
-        foreach ($equip_array as $eq) {
-            $totals['c' . $eq] = 0;
-        }
-
-        if ($this->price != null) {
+        try {
             foreach ($equip_array as $eq) {
-                foreach ($this->price as $key => $price) {
-                    if ($key == 'c' . $eq) {
-                        $totals[$key] += $price;
+                $totals['c' . $eq] = 0;
+            }    
+            if ($this->price != null) {
+                foreach ($equip_array as $eq) {
+                    foreach ($this->price as $key => $price) {
+                        if ($key == 'c' . $eq) {
+                            $totals[$key] += $price;
+                        }
                     }
                 }
-            }
-        }
-
-        if ($this->profit != null) {
-            foreach ($equip_array as $eq) {
-                foreach ($this->profit as $k => $profit) {
-                    if ($k == 'm' . $eq) {
-                        $clear_key = str_replace('m', 'c', $k);
-                        $totals[$clear_key] += $profit;
+            }    
+            if ($this->profit != null) {
+                foreach ($equip_array as $eq) {
+                    foreach ($this->profit as $k => $profit) {
+                        if ($k == 'm' . $eq) {
+                            $clear_key = str_replace('m', 'c', $k);
+                            $totals[$clear_key] += $profit;
+                        }
                     }
                 }
-            }
+            }            
+        } catch (Exception $e) {
+            Log::error($e);
         }
-
+        
         $this->update(['total' => $totals]);
     }
 
