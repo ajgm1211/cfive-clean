bc1578f268689145c450d5e7d47750d87f01158e
amiraniotm <hectordt92@gmail.com>
2022-05-09 11:26:35 +0100

    Adding italian and catalan as available languages

--- resources/views/companies/partials/form_add_companies.blade.php
+++ resources/views/companies/partials/form_add_companies.blade.php
@@ -32,5 +32,5 @@
 </div>
 <div class="form-group m-form__group">
     {!! Form::label('pdf_language', 'PDF Language') !!}
-    {{ Form::select('pdf_language',['0'=>'Choose a language','english'=>'English','spanish'=>'Spanish','portuguese'=>'Portuguese'],null,['class'=>'custom-select form-control','id' => 'pdf_language']) }}
+    {{ Form::select('pdf_language',['0'=>'Choose a language','english'=>'English','spanish'=>'Spanish','portuguese'=>'Portuguese', 'italian'=>'Italian'],null,['class'=>'custom-select form-control','id' => 'pdf_language']) }}
 </div>
