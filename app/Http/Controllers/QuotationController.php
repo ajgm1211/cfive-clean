cc17b740415fe354d4cba26ec2c58e4f20979bee
DiegoHuacanjulca <diant2405@gmail.com>
2022-05-10 05:14:20 -0500

    optimize filters to quote

--- app/Http/Controllers/QuotationController.php
+++ app/Http/Controllers/QuotationController.php
@@ -56,17 +56,42 @@ class QuotationController extends Controller
 
     function list(Request $request)
     {   
-        $company_user_id = \Auth::user()->company_user_id;
-        $subtype = \Auth::user()->options['subtype'];
-        $user_id = \Auth::user()->id;
+        $user = auth()->user();
+
+        $company_user_id = $user->company_user_id;
+        $subtype = $user->options['subtype'];
+        $user_id = $user->id;
         
         //Permisos de subtype comercial, solo puede acceder a sus propias cotizaiones
-        if($subtype === 'comercial') {
-            $results = ViewQuoteV2::filterByCurrentUser()->filter($request);
+        if ($subtype === 'comercial') {
+            $query = ViewQuoteV2::filterByCurrentUser();
         } else {
-            $results = ViewQuoteV2::filterByCurrentCompany()->filter($request);
+            $query = ViewQuoteV2::filterByCurrentCompany();
+        }
+        
+        /*
+        $attributes = [
+            'user_id',
+            'company_id',
+            ...
+        ];
+        foreach ($attributes as $attr) {
+            if ($request->has($attr)) {
+                $query->whereIn($attr, $request->input($attr));
+            }
+        }*/
+
+        if ($request->has('user_id')) {
+            $userIds = [$request->input('user_id')];
+            $query->whereIn('user_id', $userIds);
         }
 
+        if ($request->has('company_id')) {
+            $companyIds = [$request->input('company_id')];
+            $query->whereIn('company_id', $companyIds);
+        }
+
+        $results = $query->orderByDesc('id')->paginate(10);
         return QuotationListResource::collection($results);
     }
 
@@ -1159,5 +1184,127 @@ class QuotationController extends Controller
 
         return new CostSheetResource($quote, $autorate);
 
+    }
+
+    /**
+     * En adelante mover a QuotationFiltersController
+     */
+
+    private function getBaseQuery() {
+
+        $subtype = auth()->user()->options['subtype'];
+
+        if ($subtype === 'comercial') {
+            $query = ViewQuoteV2::filterByCurrentUser();
+        } else {
+            $query = ViewQuoteV2::filterByCurrentCompany();
+        }
+
+        return $query;
+    }
+
+    public function getFilterOptions() {
+        $query = $this->getBaseQuery();
+
+        $options = [];
+
+        $options['id'] = $this->getIdOptions($query);
+        $options['quote_id'] = $this->getQuoteIdOptions($query);
+        $options['custom_quote_id'] = $this->getCustomQuoteIdOptions($query);
+        $options['status'] = $this->getStatusOptions($query);
+        $options['company_id'] = $this->getCompanyIdOptions($query);
+        $options['type'] = $this->getTypeOptions($query);
+        $options['origin'] = $this->getOriginOptions($query);
+        $options['destiny'] = $this->getDestinationOptions($query);
+        $options['user_id'] = $this->getUserIdOptions($query);
+
+        return $options;
+    }
+
+    private function getDestinationOptions($query) {
+        return $query->with([
+            'destination_harbor' => function ($q) {
+                return $q->distinct('id')->get([
+                    'harbors.id', 'harbors.display_name'
+                ]);
+            }
+        ])->get([
+            'id'
+        ])->pluck('destination_harbor')
+        ->flatten()
+        ->unique('id')->values()
+        ->map(function ($harbor) {
+            $harbor->label = $harbor->display_name;
+            unset($harbor->display_name);
+            unset($harbor->quote_id);
+            return $harbor;
+        });
+    }
+
+    private function getOriginOptions($query) {
+        return $query->with([
+            'origin_harbor' => function ($q) {
+                return $q->distinct('id')->get([
+                    'harbors.id', 'harbors.display_name'
+                ]);
+            }
+        ])->get([
+            'id'
+        ])->pluck('origin_harbor')
+        ->flatten()
+        ->unique('id')->values()
+        ->map(function ($harbor) {
+            $harbor->label = $harbor->display_name;
+            unset($harbor->display_name);
+            unset($harbor->quote_id);
+            return $harbor;
+        });
+    }
+
+    private function getIdOptions($query) {
+        return $query->distinct('id')->pluck('id');
+    }
+
+    private function getTypeOptions($query) {
+        return $query->distinct('type')->pluck('type');
+    }
+    
+    private function getStatusOptions($query) {
+        return $query->distinct('status')->pluck('status');
+    }
+
+    private function getCompanyIdOptions($query) {
+        $companyIds = $query->distinct('company_id')->get(['company_id']);        
+        $companies = Company::whereIn('id', $companyIds)->get(['id', 'business_name']);
+
+        return $companies->map(function ($c) {
+            $c->label = $c->business_name;
+            unset($c->business_name);
+            return $c;
+        });
+    }
+    
+    private function getCustomQuoteIdOptions($query) {
+        return $query->distinct('custom_quote_id')->pluck('custom_quote_id');
+    }
+
+    private function getQuoteIdOptions($query) {
+        return $query->distinct('quote_id')->pluck('quote_id');
+    }
+
+    private function getUserIdOptions($query) {
+        return $query->with([
+            'user' => function ($q) {
+                return $q->select(['id', 'name', 'lastname']);
+            }
+        ])->distinct('user_id')
+        ->get(['user_id'])
+        ->pluck('user')
+        ->map(function ($u) {
+            $u->label = $u->name . ' ' . $u->lastname;
+            unset($u->name);
+            unset($u->lastname);
+            return $u;
+        });
     }
 }
