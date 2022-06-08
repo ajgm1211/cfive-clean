ec5ba286636a94de25873f70b3ce403a4dc42777
Alejandro Gonzalez <alejandro@cargofive.com>
2022-06-08 11:41:38 -0400

    Fix branch v6.6.5

--- resources/js/quote/views/App.vue
+++ resources/js/quote/views/App.vue
@@ -9,12 +9,13 @@
             <div class="row">
               <div class="col-6">
                 <b-card-title>Quotes</b-card-title>
+               
               </div>
               <div class="col-6">
                 <div class="float-right">
-                  <a href="/api/search" class="btn btn-primary btn-bg">
-                    <i class="fa fa-search"></i> Search Rates
-                  </a>
+                  <a href="/api/search" class="btn btn-primary btn-bg"
+                    ><i class="fa fa-search"></i> Search Rates</a
+                  >
                 </div>
               </div>
             </div>
@@ -41,7 +42,7 @@ import Inland from "./Inland";
 import Ocean from "./Ocean";
 import actions from "../../actions";
 import Local from "./Local";
-import DataTable from "../../components/DataTableV2";
+import DataTable from "../../components/DataTable";
 import HelpDropdown from "../../components/HelpDropdown";
 
 export default {
@@ -59,55 +60,53 @@ export default {
       activeOcean: false,
       actions: actions,
       fields: [
-        { 
-          key: "id", 
-          label: "ID", 
-          filterisOpen: true, 
-          isObject: false },
-        { 
-          key: "quote_id", 
-          label: "Quote ID", 
-          filterisOpen: true, 
-          isObject: false },
-        { 
-          key: "custom_quote_id", 
-          label: "Custom ID", 
-          filterisOpen: true, 
-          isObject: false},
+        { key: "id", label: "ID", filterIsOpen:false },
+        { key: "quote_id", label: "Quote ID", filterIsOpen:false },
+        { key: "custom_quote_id", label: "Custom ID", filterIsOpen:false },
         {
           key: "status",
           label: "status",
-          filterisOpen: true,
-          isObject: false
+          formatter: (value) => {
+            return value.name;
+          },
+          filterIsOpen:false,
+          filterTrackBy: "name",
+          trackLabel: "name",
         },
         {
           key: "company_id",
           label: "Client",
-          filterisOpen: true,
-          isObject: true
+          formatter: (value) => {
+            return this.setClient(value);
+          },
+          filterIsOpen:false,
+          filterTrackBy: "business_name",
+          trackLabel: "business_name",
         },
-        { key: "type", label: "Type", filterisOpen:true, isObject: false },
+        { key: "type", label: "Type", filterIsOpen:false },
         {
           key: "origin",
           label: "Origin",
+          filterIsOpen:false,
           collapse: "Show origins",
-          filterisOpen:true,
-          isObject: true
         },
         {
           key: "destiny",
           label: "Destiny",
+          filterIsOpen:false,
           collapse: "Show destinations",
-          filterisOpen:true,
-          isObject: true,
         },
         {
           key: "user_id",
           label: "User",
-          filterisOpen:true,
-          isObject: true,
+          formatter: (value) => {
+            return value.fullname;
+          },
+          filterIsOpen:false,
+          filterTrackBy: "fullname",
+          trackLabel: "fullname",
         },
-        { key: "created_at", label: "Created at", filterisOpen: false },
+        { key: "created_at", label: "Created at", filterIsOpen:false },
       ],
       helpOptions: [
         {
