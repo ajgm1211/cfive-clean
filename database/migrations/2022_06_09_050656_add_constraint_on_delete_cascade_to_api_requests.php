b551d6d8a1de850f8d22628888ba9e8a20791f92
DiegoHuacanjulca <diant2405@gmail.com>
2022-06-09 00:15:58 -0500

    add on cascade in price_level_id to api_requests

---
+++ database/migrations/2022_06_09_050656_add_constraint_on_delete_cascade_to_api_requests.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddConstraintOnDeleteCascadeToApiRequests extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('api_requests', function (Blueprint $table) {
+            $table->dropForeign('api_requests_price_level_id_foreign');
+            $table->foreign('price_level_id')->references('id')->on('price_levels')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('api_requests', function (Blueprint $table) {
+            $table->dropForeign('api_requests_price_level_id_foreign');
+            $table->foreign('price_level_id')->references('id')->on('price_levels');
+        });
+    }
+}
