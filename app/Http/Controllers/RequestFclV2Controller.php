06a58b00b6303d8351bbcd21a4427c85e22abd5c
Christopher Chirino <critijo@gmail.com>
2022-06-13 10:59:21 -0400

    Checkout Queue for emails in high

--- app/Http/Controllers/RequestFclV2Controller.php
+++ app/Http/Controllers/RequestFclV2Controller.php
@@ -496,7 +496,7 @@ class RequestFclV2Controller extends Controller
                     if (env('APP_VIEW') == 'operaciones') {
                         SendEmailRequestFclJob::dispatch($usercreador->toArray(), $id)->onQueue('operaciones');
                     } else {
-                        SendEmailRequestFclJob::dispatch($usercreador->toArray(), $id);
+                        SendEmailRequestFclJob::dispatch($usercreador->toArray(), $id)->onQueue('high');
                     }
                 }
                 //Calling Mix Panel's event
